90de81cdeda317deb8d81116b9183a939be196a0
Яцишин Владислав Игоревич <vladislav.yatsishin@mvideo.ru>
2024-02-05 14:40:48 +0300

    Add redis

--- code/index.php
+++ code/index.php
@@ -1,4 +1,21 @@
 <?php
 echo "Hello, Otus!";
 
+echo '<pre>';
+print_r(get_loaded_extensions());
+echo '</pre>';
+
+if (class_exists('Redis')) {
+    $redis = new \Redis();
+    try {
+        $redis->connect('redis', 6379);
+    } catch (\Exception $e) {
+        var_dump($e->getMessage());
+        die;
+    }
+
+    echo "Connection to server successfully <br>";
+
+    echo "Server is running: " . $redis->ping() . "<br>";
+}
 phpinfo();
