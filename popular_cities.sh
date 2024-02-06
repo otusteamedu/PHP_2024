8efbcea7fd64ac77c777d80e3d367f73fd46ad84
afilipov92 <afilipov92@gmail.com>
2024-02-06 19:06:44 +0300

    hw2

---
+++ popular_cities.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="users.txt"
+
+if [ ! -f "$FILE" ]; then
+  echo "Ошибка: Файл $FILE не существует"
+  exit 1
+fi
+
+POPULAR_CITIES=$(awk 'NR>1 {print $3}' $FILE | sort | uniq -c | sort -rn | head -n 3)
+
+echo "3 наиболее популярных города:"
+echo "$POPULAR_CITIES" | awk '{print $2, $1}'
