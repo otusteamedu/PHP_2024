185f8990fbe359693db959d1511aed2afdfbf56c
VictoriaBabikova <babikovav8@gmail.com>
2024-02-09 11:06:24 +0300

    fixed bash scripts: calculator.sh and sorting.sh

--- calculator.sh
+++ calculator.sh
@@ -7,13 +7,13 @@ then
         if [[ "$1" =~ .\. || "$2" =~ .\. ]]
         then 
             result=$(awk "BEGIN {printf \"%.2f\", ${1}+${2}}")
-            echo $result | sed 's/\,/\./'
+            echo "$result" | sed 's/\,/\./'
         else
             let result=$1+$2
             echo "$result"
         fi
     else
-        echo One of args is not a number
+        echo "One of args is not a number"
     fi
 else
     echo "Args less or more than 2"
--- sorting.sh
+++ sorting.sh
@@ -5,6 +5,6 @@ then
     echo "file '$1' does not exist."
     exit 1
 fi
-file=$(sort -k3 $1 | awk '{print $3}' | uniq -d)
+file=$(sort -k3 "$1" | awk '{print $3}' | uniq -d)
 echo "Popular cities:"
-echo $file
+echo "$file" | sed "/^$/d"
