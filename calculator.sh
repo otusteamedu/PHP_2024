8c985c130ce0f309100de93f692885d866db53a7
VictoriaBabikova <babikovav8@gmail.com>
2024-02-09 10:52:38 +0300

    wrote bash scripts:  calculator.sh and sorting.sh

---
+++ calculator.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+if [ "$#" -eq 2 ]
+then
+    if [[ "$1" =~ ^-?[0-9]+[.]?[0-9]*$ && "$2" =~ ^-?[0-9]+[.]?[0-9]*$ ]]
+    then
+        if [[ "$1" =~ .\. || "$2" =~ .\. ]]
+        then 
+            result=$(awk "BEGIN {printf \"%.2f\", ${1}+${2}}")
+            echo $result | sed 's/\,/\./'
+        else
+            let result=$1+$2
+            echo "$result"
+        fi
+    else
+        echo One of args is not a number
+    fi
+else
+    echo "Args less or more than 2"
+fi
---
+++ sorting.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+if [ ! -f "$1" ]
+then
+    echo "file '$1' does not exist."
+    exit 1
+fi
+file=$(sort -k3 $1 | awk '{print $3}' | uniq -d)
+echo "Popular cities:"
+echo $file
