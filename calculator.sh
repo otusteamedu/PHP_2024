b6af7841721797e7f44d7e4ef5ba3887a958d538
VictoriaBabikova <babikovav8@gmail.com>
2024-02-10 13:51:18 +0300

    fixed calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -4,14 +4,8 @@ if [ "$#" -eq 2 ]
 then
     if [[ "$1" =~ ^-?[0-9]+[.]?[0-9]*$ && "$2" =~ ^-?[0-9]+[.]?[0-9]*$ ]]
     then
-        if [[ "$1" =~ .\. || "$2" =~ .\. ]]
-        then 
-            result=$(awk "BEGIN {printf \"%.2f\", ${1}+${2}}")
-            echo "$result" | sed 's/\,/\./'
-        else
-            let result=$1+$2
-            echo "$result"
-        fi
+        result=$(awk "BEGIN {print ${1}+${2}}")
+        echo "$result" | sed 's/\,/\./'
     else
         echo "One of args is not a number"
     fi
