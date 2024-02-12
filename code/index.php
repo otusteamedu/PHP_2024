a65736f77aa0bd0888c76afa61e0972da64cea35
Яцишин Владислав Игоревич <vladislav.yatsishin@mvideo.ru>
2024-02-12 19:35:39 +0300

    Add env

--- code/index.php
+++ code/index.php
@@ -14,7 +14,7 @@ $newLine = '<br>';
 if (class_exists('Redis')) {
     $redis = new \Redis();
     try {
-        $redis->connect('redis', 6379);
+        $redis->connect('redis', getenv('REDIS_PORT'));
 
         echo 'Redis connected:' . $redis->ping() . $newLine;
         $redis->close();
@@ -27,7 +27,7 @@ if (class_exists('Redis')) {
 
 try {
     $memcached = new \Memcached();
-    $memcachedServerConnection = $memcached->addServer('memcached', 11211);
+    $memcachedServerConnection = $memcached->addServer('memcached', getenv('MEMCACHED_PORT'));
 
     if ($memcachedServerConnection === true) {
         echo 'Memcached is connected' . $newLine;
@@ -38,32 +38,26 @@ try {
     echo "Memcached error: {$e->getMessage()} $newLine";
 }
 
-$host = 'db';
-$db = 'laravel';
-$user = 'root';
-$pass = 'qwerty123!wq';
-$charset = 'utf8';
+$host = getenv('MYSQL_HOST');
+$db = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_ROOT_PASSWORD');
+$charset = getenv('MYSQL_CHARSET');
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-var_dump($dsn);
-$opt = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-
 
 try {
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
     $pdo = new PDO($dsn, $user, $pass, $opt);
     echo 'PDO is connected' . $newLine;
 } catch (PDOException $e) {
     echo "PDO error: {$e->getMessage()} $newLine";
 }
-
-echo '<pre>';
-print_r(get_loaded_extensions());
-echo '</pre>';
-phpinfo();
 ?>
 </body>
 </html>
