18f54be4e06c0685c3df60725596b06d0c5984f0
Pavel <pavelsergmgtu@gmail.com>
2024-02-15 21:37:07 +0500

    release hw3

---
+++ sites/second.local/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>second.local</title>
+</head>
+<body>
+<header>
+    <h1>Это второй сайт second.local</h1>
+</header>
+<main>
+    <?php echo 'PHP работает'?>
+</main>
+</body>
+</html>
