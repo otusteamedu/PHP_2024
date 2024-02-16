6603f17c09ba398c42de6cc6985de7359c15e9c2
ALogachev <aelof@yandex.ru>
2024-02-16 21:20:26 +0300

    ALogachev/hw2 Finished sum.sh script

--- .deployment/bash/sum.sh
+++ .deployment/bash/sum.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 PARAM_COUNT=$#
+
+is_float() {
+  if [[ $1 =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+        return 0
+      else
+        return 1
+  fi
+}
+
 if [ $PARAM_COUNT -lt 2 ]; then
     echo 'Для операции необходимо передать два числа!'
     exit 1
@@ -9,10 +18,17 @@ fi
 NUM1=$1
 NUM2=$2
 
+if ! is_float "$NUM1" || ! is_float "$NUM2"; then
+    echo 'Аргументы должны быть числами!'
+    exit 1
+fi
+
 RES=$(echo "scale=2; $NUM1 + $NUM2" | bc -l )
-# ToDo: Учесть что при аргументах -2.2 2 получается -.2
+
 if [[ $RES == .* ]]; then
     RES="0$RES"
+elif [[ $RES == '-.'* ]]; then
+    RES="-0.${RES:2}"
 fi
 
 echo $RES
