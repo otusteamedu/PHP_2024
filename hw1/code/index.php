8545e02fa8e210c1949719a6e7a7e97ffd3c6c44
neon-light84 <goroshnikov.pavel@gmail.com>
2024-02-18 23:10:23 +0300

    PHP код. Причесывание.

--- hw1/code/index.php
+++ hw1/code/index.php
@@ -1,5 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+             <title>Document</title>
+</head>
+<body>
 <?php
+echo "It's work!!!!<br>".date("Y-m-d H:i:s") ."<br><br>";
+?>
 
-echo "Привет, Otus!<br>".date("Y-m-d H:i:s") ."<br><br>";
+<form method="post" action="<?=$_SERVER['SCRIPT_NAME']?>" name="redis">
+    <b>Redis</b><br>
+    key: <input type="text" name="redis_key"><br>
+    value: <input type="text" name="redis_value"><br>
+    <button type="submit">set redis</button><br>
+</form>
+<hr>
+<form method="post" action="<?=$_SERVER['SCRIPT_NAME']?>" name="mem">
+    <b>Memcached</b><br>
+    key: <input type="text" name="mem_key"><br>
+    value: <input type="text" name="mem_value"><br>
+    <button type="submit">set memcached</button><br>
+</form>
+<hr>
+<form method="post" action="<?=$_SERVER['SCRIPT_NAME']?>">
+    <b>Read</b><br>
+    key: <input type="text" name="read_key"><br>
+    <button type="submit">read</button><br>
+</form>
+<hr>
 
-echo "Что-то еще";
+<?php
+if (!empty($_REQUEST['redis_key'])) {
+    try {
+        $redis = new Redis();
+        $redis->connect('hw1_redis');
+        $redis->set($_REQUEST['redis_key'], $_REQUEST['redis_value'], ['EX' => 60 * 5]);
+    } catch(RedisException $ex) {
+        echo "RedisException: \n";
+        echo "<pre>\n";
+        print_r($ex);
+        echo "</pre>\n";
+    }
+} elseif (!empty($_REQUEST['mem_key'])) {
+    $memcached = new Memcached();
+    $memcached->addServer('hw1_memcached', 11211);
+    $memcached->set($_REQUEST['mem_key'], $_REQUEST['mem_value'], 60 * 5);
+
+} elseif (!empty($_REQUEST['read_key'])) {
+    try {
+        $redis = new Redis();
+        $redis->connect('hw1_redis');
+        $redisResult = $redis->get($_REQUEST['read_key']);
+        if ($redisResult === false) {
+            $redisResult = '--//--';
+        }
+    } catch(RedisException $ex) {
+        echo "RedisException: \n";
+        echo "<pre>\n";
+        print_r($ex);
+        echo "</pre>\n";
+        $redisResult = '--//--';
+    }
+
+    $memcached = new Memcached();
+    $memcached->addServer('hw1_memcached', 11211);
+    $memcachedResult = $memcached->get($_REQUEST['read_key']);
+    if ($memcachedResult === false) {
+        $memcachedResult = '--//--';
+    }
+
+
+    echo "in redis: $redisResult <br>\n";
+    echo "in redis: $memcachedResult <br>\n";
+
+}
+
+
+?>
+
+
+</body>
+</html>
+
---
+++ hw1/code/mysql.php
@@ -0,0 +1,18 @@
+<?php
+$dsn = "mysql:host=hw1_mysql;dbname={$_SERVER['MYSQL_DATABASE']}";
+try {
+    $dbh = new PDO($dsn, $_SERVER['MYSQL_USER'], $_SERVER['MYSQL_PASSWORD']);
+} catch (PDOException $ex) {
+    echo "PDOException: \n";
+    print_r($ex);
+}
+
+$sth = $dbh->query('SELECT * FROM la_la');
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<pre>\n";
+print_r($result);
+echo "</pre>\n";
+
+$sth = null;
+$dbh = null;
