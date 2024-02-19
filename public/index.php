87979fb8e1129e92bffe43fba039f131f2b6a0df
alexander-pogorelov <ics.price@gmail.com>
2024-02-19 15:52:51 +0300

    hw4: made front controller smaller

--- public/index.php
+++ public/index.php
@@ -1,39 +1,8 @@
 <?php
 
-use AlexanderPogorelov\Validator\Emitter;
-use AlexanderPogorelov\Validator\Request;
-use AlexanderPogorelov\Validator\Response;
-use AlexanderPogorelov\Validator\Validator;
+use AlexanderPogorelov\Validator\App;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$request = new Request();
-$response = new Response();
-
-if (!$request->isPostRequest()) {
-    $response
-        ->setSuccess(false)
-        ->setMessage('Only Post request is allowed')
-        ->setStatusCode(405)
-        ->setReasonPhrase('Method Not Allowed')
-        ->addHeader('Allow', 'POST');
-    $emitter = new Emitter($response);
-    $emitter->emit();
-}
-
-$inputValue = $request->getPostValue('String');
-
-$result = (new Validator())->validateString($inputValue);
-
-$response
-    ->setSuccess($result->success)
-    ->setMessage($result->message);
-
-if (!$result->success) {
-    $response
-        ->setStatusCode(400)
-        ->setReasonPhrase('Bad Request');
-}
-
-$emitter = new Emitter($response);
-$emitter->emit();
+$app = new App();
+$app->run();
