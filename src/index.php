594f72b73ea430cb33aacf36c0ce1eee756b111f
AleksandrShutov <a.shutov@intensa.ru>
2024-02-24 01:00:04 +0300

    hw4

--- src/index.php
+++ src/index.php
@@ -2,37 +2,9 @@
 
 declare(strict_types=1);
 
-try {
-    if (empty($_POST["string"])) {
-        throw new Exception("параметр `string` не передан или пустой");
-    }
-    $string = $_POST["string"];
-    $count = mb_strlen($string);
+require_once "../vendor/autoload.php";
 
-    if ($string["0"] !== "(" || $string[--$count] !== ")") {
-        throw new Exception("неверный формат строки");
-    }
-    $stack = [];
+use AShutov\Hw4\App;
 
-    for ($i = 0; $i <= $count; $i++) {
-        if ($string[$i] === "(") {
-            $stack[] = $string[$i];
-            continue;
-        }
-
-        if ($string[$i] === ")" && !empty($stack)) {
-            array_pop($stack);
-        } else {
-            throw new Exception("неверный формат строки");
-        }
-    }
-
-    if (!empty($stack)) {
-        throw new Exception("неверный формат строки");
-    }
-    echo "Всё ok";
-} catch (Throwable $e) {
-    http_response_code(400);
-    echo "Всё плохо, \n";
-    echo $e->getMessage();
-}
+$app = new App();
+echo $app->run();
