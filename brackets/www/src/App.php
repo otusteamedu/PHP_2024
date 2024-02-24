1483ce65584d05527612fac043e84c4b9239be4c
hinoho <hinoho27@gmail.com>
2024-02-24 20:02:19 +0300

    style

--- brackets/www/src/App.php
+++ brackets/www/src/App.php
@@ -10,7 +10,7 @@ class App
             $request = new Request();
             $request->validateString();
             return 'OK';
-        }
+        } 
         catch (\Throwable $e) {
             http_response_code(400);
             return $e->getMessage();
