0386d6b226b04212ff437936e144e20ca2bcf066
rail <rail@schoolbusmanager.com>
2024-02-25 21:57:13 +0300

    HW5 nginx balancer and connected memcache to php-fpm

---
+++ src/public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+require_once "../../vendor/autoload.php";
+
+use RailMukhametshin\Hw\Requests\StringRequest;
+
+$request = new StringRequest();
+
+try {
+    if ($request->isPost() === false){
+        http_response_code(403);
+        throw new Exception('Method not allowed');
+    }
+
+    if ($request->isEmptyString()){
+        http_response_code(400);
+        throw new Exception('String is empty');
+    }
+
+    if (!$request->isValidString()){
+        http_response_code(400);
+        throw new Exception('String is not valid');
+    }
+
+    http_response_code(200);
+    echo json_encode([
+        'result' => 'ok',
+        'hostname' => $_SERVER['HOSTNAME']
+    ]);
+}catch (Exception $exception){
+    echo json_encode([
+        'message' => $exception->getMessage()
+    ]);
+}
+
+
+
+
+
