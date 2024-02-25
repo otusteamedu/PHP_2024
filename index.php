075f26abecb6ff300f32ab4e77aa4cdf013e2767
ALogachev <aelof@yandex.ru>
2024-02-25 22:13:05 +0300

    ALogachev/hw5 Finished with docker compose and configs

--- index.php
+++ index.php
@@ -4,37 +4,24 @@ require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
+
+echo "Привет, Otus!<br>" . date("Y-m-d H:i:s") . "<br><br>";
+
+echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+
+/*
+ * ToDo:
+ *  1) Образ nginx-upstream-only на другие два nginx контейнера
+ *  2) Образ ngnix-upstream-to-php на два php-fpm контейнера
+ *  3) Образ memcached и postgres возможно как кластера
+ */
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-// postgres data
-$host = $_ENV['POSTGRES_DB_HOST'] ?? null;
-$db   = $_ENV['POSTGRES_DB_NAME'] ?? null;
-$dbPort = $_ENV['POSTGRES_PORT'] ?? null;
-$user = $_ENV['POSTGRES_USER'] ?? null;
-$pass = $_ENV['POSTGRES_PASSWORD'] ?? null;
+
 // redis data
 $redisHost = $_ENV['REDIS_HOST'] ?? null;
 $redisPort = $_ENV['REDIS_PORT'] ?? null;
-// memcached data
-$memcachedHost = $_ENV['MEMCACHED_HOST'] ?? null;
-$memcachedPort = $_ENV['MEMCACHED_PORT'] ?? null;
 
-$dsn = "pgsql:host=$host;port=$dbPort;dbname=$db;user=$user;password=$pass";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-// postgres connection check
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    $pdo->exec("SET client_encoding TO 'UTF8';");
-    echo "Соединение с базой данных установлено успешно.<br>";
-} catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
-// redis connection check
 try {
     $redis = new Redis();
     $redis->connect($redisHost, $redisPort);
@@ -42,13 +29,13 @@ try {
 } catch (Exception $e) {
     echo "Ошибка подключения к Redis: " . $e->getMessage();
 }
-// memcached connection check
-try {
-    $memcached = new Memcached();
-    $memcached->addServer($memcachedHost, $memcachedPort);
-    echo "Соединение с Memcached успешно установлено.<br>";
-} catch (Exception $e) {
-    echo "Ошибка подключения к Memcached: " . $e->getMessage();
-}
+//// memcached connection check
+//try {
+//    $memcached = new Memcached();
+//    $memcached->addServer($memcachedHost, $memcachedPort);
+//    echo "Соединение с Memcached успешно установлено.<br>";
+//} catch (Exception $e) {
+//    echo "Ошибка подключения к Memcached: " . $e->getMessage();
+//}
 
 phpinfo();
