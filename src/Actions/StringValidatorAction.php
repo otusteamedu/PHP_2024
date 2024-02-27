6132335a62f51be4791062d817fcb3cff7cd766a
rail <rail@schoolbusmanager.com>
2024-02-27 09:14:16 +0300

    HW5 fix StringValidatorAction

--- src/Actions/StringValidatorAction.php
+++ src/Actions/StringValidatorAction.php
@@ -10,28 +10,34 @@ use RailMukhametshin\Hw\Requests\StringRequest;
 
 class StringValidatorAction
 {
-    public function run(StringRequest $request): void
+    private StringRequest $request;
+
+    public function __construct()
+    {
+        $this->request = new StringRequest();
+    }
+    public function run(): string
     {
         try {
-            if ($request->isPost() === false) {
+            if ($this->request->isPost() === false) {
                 throw new HttpException('Method not allowed', 403);
             }
 
-            if ($request->isEmptyString()) {
+            if ($this->request->isEmptyString()) {
                 throw new HttpException('String is empty');
             }
 
-            if (!$request->isValidString()) {
+            if (!$this->request->isValidString()) {
                 throw new HttpException('String is not valid');
             }
 
-            echo json_encode([
+            return json_encode([
                 'result' => 'ok',
                 'hostname' => $_SERVER['HOSTNAME']
             ]);
         } catch (Exception $exception) {
             http_response_code($exception instanceof HttpException ? $exception->getStatus() : 400);
-            echo json_encode([
+            return json_encode([
                 'message' => $exception->getMessage()
             ]);
         }
--- src/public/index.php
+++ src/public/index.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 require_once "../../vendor/autoload.php";
 
 use RailMukhametshin\Hw\Actions\StringValidatorAction;
-use RailMukhametshin\Hw\Requests\StringRequest;
 
-$request = new StringRequest();
 $action = new StringValidatorAction();
-$action->run($request);
+echo $action->run();
