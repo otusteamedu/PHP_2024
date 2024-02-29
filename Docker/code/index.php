6eb752d72a8593468cd82b7546c39053b2cdefad
BeliaevNO <BeliaevNO@alfastrah.ru>
2024-02-29 21:11:14 +0600

    Complete HW4

--- Docker/code/index.php
+++ Docker/code/index.php
@@ -1,4 +1,47 @@
 <?php
-echo "Hello, Проверяющие :)";
+require "StringAnalyzer.php";
 
-phpinfo();
+if ($_POST['string'] !== "") {
+    $string = $_POST['string'];
+} else {
+    http_response_code(400);
+    throw new \RuntimeException("Строка не найдена в POST запросе", 400);
+}
+
+header("Content-Type: application/json");
+$StringAnalyzer = new StringAnalyzer();
+
+$response = $StringAnalyzer->checkBrackets($string);
+$container = $_SERVER['HOSTNAME'];
+
+$redis = new Redis();
+
+try {
+    $redis->connect('redis', 6379);
+    $redisConnect = true;
+} catch (RedisException $e) {
+    $redisConnect = false;
+}
+
+if ($response) {
+    $data = [
+        'RedisConnect' => $redisConnect,
+        'success' => true,
+        'message' => "Строка корректна",
+        'Контейнер' => $container
+    ];
+    http_response_code(200);
+} else {
+    $data = [
+        'success' => false,
+        'message' => "Строка некорректна",
+        'Контейнер' => $container
+    ];
+    http_response_code(400);
+}
+
+
+echo json_encode($data);
+
+
+
--- code/index.php
+++ code/index.php
@@ -1,4 +1,4 @@
 <?php
 echo "Hello, Otus!";
-
+$redis = new Redis();
 phpinfo();
