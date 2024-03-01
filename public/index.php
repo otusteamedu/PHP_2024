487eade1bd00dfda2abd1a835abf7eb73afff099
BeliaevNO <BeliaevNO@alfastrah.ru>
2024-03-01 15:56:23 +0600

    Фикс линтера

--- public/index.php
+++ public/index.php
@@ -2,15 +2,7 @@
 
 use App\App;
 
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = new App();
 echo $app->run();
-
-
-
-
-
-
-
 
--- src/App.php
+++ src/App.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 namespace App;
 
 use RedisConnector\RedisConnector;
