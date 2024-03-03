86bbb9ee977e3fe874bf81ff6a701ab6e796470c
ALogachev <aelof@yandex.ru>
2024-03-03 21:46:49 +0300

    ALogachev/hw6 Finished Server app

--- src/Server.php
+++ src/Server.php
@@ -8,8 +8,33 @@ use Alogachev\Homework\Interface\IRun;
 
 class Server implements IRun
 {
+    public function __construct(
+        private readonly SocketManager $manager,
+    ) {
+    }
+
     public function run(): void
     {
         echo "\nРаботает сервер!";
+
+        $this->manager->recreateBindAndListen();
+        set_time_limit(0);
+
+        while (true) {
+            $socket = $this->manager->accept();
+
+            while (true) {
+                $msg = trim($this->manager->read($socket));
+
+                if ($msg === '') {
+                    break;
+                }
+
+                echo "Получено сообщение: $msg" . PHP_EOL;
+                $size = strlen($msg);
+                $this->manager->write("Получено {$size} байт", $socket);
+            }
+        }
+
     }
 }
