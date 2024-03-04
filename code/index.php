29754668372a8728f0970b97a62e83c1b77fdc8f
aleksnoname <gitcommit@yandex.ru>
2024-03-05 00:30:19 +0300

    test_hw4

---
+++ code/index.php
@@ -0,0 +1,12 @@
+<?php
+
+#declare(strict_types=1)
+
+//if ($_POST) {
+//    echo '<pre>';
+//    var_dump($_POST);
+//    echo '</pre>';
+//}
+//else
+
+return "Привет, Домашка 4 !<br>".date("Y-m-d H:i:s") ."<br><br>";
