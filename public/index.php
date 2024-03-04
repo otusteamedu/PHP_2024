60d5531d3ebad346501322dbf2ab1a848520c96f
a.kornienko <a.kornienko@xsolla.com>
2024-03-04 14:19:54 +0300

    [hw6]: fix code-review comments

--- public/index.php
+++ public/index.php
@@ -4,9 +4,19 @@ require_once '../vendor/autoload.php';
 
 use AKornienko\Php2024\App;
 
+$emails = [
+    '1@gmail.com',
+    '1@1.com',
+    '1@gmail',
+    '1@gmail.',
+    '@gmail.com',
+    'gmail.com',
+    '1',
+];
+
 try {
     $app = new App();
-    $messages = $app->run();
+    $messages = $app->run($emails);
     foreach ($messages as $message) {
         print_r($message . PHP_EOL);
     }
--- src/App.php
+++ src/App.php
@@ -6,20 +6,9 @@ use AKornienko\Php2024\EmailValidation\Validator;
 
 class App
 {
-    private array $emails = [
-        '1@gmail.com',
-        '1@1.com',
-        '1@gmail',
-        '1@gmail.',
-        '@gmail.com',
-        'gmail.com',
-        '1',
-    ];
-
-    public function run(): \Generator
+    public function run(array $emails): \Generator
     {
-
-        foreach ($this->emails as $email) {
+        foreach ($emails as $email) {
             if (Validator::validate($email)) {
                 $message = $email . " is valid";
             } else {
