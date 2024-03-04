b98a8eb820579b42eb5ae5d97e65b98b27ec693f
slipka <rozhko.ann@gmail.com>
2024-03-04 23:45:51 +0300

    My hw1 try 2

--- src/index.php
+++ src/index.php
@@ -4,10 +4,3 @@ echo "Привет, Otus!<br>".date("Y-m-d H:i:s") ."<br><br>";
 
 echo "111111";
 
-$redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-echo $redis->ping();
-
-//$memcached = new Memcached;
-//
-//$memcached->addServer('memcached', 11211);
