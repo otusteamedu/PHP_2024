d6e13b3e87b1235428e72d3ef653ea6ca6500880
LRazumov <masdzen@gmail.com>
2024-03-04 19:47:13 +0000

    fix for psr-12

--- app/src/Solution.php
+++ app/src/Solution.php
@@ -11,11 +11,11 @@ class Solution
      * @param ListNode $list2
      * @return ListNode
      */
-    function mergeTwoLists($list1, $list2): ListNode {
+    public function mergeTwoLists($list1, $list2): ListNode
+    {
         if ($list1 === null) {
             return $list2;
-        }
-        elseif ($list2 === null) {
+        } elseif ($list2 === null) {
             return $list1;
         }
 
@@ -23,8 +23,7 @@ class Solution
         if ($list1->val < $list2->val) {
             $list = $list1;
             $list->next = $this->mergeTwoLists($list1->next, $list2);
-        }
-        else {
+        } else {
             $list = $list2;
             $list->next = $this->mergeTwoLists($list1, $list2->next);
         }
