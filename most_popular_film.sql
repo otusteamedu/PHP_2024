eb068b4244713e8ec36e6a516121c8c06ceb4b84
VictoriaBabikova <babikovav8@gmail.com>
2024-03-06 23:05:37 +0300

    fix2

--- most_popular_film.sql
+++ most_popular_film.sql
@@ -1,6 +1,5 @@
-SELECT Films.name as "most_popular_film", COUNT(Tikets.tiket_id) as "count_of_tikets", sum(Tikets.price) as "income" FROM `Films`
-    INNER JOIN Cinema_sessions ON Films.film_id=Cinema_sessions.film_id
-    INNER JOIN Tikets ON Cinema_sessions.session_id=Tikets.session_id
-    GROUP BY Films.name
-    ORDER BY `income` DESC LIMIT 1;
-
+SELECT Films.name as "most_popular_film", COUNT(Tikets.id) as "count_of_tikets", sum(Tikets.price) as "income" FROM `Films`
+    INNER JOIN Cinema_sessions ON Films.id=Cinema_sessions.id
+    INNER JOIN Tikets ON Cinema_sessions.id=Tikets.id
+GROUP BY Films.name
+ORDER BY `income` DESC LIMIT 1;
