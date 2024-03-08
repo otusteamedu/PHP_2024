14d830cf3f5fe9256630531cfa5c7913f4ca1608
alexander-pogorelov <ics.price@gmail.com>
2024-03-08 13:45:05 +0300

    hw8: cnanges according to dev review

--- cinema_query.sql
+++
@@ -1,8 +0,0 @@
-select m.name as movie, SUM(o.quantity * t.price) as total
-from `order` o
-         inner join ticket t on t.id = o.ticket_id
-         inner join session s on t.session_id = s.id
-         inner join movie m on s.movie_id = m.id
-group by m.name
-order by total DESC
-limit 1
