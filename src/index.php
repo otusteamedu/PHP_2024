48001021e562a665aa6b4de6ad5413afdad01c38
i.branitsky <dev_bi@bankiros.ru>
2024-03-09 17:33:10 +0300

    fix problem in code review

--- src/index.php
+++ src/index.php
@@ -2,32 +2,14 @@
 
 declare(strict_types=1);
 
+require_once 'vendor/autoload.php';
+
+use Igor\ValidateBrackets\App;
+
 try {
-    if (empty($_REQUEST["string"])) {
-        throw new Exception("Не верный параметр");
-    }
-    $string = $_REQUEST["string"];
-    $count = mb_strlen($string);
-    if ($string["0"] !== "(" || $string[--$count] !== ")") {
-        throw new Exception("Не верный формат строки");
-    }
-    $stack = [];
-    for ($i = 0; $i <= $count; $i++) {
-        if ($string[$i] === "(") {
-            $stack[] = $string[$i];
-            continue;
-        }
-        if ($string[$i] === ")" && !empty($stack)) {
-            array_pop($stack);
-        } else {
-            throw new Exception("Не верный формат строки");
-        }
-    }
-    if (!empty($stack)) {
-        throw new Exception("Не верный формат строки");
-    }
-    echo "Строка валидная";
+    $app = new App();
+    echo $app->run();
 } catch (Exception $e) {
     http_response_code(400);
-    echo "Ошибка в запроса скобри не верные или параметры пусты";
+    echo $e->getMessage();
 }
