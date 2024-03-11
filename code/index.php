83709e9309486dd4f702fe782c272679104bdb4d
tory_495 <ivarthewebdeveloper@gmail.com>
2024-03-11 17:14:17 +0300

    Refactoring

--- code/index.php
+++ code/index.php
@@ -1,41 +1,8 @@
 <?php
 
-if (!empty($_POST['string'])) {
-    if (isStringValid($_POST['string'])) {
-        http_response_code(200);
-        echo 'String is valid!';
-    } else {
-        throwErrorMessageCode('String is not valid!');
-    }
-} else {
-    throwErrorMessageCode('String is empty!');
-}
+declare(strict_types=1);
 
-function isStringValid(string $string): bool
-{
-    $count = 0;
+require __DIR__ . '/src/App.php';
 
-    for ($i = 0; $i < strlen($string); $i++) {
-        if ($string[$i] === '(') {
-            $count++;
-        } else if ($string[$i] === ')') {
-            $count--;
-        }
-
-        if ($count < 0) {
-            return false;
-        }
-    }
-
-    if ($count === 0) {
-        return true;
-    }
-
-    return false;
-}
-
-function throwErrorMessageCode(string $msg): void
-{
-    http_response_code(400);
-    throw new Exception($msg);
-}
+$app = new App();
+$app->run();
