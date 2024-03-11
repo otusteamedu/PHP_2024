3175501f5a7b86fc23e02c6e03b21b3988d75c9b
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-03-11 13:04:09 +0300

    ALogachev/hw10 Algorithm has been finished.

--- src/ListMerge.php
+++ src/ListMerge.php
@@ -8,6 +8,55 @@ class ListMerge
 {
     function mergeTwoLists(ListNode $list1, ListNode $list2): ListNode
     {
-        return new ListNode(0, );
+        /*
+         * Если нулевой ListNode - то это последний элемент списка.
+         */
+//        if ($list1->val > $list2->val) {
+//            $result = new ListNode($list1->val);
+//        } elseif ($list1->val < $list2->val) {
+//            $result = new ListNode($list2->val);
+//        } else {
+//            $result = new ListNode($list1->val, $list2);
+//        }
+
+        return $list1->val >= $list2->val
+            ? new ListNode(
+                $list2->val,
+                !is_null($list2->next)
+                    ? $this->mergeTwoLists($list1, $list2->next)
+                    : $list1
+            )
+            : new ListNode(
+                $list1->val,
+                !is_null($list1->next)
+                    ? $this->mergeTwoLists($list2, $list1->next)
+                    : $list2
+            );
+
+//        $result = null;
+//
+//        switch (true) {
+//            case is_null($list1->next) && !is_null($list2->next):
+//                $result = $this->mergeTwoLists($list1, $list2->next);
+//                break;
+//            case is_null($list2->next) && !is_null($list1->next):
+//                $result = $this->mergeTwoLists($list1->next, $list2);
+//                break;
+//            case !is_null($list1->next) && !is_null($list2->next):
+//                if ($list1->val > $list2->val) {
+//                    $result = $this->mergeTwoLists($list1->next, $list2);
+//                } elseif ($list1->val < $list2->val) {
+//                    $result = $this->mergeTwoLists($list1, $list2->next);
+//                } else {
+//                    $result = $this->mergeTwoLists($list1->next, $list2->next);
+//                }
+//                break;
+//        }
+//
+//
+//
+//        return $list1->val >= $list2->val
+//            ? new ListNode($list1->val, $result)
+//            : new ListNode($list2->val, $result);
     }
 }
