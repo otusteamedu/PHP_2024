f574c99225854d201df7de15725eb150a2ab5db7
a.kornienko <a.kornienko@xsolla.com>
2024-03-11 17:56:29 +0300

    [hw7]: fix code-review comments

--- sql/ddl/init.sql
+++ sql/ddl/init.sql
@@ -10,10 +10,33 @@ CREATE TABLE IF NOT EXISTS halls (
     cinema_id bigint REFERENCES cinemas (id)
 );
 
+-- жанры
+CREATE TABLE IF NOT EXISTS countries (
+    id bigint PRIMARY KEY,
+    name varchar(255) unique
+);
+
 -- фильмы
 CREATE TABLE IF NOT EXISTS movies (
     id bigint PRIMARY KEY,
-    name          varchar(255) unique
+    name          varchar(255),
+    country_id int REFERENCES countries (id),
+    year_of_creation int,
+    duration interval,
+    description text
+);
+
+-- жанры
+CREATE TABLE IF NOT EXISTS genres (
+    id bigint PRIMARY KEY,
+    name varchar(255) unique
+);
+
+-- жанры кино
+CREATE TABLE IF NOT EXISTS movies_genres (
+    id int PRIMARY KEY,
+    genre_id bigint REFERENCES genres (id),
+    movie_id bigint REFERENCES movies (id)
 );
 
 -- сеансы
@@ -21,7 +44,8 @@ CREATE TABLE IF NOT EXISTS moviesSessions (
     id bigint PRIMARY KEY,
     hall_id bigint REFERENCES halls (id),
     movie_id bigint REFERENCES movies (id),
-    scheduled_at timestamp NOT NULL
+    scheduled_at timestamp NOT NULL,
+    scheduled_to timestamp NOT NULL
 );
 
 -- места
@@ -37,16 +61,16 @@ CREATE TABLE IF NOT EXISTS tickets (
     id bigint PRIMARY KEY,
     session_id bigint REFERENCES moviesSessions (id),
     seat_id bigint REFERENCES seats (id),
-    amount numeric NOT NULL
+    price money NOT NULL
 );
 
 -- стоимость
-CREATE TABLE IF NOT EXISTS tickets_amount (
+CREATE TABLE IF NOT EXISTS tickets_price (
     id bigint PRIMARY KEY,
     hall_id bigint REFERENCES halls (id),
-    amount_time_start time NOT NULL,
-    amount_time_end time NOT NULL,
+    price_time_start time NOT NULL,
+    price_time_end time NOT NULL,
     row_from int NOT NULL,
     row_to int NOT NULL,
-    amount numeric NOT NULL
+    price money NOT NULL
 );
