0a7883254bb86b259a2e62064213f70ca1b45c8f
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-03-11 13:04:40 +0300

    ALogachev/hw10 Showed algorithm result.

--- public/app.php
+++ public/app.php
@@ -2,9 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Dotenv\Dotenv;
+use Alogachev\Homework\ListMerge;
+use Alogachev\Homework\ListNode;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+$list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
-phpinfo();
+
+$mergeList = new ListMerge();
+$mergedList = $mergeList->mergeTwoLists($list1, $list2);
+
+while (!is_null($mergedList->next)) {
+    echo $mergedList->val. PHP_EOL;
+    $mergedList = $mergedList->next;
+}
