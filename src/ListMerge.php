8946f9ed239fdcb1494c32a24e59c4d7dc8dee15
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-03-11 13:09:42 +0300

    ALogachev/hw10 Removed commented code.

--- src/ListMerge.php
+++ src/ListMerge.php
@@ -8,17 +8,6 @@ class ListMerge
 {
     function mergeTwoLists(ListNode $list1, ListNode $list2): ListNode
     {
-        /*
-         * Если нулевой ListNode - то это последний элемент списка.
-         */
-//        if ($list1->val > $list2->val) {
-//            $result = new ListNode($list1->val);
-//        } elseif ($list1->val < $list2->val) {
-//            $result = new ListNode($list2->val);
-//        } else {
-//            $result = new ListNode($list1->val, $list2);
-//        }
-
         return $list1->val >= $list2->val
             ? new ListNode(
                 $list2->val,
@@ -32,31 +21,5 @@ class ListMerge
                     ? $this->mergeTwoLists($list2, $list1->next)
                     : $list2
             );
-
-//        $result = null;
-//
-//        switch (true) {
-//            case is_null($list1->next) && !is_null($list2->next):
-//                $result = $this->mergeTwoLists($list1, $list2->next);
-//                break;
-//            case is_null($list2->next) && !is_null($list1->next):
-//                $result = $this->mergeTwoLists($list1->next, $list2);
-//                break;
-//            case !is_null($list1->next) && !is_null($list2->next):
-//                if ($list1->val > $list2->val) {
-//                    $result = $this->mergeTwoLists($list1->next, $list2);
-//                } elseif ($list1->val < $list2->val) {
-//                    $result = $this->mergeTwoLists($list1, $list2->next);
-//                } else {
-//                    $result = $this->mergeTwoLists($list1->next, $list2->next);
-//                }
-//                break;
-//        }
-//
-//
-//
-//        return $list1->val >= $list2->val
-//            ? new ListNode($list1->val, $result)
-//            : new ListNode($list2->val, $result);
     }
 }
