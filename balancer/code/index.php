254ab382d408a75a058c76dda086e23489f7a3d2
aleksnoname <gitcommit@yandex.ru>
2024-03-12 19:27:43 +0300

    balancer

--- balancer/code/index.php
+++ balancer/code/index.php
@@ -1,7 +1,10 @@
 <?php
+declare(strict_types=1);
 
+
+session_start();
 echo "Привет, Otus!<br>".date("Y-m-d H:i:s")."<br><br>";
 
-echo "<pre>";
-var_dump($_SERVER);
-echo "</pre>";
+$_SESSION['host'] = $_SERVER['HOSTNAME'];
+
+echo $_SESSION['host'].'<br/><br/>';
