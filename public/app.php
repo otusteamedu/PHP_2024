c6b0b0281cee15a4e79e40e2cc022e644e466f6f
tory_495 <ivarthewebdeveloper@gmail.com>
2024-03-12 18:44:15 +0300

    Change directory structure

--- public/app.php
+++ public/app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use IlyaPlotnikov\SocketChat\ChatService;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 try {
     $chatService = new ChatService('server');
