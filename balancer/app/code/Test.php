b7ae973aff888903cec84dd79b911b64ea00d7a7
aleksnoname <gitcommit@yandex.ru>
2024-03-13 18:55:26 +0300

    hw4

---
+++ balancer/app/code/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Test
+{
+    public function getTest() {
+        return "Test is passed!";
+    }
+}
--- balancer/app/public/index.php
+++ balancer/app/public/index.php
@@ -1,10 +1,13 @@
 <?php
 declare(strict_types=1);
-
-
 session_start();
+
+require_once '../code/Test.php';
+
 echo "Привет, Otus!<br>".date("Y-m-d H:i:s")."<br><br>";
 
 $_SESSION['host'] = $_SERVER['HOSTNAME'];
 
-echo $_SESSION['host'].'<br/><br/>';
+$test = (new Test())->getTest();
+
+echo $_SESSION['host'].'<br/><br/>'.$test;
