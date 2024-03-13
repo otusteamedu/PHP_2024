a34507827991f244c287a9d6262409b67e4af12f
Sergei Dudyuk <dev_ds@mainfin.ru>
2024-03-13 10:56:31 +0300

    Fix code style

--- src/ListNode.php
+++ src/ListNode.php
@@ -7,7 +7,7 @@ class ListNode
     public $val = 0;
     public $next = null;
 
-    function __construct($val = 0, $next = null)
+    function __construct(int $val = 0, ?ListNode $next = null)
     {
         $this->val = $val;
         $this->next = $next;
