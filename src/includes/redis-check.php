ccd77e17407cad6f8d118ab6954c902d29b9df1e
Evgeniy Rybkin <helloevgeniyrybkin@gmail.com>
2024-03-18 21:04:08 +0300

    Applied code reformatting

--- src/includes/redis-check.php
+++ src/includes/redis-check.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-if (! function_exists('isRedisConnected')) {
-    function isRedisConnected(): bool {
+if (!function_exists('isRedisConnected')) {
+    function isRedisConnected(): bool
+    {
         $redis = new Redis();
 
         try {
-            $redis->connect(getenv('REDIS_HOST'), (int) getenv('REDIS_PORT'));
+            $redis->connect(getenv('REDIS_HOST'), (int)getenv('REDIS_PORT'));
 
             return $redis->ping();
         } catch (Throwable $e) {
