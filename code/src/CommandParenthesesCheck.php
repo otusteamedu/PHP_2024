3389db3231ab21bd8233e1b6b351bed7d53fa080
“Irina <irina.s.yurtaeva@gmail.com>
2024-03-19 08:04:50 +0200

    hw4: Balancer and parentheses: cyclomatic complexity has been reduced

--- code/src/CommandParenthesesCheck.php
+++ code/src/CommandParenthesesCheck.php
@@ -11,22 +11,25 @@ class CommandParenthesesCheck implements Command
         $param = $request->get('string');
         if (empty($param)) {
             $result->addError('Required parameter "string" is empty.');
-        } else {
-            $pointer = 0;
-            $length = strlen($param);
-            for ($i = 0; $i < $length; $i++) {
-                if ($param[$i] === '(') {
-                    $pointer++;
-                } else if ($param[$i] === ')') {
-                    $pointer--;
-                    if ($pointer < 0) {
-                        break;
-                    }
+
+            return $result;
+        }
+        $pointer = 0;
+        $length = strlen($param);
+        for ($i = 0; $i < $length; $i++) {
+            if ($param[$i] === '(') {
+                $pointer++;
+            } else if ($param[$i] === ')') {
+                $pointer--;
+                if ($pointer < 0) {
+                    break;
                 }
             }
-            if ($pointer !== 0) {
-                $result->addError('Bad Parentheses.' . $param);
-            }
+        }
+        if ($pointer !== 0) {
+            $result->addError('Bad Parentheses.' . $param);
+        } else {
+            $result->addMessage('Parentheses complement each other. ' . $param);
         }
 
         return $result;
