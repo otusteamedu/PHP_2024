1bb91300fdb0da5dbdbfbf05d1b072c74061902a
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-03-27 17:48:16 +0300

    ALogachev/hw14 Started initialisation.

--- src/App.php
+++ src/App.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace Alogachev\Homework;
 
-use Alogachev\Homework\Exception\InvalidAppTypeException;
-use Alogachev\Homework\Exception\WrongInputException;
-use Alogachev\Homework\Interface\IRun;
+use Alogachev\Homework\Elk\ElkService;
 use DI\Container;
 use Dotenv\Dotenv;
+use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\Exception\AuthenticationException;
+use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Elastic\Elasticsearch\Exception\ElasticsearchException;
+use Elastic\Elasticsearch\Exception\MissingParameterException;
+use Elastic\Elasticsearch\Exception\ServerResponseException;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -18,59 +23,112 @@ use function DI\get;
 
 final class App
 {
-    private const ALLOWED_APP_TYPES = [
-        'client',
-        'server',
-    ];
+    private const INDEX_NAME = 'otus-shop';
+    private const INDEX_ALIAS = 'books';
 
     /**
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
+     * @throws AuthenticationException
      */
     public function run(): void
     {
-//        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-//        $dotenv->load();
-//        $container = $this->resolveDI();
-//        $appType = $this->resolveAppType();
-//        /** @var IRun $app */
-//        $app = match ($appType) {
-//            'server' => $container->get(Server::class),
-//            'client' => $container->get(Client::class),
-//        };
-//        $app->run();
-//
-//        echo "Выход из приложения!" . PHP_EOL;
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+        $dotenv->load();
+        $container = $this->resolveDI();
+        $client = $container->get(Client::class);
+        $health = $client->cluster()->health();
+        echo $health->asArray()['status'] . PHP_EOL;
+        $elkSearch = $container->get(ElkService::class)->createIndex();
+        try {
+            $this->initIndex($elkSearch);
+        } catch (ElasticsearchException $exception) {
+            echo $exception->getMessage() . PHP_EOL;
+        }
+
+        /*
+         * ToDO:
+         *  1) Разобраться с анализаторами и токенизаторами для русского языка
+         *  2) Загрузить файл с каталогом книг с помощью _bulk
+        */
     }
 
+    /**
+     * @throws AuthenticationException
+     */
     private function resolveDI(): ContainerInterface
     {
-        $config = new Config();
+        $host = $_ENV['ELASTICSEARCH_HOST'];
+        $port = $_ENV['ELASTICSEARCH_PORT'];
 
         return new Container([
-            Client::class => create()->constructor(
-                get('stopWord'),
-                get(SocketManager::class)
+            Client::class => ClientBuilder::create()
+                ->setHosts(["http://$host:$port"])
+                ->build(),
+            ElkService::class => create()->constructor(
+                get(Client::class)
             ),
-            SocketManager::class => create()->constructor(
-                get('socketPath')
-            ),
-            'socketPath' => $config->getSocketPath(),
-            'stopWord' => $config->getStopWord(),
         ]);
     }
 
-    private function resolveAppType(): string
+    /**
+     * @throws ClientResponseException
+     * @throws MissingParameterException
+     * @throws ServerResponseException
+     */
+    private function initIndex(Client $client): void
     {
-        if ($_SERVER['argc'] !== 2) {
-            throw new WrongInputException();
-        }
-        $appType = strtolower(trim($_SERVER['argv'][1]));
-
-        if (!in_array($appType, self::ALLOWED_APP_TYPES)) {
-            throw new InvalidAppTypeException($appType);
+        if ($client->indices()->exists(['index' => self::INDEX_NAME])) {
+            return;
         }
 
-        return $appType;
+        $params = [
+            'index' => self::INDEX_NAME,
+            'alias' => self::INDEX_ALIAS,
+            'body' => [
+                'settings' => [
+                    'number_of_shards' => 1,
+                    'number_of_replicas' => 0,
+                    'analysis' => [
+                        'analyzer' => [
+                            'default' => [
+                                'type' => 'russian',
+                            ],
+                        ],
+                    ],
+                ],
+                'mappings' => [
+                    'properties' => [
+                        'title' => [
+                            'type' => 'text',
+                            'analyzer' => 'russian',
+                        ],
+                        'sku' => [
+                            'type' => 'keyword',
+                        ],
+                        'category' => [
+                            'type' => 'keyword',
+                            'analyzer' => 'russian',
+                        ],
+                        'price' => [
+                            'type' => 'integer',
+                        ],
+                        'stock' => [
+                            'type' => 'nested',
+                            'properties' => [
+                                'shop' => [
+                                    'type' => 'keyword',
+                                    'analyzer' => 'russian',
+                                ],
+                                'stock' => [
+                                    'type' => 'integer',
+                                ]
+                            ]
+                        ],
+                    ]
+                ]
+            ],
+        ];
+        $client->indices()->create($params);
     }
 }
---
+++ src/Elk/ElkService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alogachev\Homework\Elk;
+
+class ElkService
+{
+    public function createIndex(string $indexName): void
+    {
+
+    }
+}
