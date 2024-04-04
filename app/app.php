83f611353b72cbbcc8258e984de4c75ff50994b0
LRazumov <masdzen@gmail.com>
2024-04-04 19:50:18 +0000

    hw init

---
+++ app/app.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+require './vendor/autoload.php';
+
+use Lrazumov\Hw10\Solution;
+use Lrazumov\Hw10\ListNode;
+
+try {
+    echo 1;
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
