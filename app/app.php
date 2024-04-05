bc1c3d66f97ce0d3cbd1b0a4d24ecdfbb6eb843d
LRazumov <masdzen@gmail.com>
2024-04-05 05:00:05 +0000

    17. Letter Combinations of a Phone Number

--- app/app.php
+++ app/app.php
@@ -8,32 +8,17 @@ use Lrazumov\Hw19\ListNode;
 use Lrazumov\Hw19\Solution;
 
 try {
-
-    // head = [3,2,0,-4], pos = 1
-    $list = new ListNode(
-        3,
-        new ListNode(
-            2,
-            new ListNode(
-                0,
-                new ListNode(-4)
-            )
-        )
-    );
-    $node = $list->next;
-    $list->next->next->next->next = $node;
     $solution = (new Solution())
-        ->hasCycle($list);
-    var_dump($solution);
-    
-    // head = [1], pos = -1
-    $list = new ListNode(
-        1
-    );
-    $solution = (new Solution())
-        ->hasCycle($list);
+        ->letterCombinations('23');
     var_dump($solution);
 
+    $solution = (new Solution())
+        ->letterCombinations('');
+    var_dump($solution);
+
+    $solution = (new Solution())
+        ->letterCombinations('2');
+    var_dump($solution);
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
