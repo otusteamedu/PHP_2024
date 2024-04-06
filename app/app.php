423c56fdc8bca552e15dff2ae52d1cb0242f293c
LRazumov <masdzen@gmail.com>
2024-04-06 20:50:29 +0000

    second solution

--- app/app.php
+++ app/app.php
@@ -6,18 +6,22 @@ require './vendor/autoload.php';
 
 use Lrazumov\Hw19\ListNode;
 use Lrazumov\Hw19\Solution;
+use Lrazumov\Hw19\Solution2;
 
 try {
     $solution = (new Solution())
-        ->letterCombinations('23');
-    var_dump($solution);
-
-    $solution = (new Solution())
-        ->letterCombinations('');
-    var_dump($solution);
-
-    $solution = (new Solution())
         ->letterCombinations('2');
+    var_dump($solution);
+
+    $solution = (new Solution2())
+        ->letterCombinations('2');
+    var_dump($solution);
+
+    $solution = (new Solution())
+        ->letterCombinations('22');
+    var_dump($solution);
+    $solution = (new Solution2())
+        ->letterCombinations('22');
     var_dump($solution);
 } catch (\Exception $e) {
     echo $e->getMessage();
