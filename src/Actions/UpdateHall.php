2e065d70d7663bb239bcab59643cde00f095ed2b
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-04-12 12:45:04 +0300

    ALogachev/hw17 Fix update action

--- src/Actions/UpdateHall.php
+++ src/Actions/UpdateHall.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Alogachev\Homework\Actions;
 
-use Alogachev\Homework\DataMapper\Entity\Hall;
 use Alogachev\Homework\DataMapper\Mapper\BaseMapper;
+use Alogachev\Homework\DataMapper\Query\QueryItem;
+use Alogachev\Homework\Exception\EmptyHallException;
 use Alogachev\Homework\Exception\InvalidInputDataException;
+use PDO;
 
 class UpdateHall
 {
@@ -18,7 +20,22 @@ class UpdateHall
     public function __invoke(array $data): void
     {
         $this->validate($data);
-        $hall = Hall::create($data['name'], (int)$data['capacity'], (int)$data['rowsCount'])->setId((int)$data['id']);
+        $queryParam = new QueryItem(
+            'id',
+            PDO::PARAM_INT,
+            (int)$data['id']
+        );
+        $hall = $this->hallMapper->finById($queryParam);
+
+        if (!isset($hall)) {
+            throw new EmptyHallException((int)$data['id']);
+        }
+
+        $hall
+            ->setName($data['name'])
+            ->setCapacity((int)$data['capacity'])
+            ->setRowsCount((int)$data['rowsCount'])
+        ;
         $this->hallMapper->update($hall);
 
         echo "Hall with id {$data['id']} updated". PHP_EOL;
