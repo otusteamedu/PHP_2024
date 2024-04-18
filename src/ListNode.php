a0d6b638d084064c866d1645e5c6e8ad65b741f1
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-04-18 11:08:01 +0300

    ALogachev/hw19 Fixes.

--- src/ListNode.php
+++ src/ListNode.php
@@ -9,7 +9,7 @@ class ListNode
     public int $val = 0;
     public ?ListNode $next = null;
 
-    function __construct(int $val = 0, ?ListNode $next = null)
+    public function __construct(int $val = 0, ?ListNode $next = null)
     {
         $this->val = $val;
         $this->next = $next;
