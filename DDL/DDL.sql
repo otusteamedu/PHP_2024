9fbd9c0a39220eea8cdfa206cc41c12db6d5ce0d
aleksnoname <gitcommit@yandex.ru>
2024-04-20 18:27:50 +0300

    int

--- DDL/DDL.sql
+++ DDL/DDL.sql
@@ -22,6 +22,7 @@ CREATE TABLE IF NOT EXISTS values (
     value_date      date DEFAULT NULL,
     value_bool      bool DEFAULT NULL,
     value_float     decimal(11,2) DEFAULT NULL,
+    value_int       integer DEFAULT NULL,
     FOREIGN KEY (film_id) REFERENCES films(id),
     FOREIGN KEY (attribute_id) REFERENCES attributes(id)
 );
@@ -29,7 +30,7 @@ CREATE TABLE IF NOT EXISTS values (
 
 INSERT INTO films VALUES ('master_i_margarita','Мастер и Маргарита'),('onegin','Онегин'),('duna_2','Дюна 2');
 
-INSERT INTO attribute_types VALUES ('text'),('date'),('bool'),('float');
+INSERT INTO attribute_types VALUES ('text'),('date'),('bool'),('float'),('integer');
 
 INSERT INTO attributes VALUES ('reviews','text','Рецензии'),
                               ('award_oscar','bool','Премия Оскар'),
@@ -46,55 +47,55 @@ INSERT INTO attributes VALUES ('reviews','text','Рецензии'),
                               ('task_4','date','Запустить рекламу')
                               ;
 
-INSERT INTO values VALUES ('master_i_margarita','reviews','Рецензия 1',null,null,null),
-                          ('master_i_margarita','reviews','Рецензия 2',null,null,null),
-                          ('master_i_margarita','award_oscar','',null,true,null),
-                          ('master_i_margarita','award_nika','',null,false,null),
-                          ('master_i_margarita','world_premiere_date','','2024-04-30 12:00:00',null,null),
-                          ('master_i_margarita','ru_premiere_date','','2024-04-30 12:00:00',null,null),
-                          ('master_i_margarita','ticket_trade_begin','','2024-04-25 12:00:00',null,null),
-                          ('master_i_margarita','ticket_trade_end','','2024-05-15 12:00:00',null,null),
-                          ('master_i_margarita','seat_price','',null,null,500.00),
-                          ('master_i_margarita','seat_price_luxe','',null,null,600.00),
-                          ('master_i_margarita','task_1','',CURRENT_DATE,null,null),
-                          ('master_i_margarita','task_2','',CURRENT_DATE,null,null),
-                          ('master_i_margarita','task_3','',CURRENT_DATE + INTERVAL '20 days',null,null),
-                          ('master_i_margarita','task_4','',CURRENT_DATE + INTERVAL '20 days',null,null),
+INSERT INTO values VALUES ('master_i_margarita','reviews','Рецензия 1',null,null,null,null),
+                          ('master_i_margarita','reviews','Рецензия 2',null,null,null,null),
+                          ('master_i_margarita','award_oscar','',null,true,null,null),
+                          ('master_i_margarita','award_nika','',null,false,null,null),
+                          ('master_i_margarita','world_premiere_date','','2024-04-30 12:00:00',null,null,null),
+                          ('master_i_margarita','ru_premiere_date','','2024-04-30 12:00:00',null,null,null),
+                          ('master_i_margarita','ticket_trade_begin','','2024-04-25 12:00:00',null,null,null),
+                          ('master_i_margarita','ticket_trade_end','','2024-05-15 12:00:00',null,null,null),
+                          ('master_i_margarita','seat_price','',null,null,500.00,null),
+                          ('master_i_margarita','seat_price_luxe','',null,null,600.00,null),
+                          ('master_i_margarita','task_1','',CURRENT_DATE,null,null,null),
+                          ('master_i_margarita','task_2','',CURRENT_DATE,null,null,null),
+                          ('master_i_margarita','task_3','',CURRENT_DATE + INTERVAL '20 days',null,null,null),
+                          ('master_i_margarita','task_4','',CURRENT_DATE + INTERVAL '20 days',null,null,null),
 
-                          ('onegin','reviews','Рецензия 1',null,null,null),
-                          ('onegin','reviews','Рецензия 2',null,null,null),
-                          ('onegin','award_oscar','',null,true,null),
-                          ('onegin','award_nika','',null,true,null),
-                          ('onegin','world_premiere_date','','2024-04-29 12:00:00',null,null),
-                          ('onegin','ru_premiere_date','','2024-04-29 12:00:00',null,null),
-                          ('onegin','ticket_trade_begin','','2024-04-24 12:00:00',null,null),
-                          ('onegin','ticket_trade_end','','2024-05-15 12:00:00',null,null),
-                          ('onegin','seat_price','',null,null,550.00),
-                          ('onegin','seat_price_luxe','',null,null,650.00),
-                          ('onegin','task_1','',CURRENT_DATE,null,null),
-                          ('onegin','task_2','',CURRENT_DATE + INTERVAL '20 days',null,null),
-                          ('onegin','task_3','',CURRENT_DATE,null,null),
-                          ('onegin','task_4','',CURRENT_DATE + INTERVAL '20 days',null,null),
+                          ('onegin','reviews','Рецензия 1',null,null,null,null),
+                          ('onegin','reviews','Рецензия 2',null,null,null,null),
+                          ('onegin','award_oscar','',null,true,null,null),
+                          ('onegin','award_nika','',null,true,null,null),
+                          ('onegin','world_premiere_date','','2024-04-29 12:00:00',null,null,null),
+                          ('onegin','ru_premiere_date','','2024-04-29 12:00:00',null,null,null),
+                          ('onegin','ticket_trade_begin','','2024-04-24 12:00:00',null,null,null),
+                          ('onegin','ticket_trade_end','','2024-05-15 12:00:00',null,null,null),
+                          ('onegin','seat_price','',null,null,550.00,null),
+                          ('onegin','seat_price_luxe','',null,null,650.00,null),
+                          ('onegin','task_1','',CURRENT_DATE,null,null,null),
+                          ('onegin','task_2','',CURRENT_DATE + INTERVAL '20 days',null,null,null),
+                          ('onegin','task_3','',CURRENT_DATE,null,null,null),
+                          ('onegin','task_4','',CURRENT_DATE + INTERVAL '20 days',null,null,null),
 
-                          ('duna_2','reviews','Рецензия 1',null,null,null),
-                          ('duna_2','reviews','Рецензия 2',null,null,null),
-                          ('duna_2','award_oscar','',null,false,null),
-                          ('duna_2','award_nika','',null,false,null),
-                          ('duna_2','world_premiere_date','','2024-04-29 12:00:00',null,null),
-                          ('duna_2','ru_premiere_date','','2024-04-29 12:00:00',null,null),
-                          ('duna_2','ticket_trade_begin','','2024-04-24 12:00:00',null,null),
-                          ('duna_2','ticket_trade_end','','2024-05-15 12:00:00',null,null),
-                          ('duna_2','seat_price','',null,null,600.00),
-                          ('duna_2','seat_price_luxe','',null,null,700.00),
-                          ('duna_2','task_1','',CURRENT_DATE + INTERVAL '20 days',null,null),
-                          ('duna_2','task_2','',CURRENT_DATE + INTERVAL '20 days',null,null),
-                          ('duna_2','task_3','',CURRENT_DATE,null,null),
-                          ('duna_2','task_4','',CURRENT_DATE,null,null)
+                          ('duna_2','reviews','Рецензия 1',null,null,null,null),
+                          ('duna_2','reviews','Рецензия 2',null,null,null,null),
+                          ('duna_2','award_oscar','',null,false,null,null),
+                          ('duna_2','award_nika','',null,false,null,null),
+                          ('duna_2','world_premiere_date','','2024-04-29 12:00:00',null,null,null),
+                          ('duna_2','ru_premiere_date','','2024-04-29 12:00:00',null,null,null),
+                          ('duna_2','ticket_trade_begin','','2024-04-24 12:00:00',null,null,null),
+                          ('duna_2','ticket_trade_end','','2024-05-15 12:00:00',null,null,null),
+                          ('duna_2','seat_price','',null,null,600.00,null),
+                          ('duna_2','seat_price_luxe','',null,null,700.00,null),
+                          ('duna_2','task_1','',CURRENT_DATE + INTERVAL '20 days',null,null,null),
+                          ('duna_2','task_2','',CURRENT_DATE + INTERVAL '20 days',null,null,null),
+                          ('duna_2','task_3','',CURRENT_DATE,null,null,null),
+                          ('duna_2','task_4','',CURRENT_DATE,null,null,null)
                           ;
 
 
 CREATE INDEX IND_VALUES
-    ON values(film_id,attribute_id,value_bool,value_date,value_text,value_float);
+    ON values(film_id,attribute_id,value_bool,value_date,value_text,value_float,value_int);
 
 CREATE VIEW system_data AS
 SELECT f.name Фильм,
