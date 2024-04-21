8d70bb24bc293e1b0ebd47ada973555f264b3c10
Pavel <pavelsergmgtu@gmail.com>
2024-04-21 15:12:05 +0500

    максимально упÑкороченное решение без ООП и проверок

--- solution.php
+++ solution.php
@@ -1,5 +1,4 @@
 <?php
-
 class ListNode {
     public $val = 0;
     public $next = null;
@@ -7,58 +6,34 @@ class ListNode {
         $this->val = $val;
         $this->next = $next;
     }
-
-    /**
-     * Добавляет следующую ноду по её значению и возвращает её (для цепочного вызова)
-     * @note Добавлено для быстрого заполнения тестовыми значениями, в реализуемом методе mergeTwoLists не используется
-     * @param int $val
-     * @return self
-     */
-    function addNextByValue(int $val = 0): self
-    {
-        $nextNode = new self($val);
-        $this->next = $nextNode;
-        return $this->next;
-    }
-
-    /**
-     * Добавляет следующие ноды по значениям из массива
-     * @note Добавлено для быстрого заполнения тестовыми значениями, в реализуемом методе mergeTwoLists не используется
-     * @param array $values
-     * @return $this
-     */
-    function addNextByValuesArray(array $values): self
-    {
-        $currentNode = $this;
-        foreach ($values as $value) {
-            $currentNode = $currentNode->addNextByValue($value);
-        }
-        return $this;
-    }
 }
 
-class Solution
-{
-    /**
-     * Слить два ListNode по возрастанию
-     * @param ListNode|null $list1 отсортированный по val список
-     * @param ListNode|null $list2 отсортированный по val список
-     * @return ListNode|null
-     * @throws Exception
-     */
-    function mergeTwoLists(?ListNode $list1 = null, ?ListNode $list2 = null): ?ListNode
+class Solution {
+    function mergeTwoLists($list1, $list2)
     {
         if (!$list2) return $list1;
         if (!$list1) return $list2;
+        if ($list1->val < $list2->val) {
+            $mergedList = new ListNode($list1->val);
+            $list1 = $list1->next;
+        } else {
+            $mergedList = new ListNode($list2->val);
+            $list2 = $list2->next;
+        }
 
-        $mergedList = new ListNode($this->getMinValueAndShiftList($list1, $list2));
         $lastNode = $mergedList;
         while (!is_null($list1) && !is_null($list2)) {
-            $nextValue = $this->getMinValueAndShiftList($list1, $list2);
-            if ($nextValue < $lastNode->val) throw new Exception('Переданы не отсортированные по возрастанию списки');
+            if ($list1->val < $list2->val) {
+                $nextValue = $list1->val;
+                $list1 = $list1->next;
+            } else {
+                $nextValue = $list2->val;
+                $list2 = $list2->next;
+            }
+
             $lastNode->next = new ListNode($nextValue);
             $lastNode = $lastNode->next;
-        };
+        }
 
         if ($list1) {
             $lastNode->next = $list1;
@@ -68,38 +43,14 @@ class Solution
 
         return $mergedList;
     }
-
-    /**
-     * Получить минимальное значение из двух нод и сдвинуть эту ноду на next
-     * @param ListNode $list1
-     * @param ListNode $list2
-     * @return int|null
-     */
-    private function getMinValueAndShiftList(ListNode &$list1, ListNode &$list2): ?int
-    {
-        if ($list1->val < $list2->val) {
-            $result = $list1->val;
-            $list1 = $list1->next;
-        } else {
-            $result = $list2->val;
-            $list2 = $list2->next;
-        }
-        return $result;
-    }
 }
 
 
-$solution = new Solution();
+//test:
+$firstList = new ListNode(1, new ListNode(2, new ListNode(4)));
+$secondList = new ListNode(1, new ListNode(3, new ListNode(4)));
 
-$listFirst = new ListNode(2);
-$listFirst->addNextByValue(3)->addNextByValue(4)->addNextByValue(22)->addNextByValue(44)->addNextByValue(77)->addNextByValue(88)->addNextByValue(89);
-$listSecond = new ListNode(1);
-$listSecond->addNextByValue(8)->addNextByValue(10)->addNextByValue(27)->addNextByValue(50)->addNextByValue(88);
-var_dump($solution->mergeTwoLists($listFirst, $listSecond));
+$solutionObj = new Solution();
+$answer = $solutionObj->mergeTwoLists($firstList, $secondList);
 
-
-$listThird = new ListNode(1);
-$listThird->addNextByValuesArray([2,4,5,6,9,14,16,18,34,54,67,67,88,89,93]);
-$listFourth = new ListNode(2);
-$listFourth->addNextByValuesArray([3,4,5,7,9,10,14,14,16,18,33,34,35,36,37,37,37,37,54,67,88,89,93,94]);
-var_dump($solution->mergeTwoLists($listThird, $listFourth));
+var_dump($answer);
