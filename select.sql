7098fd506f1f15e26c3aed017be6014211d36491
AEVKISELEV <imbatvink@gmail.com>
2024-04-21 17:00:04 +0200

    hw9: homework-9

--- select.sql
+++ select.sql
@@ -1,12 +1,3 @@
-SELECT id, title, sum_with_discount, sum_without_discount FROM (
-    SELECT m.id as id, m.title, sum(t.actual_price) as sum_with_discount, sum(pp.price) as sum_without_discount FROM tickets as t
-    INNER JOIN price_place as pp
-    ON t.price_place_id = pp.id
-    INNER JOIN movie_session as ms
-    ON pp.movie_session_id = ms.id
-    INNER JOIN movie as m
-    ON ms.movie_id = m.id
-    GROUP BY m.id
-) AS aggregated_prices
-ORDER BY sum_with_discount DESC
-LIMIT 1;
+SELECT * FROM all_attributes;
+SELECT * FROM important_events_20_days;
+SELECT * FROM important_events_now;
