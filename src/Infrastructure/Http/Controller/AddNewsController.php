a0be5985c47880c2ac00b7d9593d15a502ebca60
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-04-23 11:33:40 +0300

    ALogachev/hw21 Added attributes to AddNewsController.

--- src/Infrastructure/Http/Controller/AddNewsController.php
+++ src/Infrastructure/Http/Controller/AddNewsController.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Http\Controller;
 
+use App\Application\UseCase\AddNewsUseCase;
 use App\Application\UseCase\Request\AddNewsRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
 
+#[Route(path: '/news/create', name: 'news_create', methods: 'POST')]
 class AddNewsController extends  AbstractController
 {
-    public function __invoke(AddNewsRequest $request)
-    {
-        // TODO: Implement __invoke() method.
+    public function __invoke(
+       #[MapRequestPayload] AddNewsRequest $request,
+        AddNewsUseCase $addNewsUseCase,
+    ): Response {
+        call_user_func_array($addNewsUseCase, [$request]);
+
+        return $this->json(['success' => true]);
     }
 }
