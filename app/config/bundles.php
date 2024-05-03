a522a7071d410af5c861a3ee1e4261a55b7f146f
LRazumov <masdzen@gmail.com>
2024-05-03 20:56:05 +0000

    add elastica

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
