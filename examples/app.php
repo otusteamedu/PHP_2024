fd2f1c995140fbc874414e89209e88aed11611fb
Dmitry Smolyaninov <smolyaninovda@gmail.com>
2024-05-05 18:41:48 +0700

    HW6

---
+++ examples/app.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use Dsmolyaninov\Hw6\EmailVerifier;
+
+$verifier = new EmailVerifier();
+$emails = ["example@gmail.com", "invalid-email", "test@example.ru"];
+$results = $verifier->verifyEmails($emails);
+var_dump($results);
