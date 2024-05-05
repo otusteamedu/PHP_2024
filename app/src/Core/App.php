85e29a70bdffe0f4fe2d5f6cc40cfcac839de620
Dmitry Smolyaninov <smolyaninovda@gmail.com>
2024-05-05 16:30:07 +0700

    HW5

--- app/src/Core/App.php
+++ app/src/Core/App.php
@@ -11,29 +11,41 @@ use Exception;
 class App
 {
     /**
-     *
      * @param string[] $argv
      * @throws Exception
      */
     public function run(int $argc, array $argv): void
     {
-        if ($argc > 1) {
-            $command = $argv[1];
-
-            switch ($command) {
-                case 'server':
-                    $server = new Server();
-                    $server->run();
-                    break;
-                case 'client':
-                    $client = new Client();
-                    $client->run();
-                    break;
-                default:
-                    throw new Exception("Неизвестная команда: $command");
-            }
-        } else {
+        if ($argc <= 1) {
             throw new Exception('Аргумент не был предоставлен.');
+        }
+
+        $command = $argv[1];
+        switch ($command) {
+            case 'server':
+                $this->handleServer();
+                break;
+            case 'client':
+                $this->handleClient();
+                break;
+            default:
+                throw new Exception("Неизвестная команда: $command");
+        }
+    }
+
+    private function handleServer(): void
+    {
+        $server = new Server();
+        foreach ($server->run() as $message) {
+            echo $message;
+        }
+    }
+
+    private function handleClient(): void
+    {
+        $client = new Client();
+        foreach ($client->run() as $message) {
+            echo $message;
         }
     }
 }
--- app/src/Network/Server.php
+++ app/src/Network/Server.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dsmolyaninov\Hw5\Network;
 
+use Generator;
+use RuntimeException;
 use Socket;
 
 class Server
@@ -15,70 +17,82 @@ class Server
         $this->socketHandler = new SocketHandler();
     }
 
-    public function run(): void
+    /**
+     * @return Generator<string>
+     */
+    public function run(): Generator
     {
         $socketPath = $this->socketHandler->getSocketPath();
         self::removeExistingSocket($socketPath);
 
         $serverSocket = $this->socketHandler->createSocket();
         if (!$serverSocket) {
-            throw new \RuntimeException("Ошибка при создании сокета: " . socket_strerror(socket_last_error()));
+            throw new RuntimeException("Ошибка при создании сокета: " . socket_strerror(socket_last_error()));
         }
 
         if (!$this->socketHandler->bindSocket($serverSocket, $socketPath)) {
-            throw new \RuntimeException("Ошибка при привязке сокета к адресу: " . socket_strerror(socket_last_error()));
+            throw new RuntimeException("Ошибка при привязке сокета к адресу: " . socket_strerror(socket_last_error()));
         }
 
         if (!$this->socketHandler->listenSocket($serverSocket)) {
-            throw new \RuntimeException(
+            throw new RuntimeException(
                 "Ошибка при начале прослушивания сокета: " . socket_strerror(socket_last_error())
             );
         }
 
-        echo "Сервер запущен и ожидает соединений..." . PHP_EOL;
-        $this->handleConnections($serverSocket);
+        yield "Сервер запущен и ожидает соединений..." . PHP_EOL;
+        yield from $this->handleConnections($serverSocket);
     }
 
-    private function handleConnections(Socket $serverSocket): void
+    /**
+     * @return Generator<string>
+     */
+    private function handleConnections(Socket $serverSocket): Generator
     {
         while (true) {
             $clientSocket = $this->socketHandler->acceptSocket($serverSocket);
             if (!$clientSocket) {
-                throw new \RuntimeException("Ошибка при принятии соединения: " . socket_strerror(socket_last_error()));
+                throw new RuntimeException("Ошибка при принятии соединения: " . socket_strerror(socket_last_error()));
             }
-            $this->processClient($clientSocket);
+            yield from $this->processClient($clientSocket);
         }
     }
 
-    private function processClient(Socket $clientSocket): void
+    /**
+     * @return Generator<string>
+     */
+    private function processClient(Socket $clientSocket): Generator
     {
         $pid = pcntl_fork();
         if ($pid == -1) {
-            throw new \RuntimeException("Ошибка при создании потока (процесса)");
+            throw new RuntimeException("Ошибка при создании потока (процесса)");
         } elseif ($pid === 0) {
-            $this->handleClient($clientSocket);
+            yield from $this->handleClient($clientSocket);
         } else {
             $this->socketHandler->closeSocket($clientSocket);
         }
     }
 
-    private function handleClient(Socket $clientSocket): void
+    /**
+     * @return Generator<string>
+     */
+    private function handleClient(Socket $clientSocket): Generator
     {
-        echo "Клиент подключен." . PHP_EOL;
+        yield "Клиент подключен." . PHP_EOL;
         while (true) {
             $input = socket_read($clientSocket, 1024);
             if ($input === false) {
-                throw new \RuntimeException("Ошибка при чтении данных: " . socket_strerror(socket_last_error()));
+                throw new RuntimeException("Ошибка при чтении данных: " . socket_strerror(socket_last_error()));
             }
             if (trim($input) === 'exit') {
-                echo "Клиент запросил закрытие соединения." . PHP_EOL;
+                yield "Клиент запросил закрытие соединения." . PHP_EOL;
                 $this->socketHandler->closeSocket($clientSocket);
-                exit();
+                return;
             }
-            echo "Получено от клиента: " . trim($input) . PHP_EOL;
+            yield "Получено от клиента: " . trim($input) . PHP_EOL;
             $confirmation = "Received " . strlen($input) . " bytes";
             if (!socket_write($clientSocket, $confirmation, strlen($confirmation))) {
-                throw new \RuntimeException(
+                throw new RuntimeException(
                     "Ошибка при отправке данных клиенту: " . socket_strerror(socket_last_error())
                 );
             }
