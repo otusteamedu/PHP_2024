33302c03aad1f9778cbf7c73ebaa04515773db0f
Dmitry Smolyaninov <smolyaninovda@gmail.com>
2024-05-06 13:41:42 +0700

    HW6

--- examples/app.php
+++ examples/app.php
@@ -9,4 +9,3 @@ use Dsmolyaninov\Hw6\EmailVerifier;
 $verifier = new EmailVerifier();
 $emails = ["example@gmail.com", "invalid-email", "test@example.ru"];
 $results = $verifier->verifyEmails($emails);
-var_dump($results);
