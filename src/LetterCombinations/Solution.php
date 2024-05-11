aa3e1c44ca59763ed303c1cda713d23174a4d802
Alexander <alexandermail1@yandex.ru>
2024-05-11 23:55:31 +0300

    Homework 14

---
+++ src/LetterCombinations/Solution.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AlexanderGladkov\LetterCombinations;
+
+class Solution
+{
+    private array $digitsToLetters = [
+        '2' => ['a', 'b', 'c'],
+        '3' => ['d', 'e', 'f'],
+        '4' => ['g', 'h', 'i'],
+        '5' => ['j', 'k', 'l'],
+        '6' => ['m', 'n', 'o'],
+        '7' => ['p', 'q', 'r', 's'],
+        '8' => ['t', 'u', 'v'],
+        '9' => ['w', 'x', 'y', 'z'],
+    ];
+
+    private array $output = [];
+    private int $outputIndex = 0;
+    private array $digits  = [];
+
+    /**
+     * @param string $digits
+     * @return string[]
+     */
+    function letterCombinations(string $digits)
+    {
+        if ($digits === '') {
+            return [];
+        }
+
+        $this->digits = str_split($digits);
+        $this->processDigit('', 0);
+        return $this->output;
+    }
+
+    function processDigit(string $combination, int $digitIndex)
+    {
+        $digit = $this->digits[$digitIndex];
+        $letters = $this->digitsToLetters[$digit];
+        foreach ($letters as $letter) {
+            if ($digitIndex < count($this->digits) - 1) {
+                $this->processDigit($combination . $letter, $digitIndex +1);
+            } else {
+                $this->output[$this->outputIndex++] = $combination . $letter;
+            }
+        }
+    }
+}
---
+++ src/LinkedListCycle/ListNode.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlexanderGladkov\LinkedListCycle;
+
+class ListNode
+{
+    public int $val = 0;
+    public ?ListNode $next = null;
+
+    public function __construct(int $val = 0, ?ListNode $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
