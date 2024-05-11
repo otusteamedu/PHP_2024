4e2d77689afbacf49ea488d9b0ded1dab1aa62ff
uvzhigaryova <juliazhigaryova@yandex.ru>
2024-05-11 17:22:43 +0400

    HW #8, version1.0

---
+++ src/Solution.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JuliaZhigareva\OtusComposerPackage;
+
+/**
+ * Definition for a singly-linked list.
+ *
+ *
+ *
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ *
+ * Временная сложность: O(n)
+ * Алгоритм выполняет итерацию по каждому узлу в обоих входных списках по одному разу,
+ * что делает его линейным. Следовательно, временная сложность равна O(n),
+ * где n - общее количество узлов во входных списках.
+ */
+class Solution
+{
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2)
+    {
+        $head = new ListNode();
+        $current = $head;
+        while ($list1 !== null and $list2 !== null) {
+            if ($list1->val < $list2->val) {
+                $current->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $current->next = $list2;
+                $list2 = $list2->next;
+            }
+
+            $current = $current->next;
+        }
+
+
+        if ($list1 !== null) {
+            $current->next = $list1;
+        } elseif ($list2 !== null) {
+            $current->next = $list2;
+        }
+
+        return $head->next;
+    }
+}
