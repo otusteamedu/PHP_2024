2b635d325bc15e9d3604cb1f7ed82b35362830b1
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-05-22 11:32:49 +0300

    ALogachev/hw24 Continue fighting with Doctrine collections.

--- src/Infrastructure/Database/EventListener/CollectionSubstitutionEventListener.php
+++ src/Infrastructure/Database/EventListener/CollectionSubstitutionEventListener.php
@@ -5,40 +5,63 @@ declare(strict_types=1);
 namespace App\Infrastructure\Database\EventListener;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Doctrine\ORM\Event\PreFlushEventArgs;
 use ReflectionClass;
+use ReflectionException;
+use ReflectionProperty;
 
-class PreFlushEventListener
+class CollectionSubstitutionEventListener
 {
+    private array $domainCollections = [];
+
     public function preFlush(PreFlushEventArgs $args): void
     {
-//        dd('here');
         $entityManager = $args->getObjectManager();
         $unitOfWork = $entityManager->getUnitOfWork();
         $entities = $unitOfWork->getScheduledEntityInsertions();
         $entities = array_merge($entities, $unitOfWork->getScheduledEntityUpdates());
 
         foreach ($entities as $entity) {
-            $this->substituteCollections($entity);
+            $this->substituteToDoctrineCollections($entity);
         }
-//        dd('FINISH');
     }
 
-    private function substituteCollections(object $entity): void
+    /**
+     * @throws ReflectionException
+     */
+    public function postFlush(PostFlushEventArgs $args): void
+    {
+        $this->substituteToInitialCollections();
+    }
+
+    private function substituteToDoctrineCollections(object $entity): void
     {
         $reflectionClass = new ReflectionClass($entity);
         foreach ($reflectionClass->getProperties() as $property) {
             $value = $property->getValue($entity);
-//            dump($this->isCustomCollection($value));
             if ($this->isCustomCollection($value)) {
-//                dump($value);
+                $this->domainCollections[] = [$entity, $property->getName(), $value];
                 $property->setValue(
                     $entity,
                     new ArrayCollection(is_array($value) ? $value : $value->toArray())
                 );
             }
         }
-//        dump($entity);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function substituteToInitialCollections(): void
+    {
+        foreach ($this->domainCollections as [$entity, $property, $originalCollection]) {
+            $reflectionProperty = new ReflectionProperty($entity, $property);
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue($entity, $originalCollection);
+        }
+
+        $this->domainCollections = [];
     }
 
     private function isCustomCollection(mixed $value): bool
