ee01d0d4e9c3f8a56e3498353d7dce549b656f3e
VictoriaBabikova <babikovav8@gmail.com>
2024-05-29 16:15:31 +0300

    fix2

--- task2/src/Solution.php
+++ task2/src/Solution.php
@@ -11,16 +11,18 @@ class Solution
      * @param Integer $denominator
      * @return String
      */
-    function fractionToDecimal(int $numerator, int $denominator): string
+    public function fractionToDecimal(int $numerator, int $denominator): string
     {
         $array = array();
         $modulo = $numerator % $denominator;
         $counter = 1;
-        while($modulo != 0) {
-            if ($array[$modulo-1] != 0) break;
-            $array[$modulo-1] = $counter;
+        while ($modulo != 0) {
+            if ($array[$modulo - 1] != 0) {
+                break;
+            }
+            $array[$modulo - 1] = $counter;
             $modulo *= 10;
-            $number  = (number_format($modulo/$denominator,100));
+            $number  = (number_format($modulo / $denominator, 100));
             $number = rtrim($number, '0');
             $explode = explode('.', $number);
             $period .= abs((int)$explode[0]);
@@ -28,19 +30,19 @@ class Solution
             $counter++;
         }
         if ($modulo != 0) {
-            $number  = ($numerator/$denominator);
+            $number  = ($numerator / $denominator);
             if (str_contains((string)$number, '-')) {
-                $number  = (number_format($modulo/$denominator,100));
+                $number  = (number_format($modulo / $denominator, 100));
                 $number = rtrim($number, '0');
                 $part = explode('.', $number);
             } else {
                 $part = explode('.', (string)$number);
             }
-            return $part[0] .'.'. substr($period, 0, $array[$modulo-1]-1) . '(' . substr($period, $array[$modulo-1]-1) .')';
+            return $part[0] .'.'. substr($period, 0, $array[$modulo - 1] - 1) . '(' . substr($period, $array[$modulo - 1] - 1) .')';
         } else {
-            $number = $numerator/$denominator;
+            $number = $numerator / $denominator;
             if (str_contains((string)$number, '-')) {
-                $number  = (number_format($numerator/$denominator,100));
+                $number  = (number_format($numerator / $denominator, 100));
                 $number = rtrim($number, '.0');
                 $number = preg_replace('/,/', '', $number);
                 return ($number);
