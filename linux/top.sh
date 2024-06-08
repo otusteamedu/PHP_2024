5652c0a489ff54752f0903375efa6860a118246c
Yuri Serikov <ys@inmoloko.ru>
2024-06-09 00:43:14 +0500

    hw2: top script fix

--- linux/top.sh
+++ linux/top.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -
 
-if [ $# = 1 ]
+if [ $# = 1 ] & [ -e $1 ]
 then
-	awk '{print $3}' table.txt | sort | uniq -c | sort -r | head -3 | awk '{print $2}'
+	awk '{print $3}' $1 | sort | uniq -c | sort -r | head -3 | awk '{print $2}'
 else
 	echo "ERROR: Invalid arguments"
 fi
