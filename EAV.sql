4fb762ecb22f966d6978a98f92934493f3511280
Sergei Dudyuk <dev_ds@mainfin.ru>
2024-06-09 13:08:27 +0300

    fix HW9

--- EAV.sql
+++ EAV.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS `films`;
 
 CREATE TABLE `films`
 (
-    `id`          int          NOT NULL AUTO_INCREMENT,
-    `title`       text NOT NULL,
-    `description` text,
+    `id`    SERIAL PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -14,34 +14,30 @@ DROP TABLE IF EXISTS `type_attr`;
 
 CREATE TABLE `type_attr`
 (
-    `id`   int         NOT NULL AUTO_INCREMENT,
-    `type` varchar(11) NOT NULL,
+    `id`  SERIAL PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL
     PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS `name_attrs`;
+DROP TABLE IF EXISTS `attrs`;
 
-CREATE TABLE `name_attrs`
+CREATE TABLE `attrs`
 (
-    `id`                int         NOT NULL AUTO_INCREMENT,
-    `name`              text NOT NULL,
-    `attr_id` int         NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `index_attr_id` (`attr_id`),
-    CONSTRAINT `foreign_key_attr_id` FOREIGN KEY (`attr_id`) REFERENCES `type_attr` (`id`)
+    `id`      SERIAL PRIMARY KEY,
+    `name`    VARCHAR(100) NOT NULL,
+    `type_id` INT  NOT NULL REFERENCES type_attr (id)
 );
 
 DROP TABLE IF EXISTS `film_attr_values`;
 
 CREATE TABLE `film_attr_values`
 (
-    `id`           int         NOT NULL AUTO_INCREMENT,
-    `value`        text NOT NULL,
-    `attr_id` int         NOT NULL,
-    `film_id`    int         NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `index_film_id` (`film_id`),
-    KEY `index_attr_id` (`attr_id`),
-    CONSTRAINT `foreign_key_film_id` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`),
-    CONSTRAINT `foreign_key_attr_id` FOREIGN KEY (`attr_id`) REFERENCES `type_attr` (`id`)
+    `film_id`     INT NOT NULL REFERENCES films (id),
+    `attr_id` INT NOT NULL REFERENCES attrs (id),
+    `value_int`    INTEGER,
+    `value_text`   TEXT,
+    `value_date`   DATE,
+    `value_number` NUMERIC,
+    `value_bool`   BOOLEAN,
+    PRIMARY KEY (film_id, attr_id)
 );
--- VIEW.sql
+++ VIEW.sql
@@ -1,23 +1,25 @@
 DROP VIEW IF EXISTS `premier_list`;
-CREATE VIEW `premier_list` (`film_name`, `is_actual_today`, `is_actual_after_20_days`)
+CREATE VIEW `premier_list`
 AS
-SELECT `movie`.`title`                                                      AS `film_name`,
-       `attr_value`.`value`                                                     AS `day_premier`
-       IF((`day_premier` <= curdate()), 'Да', 'Нет')                     AS `is_actual_today`,
-       IF((`day_premier` >= (curdate() + INTERVAL 20 DAY)), 'Да', 'Нет') AS `is_actual_after_20_days`,
-FROM ((`films` `movie`
-    JOIN `film_attr_values` `attr_value` ON ((`movie`.`id` = `attr_value`.`film_id`)))
-    JOIN `type_attr` `attr` ON ((`attr_value`.`attr_id` = `attr`.`id`)))
-WHERE (`attr`.`name` = 'day_premier');
+SELECT f.title                       as film_title,
+       a.name                        as attr_name,
+       COALESCE(fav.value_int::TEXT, fav.value_text, fav.value_date::TEXT, fav.value_number::TEXT,
+                fav.value_bool::TEXT) as value
+FROM films f
+         JOIN film_attr_values fav ON m.id = av.movie_id
+         JOIN attrs a ON fav.attribute_id = a.id
+WHERE a.name = 'task_date'
+  AND (fav.value_date = CURRENT_DATE OR fav.value_date = CURRENT_DATE + INTERVAL '20 day');
 
 DROP VIEW IF EXISTS `analytics`;
-CREATE VIEW `analytics` (`film_name`, `type`, `name`, `value`)
+CREATE VIEW `analytics`
 AS
-SELECT `movie`.`title`  AS `film_name`,
-       `attr`.`type`  AS `type`,
-       `attr_name`.`name`   AS `name`,
-       `attr_value`.`value` AS `value`
-FROM (((`film_entity` `movie`
-    JOIN `film_attr_values` `attr_value` ON ((`movie`.`id` = `attr_value`.`film_id`)))
-    JOIN `type_attr` `attr` ON ((`attr_value`.`attr_id` = `attr`.`id`)))
-    JOIN `name_attr` `attr_name` ON ((`attr`.`id` = `attr_name`.`attr_id`)));
+SELECT f.title                       as film_title,
+       at.name                       as attr_type,
+       a.name                        as attr_name,
+       COALESCE(fav.value_int::TEXT, fav.value_text, fav.value_date::TEXT, fav.value_number::TEXT,
+                fav.value_bool::TEXT) as value
+FROM films f
+         JOIN film_attr_values fav ON f.id = fav.movie_id
+         JOIN attrs a ON fav.attribute_id = a.id
+         JOIN type_attr ta ON a.type_id = ta.id;
