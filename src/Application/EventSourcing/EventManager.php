cd665ac484e143063c2a153f9da1f6d957fb3884
Alexey Logachev <aleksei.logachev@coolclever.ru>
2024-06-10 17:52:40 +0300

    ALogachev/hw24 Started Observer pattern.

---
+++ src/Application/EventSourcing/EventManager.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\EventSourcing;
+
+class EventManager implements IEventPublisher
+{
+    private array $listeners = [];
+
+    public function subscribe(string $eventType, IEventListener $eventListener): void
+    {
+        $this->listeners[$eventType][] = $eventListener;
+    }
+
+    public function unSubscribe(string $eventType, IEventListener $eventListener): void
+    {
+        if (!isset($this->listeners[$eventType])) {
+            return;
+        }
+
+        $this->listeners[$eventType] = array_filter($this->listeners[$eventType], fn($obs) => $obs !== $eventListener);
+    }
+
+    public function notify(object $event): void
+    {
+        $eventType = $event::class;
+        if (!isset($this->listeners[$eventType])) {
+            return;
+        }
+
+        foreach ($this->listeners[$eventType] as $listener) {
+            $listener->update($event);
+        }
+    }
+}
---
+++ src/Application/EventSourcing/IEventListener.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\EventSourcing;
+
+interface IEventListener
+{
+    public function execute(object $event): void;
+}
