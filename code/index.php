44fb07c48df37b62119043f40511f7613a94a68c
kyberlox <64195832+kyberlox@users.noreply.github.com>
2024-06-11 23:31:43 +0400

    Update index.php

--- code/index.php
+++ code/index.php
@@ -2,8 +2,6 @@
 
 echo "Привет, Otus!!!!<br>".date("Y-m-d H:i:s") ."<br><br>";
 
-
-
 $redis = new Redis();
 $redis->connect('redis', '6379');
 $redis->auth('qwerty');
@@ -13,16 +11,12 @@ if ($redis->ping()) {
 }
 echo $redis->get("redis") . '<br><br>';
 
-
-
 $mem = new Memcached();
 $mem->addServer('memcached', 11211);
 $mem->set('int', 99);
 var_dump($mem->get('int')  . '<br>');
 $mem->set('string', '<br> Memcached is WORKING!');
 var_dump($mem->get('string') . '<br>');
-
-
 
 $connect_data = sprintf(
     "pgsql:host=%s;port=%s;dbname=%s;user=%s;password=%s",
@@ -35,7 +29,4 @@ $connect_data = sprintf(
 $dbh = new PDO($connect_data);
 echo "<br><br> PostgeSQL is WORKING! <br>";
 
-
-
 echo "<br> by Kyberlox <br>";
-?>
