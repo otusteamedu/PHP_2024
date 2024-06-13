bd1e722dbceba2704da21b0c1680ca171c3bb952
aleksnoname <gitcommit@yandex.ru>
2024-06-13 20:57:19 +0300

    1

---
+++ code/src/Infrastructure/Adapter/PizzaAdapter.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure\Adapter;
+
+use App\Application\Interface\StrategyInterface;
+
+class PizzaAdapter implements StrategyInterface
+{
+
+    public function getType(): string
+    {
+        // TODO: Implement getType() method.
+    }
+}
