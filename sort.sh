759a373b518cdb35a9036bd3cd181a8190555546
PIvanov <ivanov.p@cmd.su>
2024-06-14 10:53:10 +0300

    hw2 Скрипт sort.sh переписан в более компактной форме. Скрипт sum.sh - добавлена очистка входных аргументов от знака +, изменено регулярное выражение проверки входных аргументов.

--- sort.sh
+++ sort.sh
@@ -9,23 +9,5 @@ if [ ! -e "$file" ]; then
     exit 1
 fi
 
-# Чтение данных из файла data.txt
-data=$(cat $file)
-
-# Удаление строки заголовков, пустых строк и строк, начинающихся с пробела
-data=$(echo "$data" | grep -Ev '^([[:space:]]|[[:alpha:]])+')
-
-# Проверка наличия данных
-if [ -z "$data" ]; then
-    echo "В файле отсутствуют необходимые данные!"
-    exit 1
-fi
-
-# Подсчет количества вхождений каждого города
-uniq_cities=$(echo "$data" | awk '{print $3}' | sort | uniq -c)
-
-# Сортировка по количеству вхождений в порядке убывания
-sorted_cities=$(echo "$uniq_cities" | sort -nr)
-
-# Вывод первых трех строк отсортированного списка
-head -n 3 <(echo "$sorted_cities") | awk '{print $2}'
+# Вывод трёх наиболее популярных города среди пользователей системы
+cat "$file" | grep -Ev '^([[:space:]]|[[:alpha:]])+' |  awk '{print $3}' | sort | uniq -c | sort -nr | head -n 3 | awk '{print $2}'
