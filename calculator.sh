1c540e90597bf1509bb0d3dce3ae99b0fc4ecf05
stasya0903 <stasya0903@mail.ru>
2024-06-15 11:27:45 -0600

    hw2 fixed bug

--- calculator.sh
+++ calculator.sh
@@ -2,13 +2,13 @@
 
 first_num=$1
 second_num=$2
-re_number='^[-]?[0-9]+([.][0-9]+)?$'
+re_number='^[+-]?[0-9]+([.][0-9]+)?$'
 
 if ! [[ $first_num =~ $re_number && $second_num =~ $re_number ]];
 then
   echo 'Введите два числа'; exit 1;
 fi
 
-sum=$(awk "BEGIN {print $first_num+$second_num; exit}")
+sum=$(awk "BEGIN {print $first_num + $second_num; exit}")
 
 echo "Сумма чисел $first_num и $second_num равна $sum"
