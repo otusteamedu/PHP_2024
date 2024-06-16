600a552d758a640c4cde69ee5c43f3a8256a8042
Alexander <alexandermail1@yandex.ru>
2024-06-16 20:54:30 +0300

    Test data moved from Application class

--- src/EmailValidation/Application.php
+++ src/EmailValidation/Application.php
@@ -6,24 +6,16 @@ namespace AlexanderGladkov\EmailValidation;
 
 class Application
 {
-    public function run(): void
+    public function run(array $emails): array
     {
-        $emails = [
-            'test@gmail.com',
-            'test1@mail.ru',
-            'test@yandex.ru',
-            'test@gmail.org',
-            'test-gmail.com',
-            'test.test.test@gmail.com',
-            'test.test.test@mail.ru',
-            'test@gmial',
-        ];
-
         $emailValidator = new EmailValidator();
+        $errors = [];
         foreach ($emails as $email) {
             if (!$emailValidator->validate($email)) {
-                echo "$email - невалидный email<br>" . PHP_EOL;
+                $errors [] = "$email - невалидный email<br>";
             }
         }
+
+        return $errors;
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -7,7 +7,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use AlexanderGladkov\EmailValidation\Application;
 
 try {
-    (new Application())->run();
+    $testEmails = [
+        'test@gmail.com',
+        'test1@mail.ru',
+        'test@yandex.ru',
+        'test@gmail.org',
+        'test-gmail.com',
+        'test.test.test@gmail.com',
+        'test.test.test@mail.ru',
+        'test@gmial',
+    ];
+
+    $errors = (new Application())->run($testEmails);
+    foreach ($errors as $error) {
+        echo $error . PHP_EOL;
+    }
 } catch (Throwable $e) {
     echo $e->getMessage() . PHP_EOL;
 }
