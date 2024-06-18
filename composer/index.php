5c4194bfcb278b76cc5e3745898c397c1b2b024e
Evgeniy Smirnov <evgenysmrnv@gmail.com>
2024-06-18 20:37:36 +0300

    homework3

---
+++ composer/index.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+require __DIR__ . '/vendor/autoload.php';
+
+use \Evgenysmrnv\Security\SecurityPatch;
+
+$filename = 'filename example.txt';
+$filename = \Evgenysmrnv\Security\SecurityPatch::checkHttpHeader($filename);
+echo $httpHeaderExample = 'Content-Disposition: attachment; filename="' . $filename . '"';
