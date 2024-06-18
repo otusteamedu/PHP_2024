0a5acbb5c02ad4cee32549ec3a2905d98192b696
kyberlox <64195832+kyberlox@users.noreply.github.com>
2024-06-18 12:04:12 +0400

    Добавил обработку для случая пустой строки top3.sh
    Это должно не только исправить то исключение, но ещё и случаи, где файл пустой или когда пустые строки встречаются чаще.

--- top3.sh
+++ top3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '{NR>1 && count[$3]++} END {for (word in count) print word, count[word]}' ./table.txt | sort -rk 2 | head -n 3 | awk '{print $1}'
+awk '{NR>1 && count[$3]++} END {for (word in count) if (word != "") print word, count[word]}' ./table.txt | sort -rk 2 | head -n 3 | awk '{print $1}'
