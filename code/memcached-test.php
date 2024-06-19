a6efb56b5781671b1beb27b3465b8211389f0a42
Pautinich <pautinich@protonmail.com>
2024-06-19 22:52:24 +0300

    Add memcached to build

---
+++ code/memcached-test.php
@@ -0,0 +1,33 @@
+<?php
+
+$memcached = new Memcached();
+$memcached->addServer('memcached', 11211);
+
+// Проверка состояния сервера
+$status = $memcached->getStats();
+if ($status) {
+    echo "Connected to Memcached server.\n";
+} else {
+    echo "Failed to connect to Memcached server.\n";
+}
+
+// Установка значения в кэш
+$setResult = $memcached->set('key', 'Hello, Memcached!', 60); // 60 секунд
+
+if ($setResult) {
+    echo "Value successfully set in cache.\n";
+} else {
+    echo "Failed to set value in cache.\n";
+}
+
+// Получение значения из кэша
+$value = $memcached->get('key');
+if ($value) {
+    echo $value; // Выводит "Hello, Memcached!"
+} else {
+    echo "Нет данных в кэше.\n";
+    $resultCode = $memcached->getResultCode();
+    echo "Error code: " . $resultCode . "\n";
+}
+
+
