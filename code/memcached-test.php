5f047d131654c8fe3ad9f8b891114e2990e0052f
Pautinich <pautinich@protonmail.com>
2024-06-19 22:58:43 +0300

    Add memcached to build & fix PSR-error

--- code/memcached-test.php
+++ code/memcached-test.php
@@ -6,18 +6,18 @@ $memcached->addServer('memcached', 11211);
 // Проверка состояния сервера
 $status = $memcached->getStats();
 if ($status) {
-    echo "Connected to Memcached server.\n";
+    echo "Connected to Memcached server.<br>\n";
 } else {
-    echo "Failed to connect to Memcached server.\n";
+    echo "Failed to connect to Memcached server.<br>\n";
 }
 
 // Установка значения в кэш
 $setResult = $memcached->set('key', 'Hello, Memcached!', 60); // 60 секунд
 
 if ($setResult) {
-    echo "Value successfully set in cache.\n";
+    echo "Value successfully set in cache.<br>\n";
 } else {
-    echo "Failed to set value in cache.\n";
+    echo "Failed to set value in cache.<br>\n";
 }
 
 // Получение значения из кэша
