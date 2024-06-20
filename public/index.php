cb0f4238fb10b0c3837be12c14fe5d14bc716cd0
Timirey <misiru.artiom@gmail.com>
2024-06-20 12:16:41 +0300

    Add parentheses check ups

--- public/index.php
+++ public/index.php
@@ -1,3 +1,42 @@
-<?php
+<?php declare(strict_types=1);
 
-phpinfo();
+function checkParentheses(string $string): bool
+{
+    $chars = str_split($string);
+
+    $stack = [];
+
+    foreach ($chars as $char) {
+        if ($char === '(') {
+            $stack[] = $char;
+        } else if ($char === ')') {
+            if (empty($stack)) {
+                return false;
+            }
+
+            array_pop($stack);
+        }
+    }
+
+    return empty($stack);
+}
+
+header('Content-Type: application/json');
+
+$string = $_POST['string'] ?? null;
+
+try {
+    if (empty($string) || !is_string($string)) {
+        throw new Exception('The provided input is not a valid string.', 400);
+    }
+
+    if (!checkParentheses($string)) {
+        throw new Exception('Parentheses are invalid.', 400);
+    }
+
+    echo 'Parentheses are valid!';
+} catch (Exception $exception) {
+    http_response_code($exception->getCode());
+
+    echo $exception->getMessage();
+}
