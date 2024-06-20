64627acf5e3d3db0a0fac663738794c2ddf4b3f7
Timirey <misiru.artiom@gmail.com>
2024-06-20 10:47:18 +0300

    Update with infinite arguments count

--- sum.sh
+++ sum.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-term_1=$1
-term_2=$2
-
-if [ "$#" -ne 2 ]; then
-  echo "You should pass 2 terms (arguments) for summation."
+if [ "$#" -lt 2 ]; then
+  echo "You should pass at least 2 terms (arguments) for summation."
   exit 1
 fi
 
 number_regex='^-?[0-9]+([.][0-9]+)?$'
 
-if ! [[ $term_1 =~ $number_regex ]]; then
-  echo "The first argument is not a valid number."
-  exit 1
-fi
+sum=0
 
-if ! [[ $term_2 =~ $number_regex ]]; then
-  echo "The second argument is not a valid number."
-  exit 1
-fi
+for term in "$@"; do
+  if ! [[ $term =~ $number_regex ]]; then
+    echo "The argument '$term' is not a valid number."
+    exit 1
+  fi
 
-echo "$term_1 + $term_2" | bc
+  sum=$(echo "$sum + $term" | bc)
+done
+
+echo "$sum"
