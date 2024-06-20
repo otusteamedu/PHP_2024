7133b3147cb9e93e1f487b9dfe637c83b56f687c
ivasonik <ivasonik@mail.ru>
2024-06-20 15:38:54 +0300

    first problem: added first symbol check

--- sum1.sh
+++ sum1.sh
@@ -4,6 +4,19 @@
 read -p "Enter the first number: " num1
 read -p "Enter the second number: " num2
 
+# check variables for first "+"
+
+#remove "+" from the beginning of variables if any
+
+if [[ $num1 == "+"* ]]
+then
+	num1=$(cut -c2- <<< $num1)
+fi
+if [[ $num2 == "+"* ]]
+then
+	num2=$(cut -c2- <<< $num2)
+fi
+
 # Use bc to calculate the sum
 sum=$(echo "$num1 + $num2" | bc)
 
