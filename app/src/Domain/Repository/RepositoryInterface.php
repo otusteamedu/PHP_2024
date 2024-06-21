619bb54c83aac817f4cb319c88f8f2e93adbfc5f
hinoho <hinoho27@gmail.com>
2024-06-21 14:46:55 +0300

    fix

--- app/src/Domain/Repository/RepositoryInterface.php
+++ app/src/Domain/Repository/RepositoryInterface.php
@@ -6,6 +6,5 @@ namespace Kagirova\Hw14\Domain\Repository;
 
 interface RepositoryInterface
 {
-    public function __construct(string $host, string $user, string $password);
     public function search($params);
 }
