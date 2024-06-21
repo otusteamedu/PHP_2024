f07a7dd5a676352e87cbaea0456f51c2f13745ce
Stanislav Gavrilchik <dev_gs@bankiros.ru>
2024-06-21 11:07:31 +0300

    Sixth homework hw6 - Email validators

--- app/app.php
+++ app/app.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
-use Otus\Hw5\App;
+use Otus\Hw6\App;
 
 try {
-    (new App())->run();
+    $app = new App();
+    $app->run();
+    echo $app->getMessage();
 } catch (\Exception $e) {
     echo "ERROR: {$e->getMessage()}" . PHP_EOL;
 }
--- app/src/App.php
+++ app/src/App.php
@@ -1,32 +1,71 @@
 <?php
 
-namespace Otus\Hw5;
+declare(strict_types=1);
 
-use Otus\Hw5\dictionary\ServicesDictionary;
+namespace Otus\Hw6;
 
 class App
 {
-    /**
-     * @return void
-     * @throws \Exception
-     */
-    public function run(): void
+    /** @var string|null */
+    protected ?string $message;
+
+    /** @var EmailValidator */
+    private EmailValidator $emailValidator;
+
+    /** @var EmailDnsValidator */
+    private EmailDnsValidator $emailDnsValidator;
+
+    public function __construct()
     {
-        $participant = $this->getParticipant();
-
-        if (!array_key_exists($participant, ServicesDictionary::$allowedParticipants)) {
-            throw new \Exception('Access is denied!' . PHP_EOL);
-        }
-
-        $participant = new ServicesDictionary::$allowedParticipants[$participant]();
-        $participant->startChat();
+        $this->emailValidator = new EmailValidator();
+        $this->emailDnsValidator = new EmailDnsValidator();
     }
 
     /**
-     * @return string|null
+     * @return void
      */
-    private function getParticipant(): ?string
+    public function run(): void
     {
-        return $_SERVER['argv'][1] ?? null;
+        $emails = $this->getEmails();
+
+        foreach ($emails as $email) {
+            $this->emailValidator->validate($email);
+            $this->emailDnsValidator->validate($email);
+        }
+
+        $this->setMessage();
+    }
+
+    /**
+     * @return array
+     */
+    private function getEmails(): array
+    {
+        if (!empty($_SERVER['argv'])) {
+            array_shift($_SERVER['argv']);
+        }
+
+        return $_SERVER['argv'] ?? [];
+    }
+
+    /**
+     * @return void
+     */
+    protected function setMessage(): void
+    {
+        $errors = $this->emailValidator->getErrors();
+        $errors .= $this->emailDnsValidator->getErrors();
+
+        $this->message = !empty($errors)
+            ? $errors
+            : 'All emails are valid :)' . PHP_EOL;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
     }
 }
