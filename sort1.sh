89f0e286292a540da469e9ad362cbcd22f04ae19
ivasonik <ivasonik@mail.ru>
2024-06-23 17:45:56 +0300

    second problem: caption + blank lines

--- sort1.sh
+++ sort1.sh
@@ -1,4 +1,15 @@
-cut --fields=3,4 --delim=' ' cities.txt | awk '{a[$1] += $2} END{for (i in a) print a[i],i}' | sort -r | sed -n '1,3 p'
+sed '1d' cities.txt > c.txt
+sed '/^$/d' c.txt > d.txt
+cut --fields=3 --delim=' ' d.txt > e.txt
+awk '{print 1,$0}' e.txt > f.txt
+awk '{a[$2] += $1} END{for (i in a) print a[i],i}' f.txt | sort -r | sed -n '1,3 p'
+rm c.txt
+rm d.txt
+rm e.txt
+rm f.txt 
+
+#sed '/^$/d' cities.txt
+#awk '{a[$1] += $2} END{for (i in a) print a[i],i}' | sort -r | sed -n '1,3 p'
 
 
 
