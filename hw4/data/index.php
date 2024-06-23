c278809d872ea47520b3894e8c4cf0b9a498f2ac
Evgeniy Smirnov <evgenysmrnv@gmail.com>
2024-06-23 20:18:51 +0300

    phpcs fix

--- hw4/data/index.php
+++ hw4/data/index.php
@@ -12,33 +12,34 @@ if ($_POST['string'] == "") {
     }
 }
 
-function isValid($str){
+function isValid($str): bool
+{
     $star = 0;
     $open = [];
     $len = strlen($str);
 
-    for($i = 0; $i < $len; ++$i){
-        if($str[ $i ] == ')'){
-            if(count($open) > 0) array_pop($open);
-            elseif($star > 0) $star--;
+    for ($i = 0; $i < $len; ++$i) {
+        if ($str[$i] == ')') {
+            if (count($open) > 0) array_pop($open);
+            elseif ($star > 0) $star--;
             else return false;
-        }elseif( $str[ $i ] == '('){
+        } elseif ($str[$i] == '(') {
             array_push($open, $i);
-        }else{
+        } else {
             $star++;
         }
     }
 
-    if(count($open) === 0) return true;
+    if (count($open) === 0) return true;
     // check leftover open braces from the back
     $star = $ptr = 0;
     $open = array_reverse($open);
 
-    for($i = $len - 1; $i >= 0 && $ptr < count($open); --$i){
-        if($str[ $i ] == '*'){
+    for ($i = $len - 1; $i >= 0 && $ptr < count($open); --$i) {
+        if ($str[$i] == '*') {
             $star++;
-        }else if($i == $open[ $ptr ]){
-            if($star == 0) return false;
+        } else if ($i == $open[$ptr]) {
+            if ($star == 0) return false;
             $star--;
             $ptr++;
         }
