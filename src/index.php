eb1403330600feccb577eb976e068723d7ec120b
evgenyart <albatros98@yandex.ru>
2024-06-23 15:09:50 +0300

    hw4 part2

--- src/index.php
+++ src/index.php
@@ -7,3 +7,5 @@ include __DIR__ . '/Validator.php';
 if (isset($_POST['string'])) {
     Validator::validate($_POST['string']);
 }
+
+echo PHP_EOL . PHP_EOL . "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
