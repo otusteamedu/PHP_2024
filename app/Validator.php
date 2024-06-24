44a2579cac1f9f8c2f772fe9ffcd0b9976cb9d5e
evgenyart <albatros98@yandex.ru>
2024-06-24 08:35:21 +0300

    fix errors phpstan

--- app/Validator.php
+++ app/Validator.php
@@ -14,34 +14,34 @@ class Validator
             if (!strlen($string)) {
                 Response::answer('Пустая строка', true);
             }
-    
+
             if (!preg_match("/^[)(]+$/u", $string)) {
                 Response::answer('Обнаружены недопустимые символы, допустимы только ( и )', true);
             }
-    
+
             $length = strlen($string);
-    
+
             $counter = 0;
             for ($i = 0; $i < $length; $i++) {
                 $chr = substr($string, $i, 1);
-    
+
                 if ($i == 0) {
                     if ($chr != '(') {
                         Response::answer('Некоректная строка, должна начинаться с (', true);
                     }
                 }
-    
+
                 if ($chr == '(') {
                     $counter++;
                 } elseif ($chr == ')') {
                     $counter--;
                 }
-    
+
                 if ($counter < 0) {
                     Response::answer('Некорректная строка - скобок ) больше чем открываюших', true);
                 }
             }
-    
+
             if ($counter != 0) {
                 Response::answer('Некорректная строка - скобок ( больше чем закрывающих', true);
             } else {
