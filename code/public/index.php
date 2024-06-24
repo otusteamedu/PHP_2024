cea002a3824520075a46dbe5c2d02babc85b4dcd
aleksnoname <gitcommit@yandex.ru>
2024-06-24 21:17:38 +0300

    2 task

--- code/public/index.php
+++ code/public/index.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+
+# Task 1 (https://leetcode.com/problems/intersection-of-two-linked-lists/)
+
 /**
  * Definition for a singly-linked list.
  */
@@ -48,3 +51,45 @@ class Solution {
     }
 }
 
+# Task 2 (https://leetcode.com/problems/fraction-to-recurring-decimal/description/)
+
+/**
+ * @param Integer $n
+ * @param Integer $d
+ * @return String
+ */
+function fractionToDecimal(int $n, int $d): string {
+
+    $div = $n/$d;
+
+    if (!is_float($div)) {
+        return (string)$div;
+    }
+
+    $divToArr = explode(".", (string)$div);
+    $divAfterDot = $divToArr[1];
+    $result = $divToArr[0];
+    //$length = strlen($divAfterDot);
+    $a = '';
+    $match = '';
+    $arr = str_split($divAfterDot);
+
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($arr[$i] === $arr[$i+1]) {
+            $match = $arr[$i];
+            return $result.'.'.$a.'('.$match.')';
+        } else {
+            $a.= $arr[$i];
+            if ($a[0] === $arr[$i]) {
+                for ($j = 0; $j < strlen($a); $j++) {
+                    if ($a[$j] === $arr[$i+$j]) {
+                        $match .= $a[$j];
+                        return $result.'.('.$match.')';
+                    }
+                }
+            }
+        }
+    }
+}
+
+echo fractionToDecimal(1,6);
