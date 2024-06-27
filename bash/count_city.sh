ef7fab536fccf47b63ae434a3c42a2800f9b473b
sergey-program <sergey.program@gmail.com>
2024-06-28 00:59:32 +0300

    refactor. formatting

--- bash/count_city.sh
+++ bash/count_city.sh
@@ -17,11 +17,10 @@ echo -e;
 echo -e "let's search best one";
 echo -e;
 
-#CITY=$(awk 'NR>1 {print $3}' "$1" | sort | uniq -c | sort -nr)
-CITY=$(awk 'NR>1 {print $3}' "$1")
+CITY=$(awk -F" " 'NR>1 {print $3}' "$1")
 
 echo "Get third column as list:"
-echo "$CITY" | sort;
+echo "$CITY" | sort -nr;
 echo -e;
 echo "Get only unique and count:"
 echo "$CITY" | sort | uniq -c;
@@ -30,10 +29,10 @@ echo "Sort items by count:"
 echo "$CITY" | sort | uniq -c | sort -nr;
 echo -e;
 echo -e;
-echo "Top three city:"
 
 SORTED=$(awk 'NR>1 {print $3}' "$1" | sort | uniq -c | sort -nr);
 
+echo "Top three city:"
 echo "$SORTED" | head -n 3
 
 exit 0;
