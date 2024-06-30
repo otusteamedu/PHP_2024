d7b78d20205787cdc60145784cb590e3ec45c966
Ivan <kuzmin_ia@energotronika.ru>
2024-07-01 00:26:22 +0700

    Балансировка nginx

--- code/index.php
+++ code/index.php
@@ -1,42 +1,8 @@
 <?php
-$keyRequest = "string";
-try {
-  echo checkRequest($_POST, $keyRequest);
-} catch (Exception $e) {
-  echo $e->getMessage();
-}
-function checkRequest($request, $keyRequest)
-{
-  if (empty($request)) {
-    header("HTTP/1.0 400 Bad Request");
-    throw new Exception("Передан пустой массив");
-  } else if (!array_key_exists($keyRequest, $request)) {
-    header("HTTP/1.0 400 Bad Request");
-    throw new Exception("Не указан параметр с ключом " . $keyRequest);
-  } else if (!isCorrect($request[$keyRequest])) {
-    header("HTTP/1.0 400 Bad Request");
-    throw new Exception("Ошибка в скобках, проверьте правильность написания");
-  } else {
-    header("HTTP/1.0 200 OK");
-    return "Ошибок не найдено";
-  }
-}
 
-function isCorrect($string)
-{
-  $len = strlen($string);
-  $stack = [];
-  for ($i = 0; $i < $len; $i++) {
-    $simbol = $string[$i];
-    if ($simbol == '(') {
-      $stack[] = $simbol;
-    } elseif ($simbol == ')') {
-      if (!$last = array_pop($stack)) {
-        return false;
-      } elseif ($simbol === ')' && $last != '(') {
-        return false;
-      }
-    }
-  }
-  return count($stack) === 0;
-}
+echo "Привет, Otus!<br>" . date("Y-m-d H:i:s") . "<br><br>";
+echo "Прокси сервер"  . $_SERVER['SERVER_ADDR'] . "<br><br>";
+echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+
+
+echo "Connection to server successfully <br>";
