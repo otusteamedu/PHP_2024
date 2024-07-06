86250ca634c5c2fb2afbbd3386d0b8b98f926a58
sergey-program <sergey.program@gmail.com>
2024-07-07 01:58:54 +0300

    fix. remove old code

--- www/index.php
+++ www/index.php
@@ -1,9 +1,9 @@
 <?php
 
-echo '<h1>Hello world! ' . date('Y-m-d h:i:s') . '</h1>';
-
 ini_set('display_errors', 'on');
 
+echo '<h2>Hello world! ' . date('Y-m-d h:i:s') . '</h2>';
+echo '<h1>Server ip address '. $_SERVER['SERVER_ADDR'].'</h1>';
 
 // check mysql
 try {
@@ -18,31 +18,6 @@ try {
     echo 'Database connection: successful</br>';
 } catch (\Throwable $e) {
     echo 'Database connection: error. ' . $e->getMessage() . '</br>';
-}
-
-// check memcache
-try {
-    $memcached = new Memcache();
-    $memcached->addServer(getenv('MEMCACHED_HOST'), getenv('MEMCACHED_PORT_IN'));
-    $memcached->set('key', 12);
-
-    if (!empty($memcached->get('key'))) {
-        echo 'Memcached connection: successful</br>';
-    } else {
-        echo 'Memcached connection: error</br>';
-    }
-} catch (\Throwable $e) {
-    echo 'Memcached connection: error. ' . $e->getMessage() . '<br>';
-}
-
-// check redis
-try {
-    $redis = new Redis();
-    $redis->connect(getenv('REDIS_HOST'), getenv('REDIS_PORT_IN'));
-
-    echo 'Redis connection: successful. Ping: ' . $redis->ping() . '<br>';
-} catch (\Throwable $e) {
-    echo 'Redis connection: error. ' . $e->getMessage() . '<br>';
 }
 
 phpinfo();
