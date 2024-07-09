ad1964685d1c425b3a848d7b26d64f3de743edef
Yurii Serikov <yu.serikov@myspaceport.ru>
2024-07-09 10:58:45 +0500

    YSerikov/hw8

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PHP_2024
 
 https://otus.ru/lessons/razrabotchik-php/?utm_source=github&utm_medium=free&utm_campaign=otus
+
+### Time Complexity:
+> O(N+M)
+
+Single `whie` loop that breaks when one of the arrays runs out of elements.
+
+### Space Complexity:
+> O(1)
+
+The algorithm does not create additional data structures for calculations.
---
+++ solution.php
@@ -0,0 +1,46 @@
+<?php
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution
+{
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2)
+    {
+        $res = new ListNode();
+        $head = $res;
+        while ($list1 || $list2) {
+            if ($list1 === null) {
+                $head->next = $list2;
+                break;
+            }
+            if ($list2 === null) {
+                $head->next = $list1;
+                break;
+            }
+            if ($list1->val <= $list2->val) {
+                $head->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $head->next = $list2;
+                $list2 = $list2->next;
+            }
+            $head = $head->next;
+        }
+        $res = $res->next;
+        return $res;
+    }
+}
