11ccf2e837a3e98a66994b8b0ed9278c79a13ba6
Яцишин Владислав Игоревич <vladislav.yatsishin@mvideo.ru>
2024-07-10 17:58:18 +0300

    Rework task2

--- code/task2.php
+++ code/task2.php
@@ -10,64 +10,37 @@ class Solution
      */
     function fractionToDecimal($numerator, $denominator)
     {
-        $result = $numerator / $denominator;
-        $formatted = number_format($result, 16);
-        $digitsAfterPoint = substr(strrchr($result, '.'), 1);
-        $digitsAfterPointCount = strlen($digitsAfterPoint);
-        if ($digitsAfterPointCount <= 6) {
-            var_dump('low count');
-            return (string)$result;
+        if (empty($numerator)) {
+            return '0';
         }
 
-        $firstSymbol = null;
-        $firstPart = '';
-        $secondPart = '';
-        $firstPartWithoutEqual = '';
-        $secondPartWithoutEqual = '';
-        $isFirstPartFinished = false;
-        $isFractionEqual = false;
-        $isFractionDuplicate = false;
-        $lastDigit = null;
-        $lastDigitDuplicateTimes = 0;
+        $isNegative = ($numerator * $denominator) < 0;
+        $numerator = abs($numerator);
+        $denominator = abs($denominator);
 
-        foreach (str_split($digitsAfterPoint) as $digitAfterPoint) {
-            if ($digitAfterPoint === $firstSymbol && $isFirstPartFinished && $digitAfterPoint !== $lastDigit) {
-                $isFractionEqual = $firstPart === $secondPart;
-                break;
-            }
-            if ($digitAfterPoint === $firstSymbol && $digitAfterPoint !== $lastDigit) {
-                $isFirstPartFinished = true;
-            }
-            if ($firstSymbol === null) {
-                $firstSymbol = $digitAfterPoint;
-            }
-
-            if (!$isFirstPartFinished && !$isFirstPartFinished) {
-                $firstPart .= $digitAfterPoint;
-            } else {
-                $secondPart .= $digitAfterPoint;
-            }
-
-            if ($lastDigit === $digitAfterPoint) {
-                if ($lastDigitDuplicateTimes++ > 5) {
-                    $secondPartWithoutEqual = $digitAfterPoint;
-                    $isFractionDuplicate = true;
-                    $firstPartWithoutEqual = substr($firstPartWithoutEqual, 0, -1);
-                    break;
-                }
-            } else {
-                $firstPartWithoutEqual .= $digitAfterPoint;
-            }
-
-            $lastDigit = $digitAfterPoint;
+        $firstPart = (int)($numerator / $denominator);
+        $result = ($isNegative ? '-' : '') . $firstPart;
+        $modulo = $numerator % $denominator;
+        if ($modulo == 0) {
+            return $result;
         }
 
-        if ($isFractionEqual) {
-            $result = "0.({$firstPart})";
-        } elseif ($isFractionDuplicate) {
-            $result = "0.{$firstPartWithoutEqual}({$secondPartWithoutEqual})";
+        $result .= '.';
+        $positions = [];
+        $decimal = '';
+
+        while ($modulo != 0 && !isset($positions[$modulo])) {
+            $positions[$modulo] = strlen($decimal);
+            $modulo *= 10;
+            $decimal .= intdiv($modulo, $denominator);
+            $modulo %= $denominator;
         }
 
-        return $result;
+        if ($modulo == 0) {
+            return $result . $decimal;
+        } else {
+            $repeatedStart = $positions[$modulo];
+            return $result . substr($decimal, 0, $repeatedStart) . "(" . substr($decimal, $repeatedStart) . ")";
+        }
     }
 }
