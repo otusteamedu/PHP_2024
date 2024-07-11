7488c81b930dcebc79b9ebccb7fa847cf1dce559
t.bublikova <tbublikova@gmail.com>
2024-07-11 17:26:59 +0200

    hw10 - fix phpCs

--- index.php
+++ index.php
@@ -1,62 +1,40 @@
 <?php
 
-namespace TwoSortedLists;
-
-class ListNode
+function mergeTwoLists($list1, $list2)
 {
-    public $val = 0;
-    public $next = null;
-
-    function __construct($val = 0, $next = null)
-    {
-        $this->val = $val;
-        $this->next = $next;
+    if ($list1 === null) {
+        return $list2;
     }
-}
+    if ($list2 === null) {
+        return $list1;
+    }
 
-class Solution
-{
-    /**
-     * @param ListNode $list1
-     * @param ListNode $list2
-     * @return ListNode
-     */
-    public function mergeTwoLists($list1, $list2)
-    {
-        if ($list1 === null) {
-            return $list2;
-        }
-        if ($list2 === null) {
-            return $list1;
-        }
+    if ($list1->val <= $list2->val) {
+        $newlist = $list1;
+        $list1 = $list1->next;
+    } else {
+        $newlist = $list2;
+        $list2 = $list2->next;
+    }
 
+    $current = $newlist;
+
+    while ($list1 && $list2) {
         if ($list1->val <= $list2->val) {
-            $newlist = $list1;
+            $current->next = $list1;
             $list1 = $list1->next;
         } else {
-            $newlist = $list2;
+            $current->next = $list2;
             $list2 = $list2->next;
         }
-
-        $current = $newlist;
-
-        while ($list1 && $list2) {
-            if ($list1->val <= $list2->val) {
-                $current->next = $list1;
-                $list1 = $list1->next;
-            } else {
-                $current->next = $list2;
-                $list2 = $list2->next;
-            }
-            $current = $current->next;
-        }
-
-        if ($list1 !== null) {
-            $current->next = $list1;
-        } else {
-            $current->next = $list2;
-        }
-
-        return $newlist;
+        $current = $current->next;
     }
+
+    if ($list1 !== null) {
+        $current->next = $list1;
+    } else {
+        $current->next = $list2;
+    }
+
+    return $newlist;
 }
