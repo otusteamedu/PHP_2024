65604a0e99a3ff0f98921e6fcf42ae07f4c6753b
Aliaksandr Harshkou <dev@ru.myfin.by>
2024-07-16 00:17:55 +0300

    hw19

--- www/src/rabbit/Consumer.php
+++ www/src/rabbit/Consumer.php
@@ -6,6 +6,8 @@ class Consumer extends RabbitMQ
 {
     public function consume(string $queueName, callable $callback): void
     {
+        $this->declare($queueName);
+
         $this->getChannel()->basic_consume($queueName, '', false, true, false, false, $callback);
 
         while ($this->getChannel()->is_consuming()) {
