279c375710ad6b7e6de21274e28d1533eba48d2a
Яцишин Владислав Игоревич <vladislav.yatsishin@mvideo.ru>
2024-07-16 19:59:52 +0300

    Remove legacy

--- code/sql/most_expansive.sql
+++ code/sql/most_expansive.sql
@@ -1,8 +1,8 @@
 select movie.title, sum(movie.most_expansive)
-from (select m.title, s.basePrice, sum(t.price) as most_expansive
+from (select m.title, sum(t.price) as most_expansive
       from ticket t
                join session s on t.session_id = s.id
                join movie m on s.movie = m.id
-      group by t.session_id, m.title, s.basePrice) as movie
+      group by t.session_id, m.title) as movie
 group by movie.title
 order by sum(movie.most_expansive) DESC limit 1;
