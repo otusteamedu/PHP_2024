a56b85b972d15c67fc7f05671989233ef4d93558
Timirey <misiru.artiom@gmail.com>
2024-07-21 21:30:20 +0300

    Get actual results

--- 3.3. Cамые редко используемые индексы.sql
+++ 3.3. Cамые редко используемые индексы.sql
@@ -23,5 +23,15 @@ LIMIT 5;
  */
 
 /**
-   Таблица с результатами ~ 100.000 данных (после оптимизации)
+    Таблица с результатами ~ 100.000 данных (после оптимизации)
+
+    +--------------------------+----------+-----------+
+    |index_name                |table_name|usage_count|
+    +--------------------------+----------+-----------+
+    |tickets_pkey              |tickets   |0          |
+    |customers_email_key       |customers |0          |
+    |idx_sessions_start_time   |sessions  |2          |
+    |idx_tickets_purchased_date|tickets   |9          |
+    |idx_sessions_start_date   |sessions  |15         |
+    +--------------------------+----------+-----------+
  */
