fa080251d843811974dfd731d72d15a039f3e31a
EkaterinaKonyaeva <e.konyaeva@notamedia.ru>
2024-08-10 01:58:22 +0300

    add hw6

---
+++ code/index.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Ekonyaeva\Otus\EmailValidate;
+
+$emails = [
+    'example@example.com',
+    'user@.com',
+    'test.email+alex@leetcode.com',
+    'user.name@domain.co.uk',
+    'username@domain..com',
+    'example@.com',
+    '@example.com',
+    'user@subdomain.domain.com',
+    'first_last@domain.org',
+    'user@domain.corporate',
+    'user@domain.a1',
+    'name123@numbers.com',
+    '"user@name".com',
+    'user@domain.com.',
+    'email@domain.name',
+    'user@domain..com',
+    'user@-domain.com',
+    'email@domain.c',
+    'email@domain.museum',
+    'test@.domain.com',
+];
+
+foreach (EmailValidate::validateArray($emails) as $key => $result) {
+    if (!$result) {
+        echo $emails[$key] . " - определён как невалдиный " . '<br>';
+    } else {
+        echo $emails[$key] . " - определён как валдиный " . '<br>';
+    }
+}
