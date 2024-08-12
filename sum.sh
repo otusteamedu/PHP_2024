05544601005a0936badd31b9a02988dd2723ab6c
Sergey Komarov <sk-2006@mail.ru>
2024-08-12 11:18:50 +0300

    hw2

--- sum.sh
+++ sum.sh
@@ -2,12 +2,7 @@
 
 re='^[+-]?[0-9]+([.][0-9]+)?$'
 
-if which bc > /dev/null
-then
-    ! [[ $1 =~ $re ]] && echo "Firt argument is not integer" && exit 1;
-    ! [[ $2 =~ $re ]] && echo "Second argument is not integer" && exit 1;
+! [[ $1 =~ $re ]] && echo "Firt argument is not integer" && exit 1;
+! [[ $2 =~ $re ]] && echo "Second argument is not integer" && exit 1;
 
-    echo "$1 + $2" | bc
-else
-    echo "bc package is not isntalled"
-fi
+echo $(awk "BEGIN {printf $1 + $2}")
