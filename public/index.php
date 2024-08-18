417ab0e856443f19ecaf2493f0b5ee239393556e
Vadim Kasatkin <v.kasatkin@mcart.ru>
2024-08-18 20:51:00 +0400

    подключние своего пакета

--- public/index.php
+++ public/index.php
@@ -1,24 +1,5 @@
 <?php
 
-$redis = new Redis();
-$redis->connect('redis', 6379);
-echo $redis->ping('Redis: ok') ?: 'Redis: error';
-echo '<br>';
+include __DIR__ . '/../vendor/autoload.php';
 
-$memcached = new Memcached();
-$memcached->addServer('otusphp_memcached', 11211);
-echo $memcached->getStats()
-    ? 'Memcached: ok'
-    : 'Memcached: error';
-echo '<br>';
-
-try {
-    $pdo = new PDO(
-        'mysql:host=otusphp_db;dbname=otusphp',
-        'root',
-        'root'
-    );
-    echo 'DB: ok';
-} catch (Exception $e) {
-    echo 'DB: error';
-}
+\PenguinAstronaut\OtusTestPackage\Core\HelloWorld::hello('Penguin!');
