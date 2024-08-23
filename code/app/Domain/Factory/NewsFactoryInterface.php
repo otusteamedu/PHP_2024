5d4a14432fd384c21e66c4cfe50dc9af9c2db3f6
Viktor Rikhert <viking311@gmail.com>
2024-08-23 09:12:34 +0700

    add domain's classes

---
+++ code/app/Domain/Factory/NewsFactoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Factory;
+
+use App\Domain\Entity\News;
+
+interface NewsFactoryInterface
+{
+    public function create(string $url): News;
+}
---
+++ code/app/Domain/ValueObject/ExportDate.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\ValueObject;
+
+use DateTimeImmutable;
+use InvalidArgumentException;
+
+class ExportDate
+{
+    /** @var DateTimeImmutable */
+    private DateTimeImmutable $value;
+
+    /**
+     * @param string|DateTimeImmutable $value
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws DateMalformedStringException
+     */
+    public function __construct(string|DateTimeImmutable $value): void
+    {
+        if (is_string($value)) {
+            $this->value = new DateTimeImmutable($value);
+        } elseif ($value instanceof DateTimeImmutable) {
+            $this->value = $value;
+        } else {
+            throw new InvalidArgumentException('Date is incorrect');
+        }
+    }
+}
