977f9c9ccf0ad6c4ee5c1a837e8971d44b4b3279
mukhametali <m.kussaiynov1996@gmail.com>
2024-08-31 00:00:36 +0500

    hw4

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # PHP_2024
 
 https://otus.ru/lessons/razrabotchik-php/?utm_source=github&utm_medium=free&utm_campaign=otus
+
+### 1.1. Консольная команда для правильных скобок
+```php
+ curl -X POST http://mysite.local -d "string=(()()()(()(()))())"
+```
+### 1.2. Консольная команда для неправильных скобок
+```php
+ curl -X POST http://mysite.local -d "string=(()()()(()(()))()))"
+```
+### 1.3. Консольная команда для пустого запроса
+```php
+ curl -X POST http://mysite.local -d "string"
+```
+### 1.4. Консольная команда для недопустимого метода
+```php
+ curl -X GET http://mysite.local -d "string"
+```
+### 2. Каждый раз когда обновляем страницу или делаем curl-запрос в консли, то будет выводиться разные имя хостов
+```php
+ Имя хоста: php2-host
+ Метод не разрешен.
+```
---
+++ src/index.php
@@ -0,0 +1,43 @@
+<?php
+
+$hostname = gethostname();
+header('Content-Type: text/plain');
+echo "Имя хоста: " . $hostname . PHP_EOL;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['string']) && !empty(trim($_POST['string']))) {
+        $string = trim($_POST['string']);
+        if (isValidString($string)) {
+            http_response_code(200);
+            echo 'Строка действительна.';
+            echo PHP_EOL;
+        } else {
+            http_response_code(400);
+            echo 'Строка недействительна.';
+            echo PHP_EOL;
+        }
+    } else {
+        http_response_code(400);
+        echo 'Строковый параметр отсутствует или пуст.';
+        echo PHP_EOL;
+    }
+} else {
+    http_response_code(405);
+    echo 'Метод не разрешен.';
+    echo PHP_EOL;
+}
+
+function isValidString($str) {
+    $stack = [];
+    foreach (str_split($str) as $char) {
+        if ($char === '(') {
+            array_push($stack, $char);
+        } elseif ($char === ')') {
+            if (empty($stack)) {
+                return false;
+            }
+            array_pop($stack);
+        }
+    }
+    return empty($stack);
+}
