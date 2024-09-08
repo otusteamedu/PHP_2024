829e536b8dd68685322d847edebc354ab9f2c147
Vadim Kasatkin <v.kasatkin@mcart.ru>
2024-09-08 17:02:46 +0400

    перенос логики из index.php

--- public/index.php
+++ public/index.php
@@ -2,18 +2,9 @@
 
 declare(strict_types=1);
 
+use PenguinAstronaut\App\App;
+
 include __DIR__ . '/../vendor/autoload.php';
 
-use PenguinAstronaut\App\Exceptions\EmptyStringException;
-use PenguinAstronaut\App\Exceptions\InvalidStringException;
-use PenguinAstronaut\App\Validator;
-
-
-try {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $validator = new Validator();
-        $validator->validateString($_POST['string'] ?? '');
-    }
-} catch (EmptyStringException | InvalidStringException $e) {
-    http_response_code(400);
-}
+$app = new App();
+$app->run();
