38cf7e10d582f7d3869de4590fb08eaa0f2b4ce0
Yurii Serikov <uioiopu@gmail.com>
2024-09-09 22:37:05 +0500

    hw11: es core

--- code/public/index.php
+++ code/public/index.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Otus\App\EmailCheker\EmailCheker;
+use Elastic\Elasticsearch\ClientBuilder;
 
-(new EmailCheker())->run();
+$client = ClientBuilder::create()
+    ->setHosts(['https://elastic:9200'])
+    ->setBasicAuthentication('elastic', '_osHo1z=ne7sDq44S+4z')
+    ->setSSLVerification(false)
+    ->build();
+
+try {
+    $response = $client->cluster()->health();
+    echo "Elasticsearch cluster is up and running!<br>";
+    echo "Cluster Health: " . $response['status'] . "<br>";
+    echo "Node Count: " . $response['number_of_nodes'] . "<br>";
+    echo "Active Primary Shards: " . $response['active_primary_shards'] . "<br>";
+    echo "Active Shards: " . $response['active_shards'] . "<br>";
+} catch (Exception $e) {
+    echo "Elasticsearch is not reachable: " . $e->getMessage();
+}
