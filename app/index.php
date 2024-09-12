607c2318db585c050d673dfe2e05dd4a25f70723
john9683 <john_9683@mail.ru>
2024-09-12 16:27:45 +0700

    hw-1

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo 'it works!<br>'.date('Y-m-d H:i:s') .'<br>';
+
+$redis = new Redis();
+$connect = $redis->connect('redis');
+echo ($connect ? 'Redis: коннект установлен' : 'Redis: коннект отсутсвует') .'<br>';
+
+$auth = $redis->auth($_ENV['REDIS_PASSWORD']);
+echo ($auth ? 'Redis: авторизация выполнена' : 'Redis: в доступе отказано') .'<br>';
+
+$redis->publish(
+  'redis-test',
+  json_encode([
+    'test' => 'success'
+  ])
+);
+
+$redis->close();
+
+phpinfo();
