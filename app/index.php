94a2e3bdaceaf591365669a4ce8e29d8340089bd
john9683 <john_9683@mail.ru>
2024-09-12 16:57:47 +0700

    hw-1 fix 1

--- app/index.php
+++ app/index.php
@@ -1,20 +1,15 @@
 <?php
 
-echo 'it works!<br>'.date('Y-m-d H:i:s') .'<br>';
+echo 'it works!<br>' . date('Y-m-d H:i:s') . '<br>';
 
 $redis = new Redis();
 $connect = $redis->connect('redis');
-echo ($connect ? 'Redis: коннект установлен' : 'Redis: коннект отсутсвует') .'<br>';
+echo ($connect ? 'Redis: коннект установлен' : 'Redis: коннект отсутсвует') . '<br>';
 
 $auth = $redis->auth($_ENV['REDIS_PASSWORD']);
-echo ($auth ? 'Redis: авторизация выполнена' : 'Redis: в доступе отказано') .'<br>';
+echo ($auth ? 'Redis: авторизация выполнена' : 'Redis: в доступе отказано') . '<br>';
 
-$redis->publish(
-  'redis-test',
-  json_encode([
-    'test' => 'success'
-  ])
-);
+$redis->publish('redis-test', json_encode(['test' => 'success']));
 
 $redis->close();
 
