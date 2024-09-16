eb6a1963e02a17340ba42964d953caeacddd775f
stasya0903 <stasya0903@mail.ru>
2024-09-16 15:40:29 -0600

    #hw 8 added resolution

---
+++ merge_two_lists.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * $currentNode * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2)
+    {
+        $head = new ListNode();
+        $currentNodeFirst = $list1;
+        $currentNodeSecond = $list2;
+        $currentNodeMerged = $head;
+
+        while ($currentNodeFirst && $currentNodeSecond) {
+            $firstValue = $currentNodeFirst->val;
+            $secondValue = $currentNodeSecond->val;
+
+            if ($firstValue > $secondValue) {
+                $currentNodeMerged->next = $currentNodeSecond;
+                $currentNodeSecond = $currentNodeSecond->next;
+            } else {
+                $currentNodeMerged->next = $currentNodeFirst;
+                $currentNodeFirst = $currentNodeFirst->next;
+            }
+            $currentNodeMerged = $currentNodeMerged->next;
+        }
+
+        $tail = $currentNodeFirst ?? $currentNodeSecond;
+        if ($tail) {
+            $currentNodeMerged->next = $tail;
+        }
+
+        return $head->next;
+    }
+}
+
+/**
+ * Вычислительная сложность - О(n)
+ * Так как мы проходимся одновременно по обоим массивам, сложность будет расти пропорционально
+ * размерам переданных массивов
+ */
