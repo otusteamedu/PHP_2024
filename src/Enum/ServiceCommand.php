e9355148b639fc7fa2eedb326608a902bbbe212b
john9683 <john_9683@mail.ru>
2024-09-21 23:42:31 +0700

    fix-4

--- src/Enum/ServiceCommand.php
+++ src/Enum/ServiceCommand.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Enum;
 
 enum ServiceCommand: string {
-  case ServerStart = 'server:start';
-  case ClientStart = 'client:start';
-  case ChatStop    = 'chat:stop';
+    case ServerStart = 'server:start';
+    case ClientStart = 'client:start';
+    case ChatStop    = 'chat:stop';
 }
 
--- src/Enum/ServiceMessage.php
+++ src/Enum/ServiceMessage.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace App\Enum;
 
 enum ServiceMessage: string {
-  case ServerStarted     = "[Server] Server started \n";
-  case WelcomeToChat     = " Welcome to the chat! To stop the chat, type 'chat:stop'";
-  case ClientMessage     = '[Client message] ';
-  case ServerAnswer      = 'Server received message: ';
-  case ReceivedBytes     =  '; received bytes: ';
-  case ClientStoppedChat = "[Server] Client stopped the chat \n";
-  case ServerStopped     = "[Server] Server has been stopped \n";
-  case ClientStarted     = "[Client started] \n";
-  case ServerMessage     = '[Server message] ';
-  case ClientInvitation  = '[Write your message] ';
-  case ClientStopped     = "[Server message] Client stopped the chat, chat has been stopped \n";
+    case ServerStarted     = "[Server] Server started \n";
+    case WelcomeToChat     = " Welcome to the chat! To stop the chat, type 'chat:stop'";
+    case ClientMessage     = '[Client message] ';
+    case ServerAnswer      = 'Server received message: ';
+    case ReceivedBytes     =  '; received bytes: ';
+    case ClientStoppedChat = "[Server] Client stopped the chat \n";
+    case ServerStopped     = "[Server] Server has been stopped \n";
+    case ClientStarted     = "[Client started] \n";
+    case ServerMessage     = '[Server message] ';
+    case ClientInvitation  = '[Write your message] ';
+    case ClientStopped     = "[Server message] Client stopped the chat, chat has been stopped \n";
 }
