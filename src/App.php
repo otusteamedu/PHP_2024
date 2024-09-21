e652a160ecba6a578e7f1475082953d20bc76de7
john9683 <john_9683@mail.ru>
2024-09-21 23:39:05 +0700

    fix-2

--- src/App.php
+++ src/App.php
@@ -19,11 +19,11 @@ class App
     public function run($argv): void
     {
         if (in_array(ServiceCommand::ServerStart->value, $argv)) {
-          (new ServerController())->run();
+            (new ServerController())->run();
         }
 
         if (in_array(ServiceCommand::ClientStart->value, $argv)) {
-          (new ClientController())->run();
+            (new ClientController())->run();
         }
     }
 }
