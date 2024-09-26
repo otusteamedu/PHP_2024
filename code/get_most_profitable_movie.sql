4bc8a6789b9e15162a61ca52e377b2557dd9eb08
PIvanov <xeolit@gmail.com>
2024-09-26 15:45:07 +0300

    hw7. fix

--- code/get_most_profitable_movie.sql
+++ code/get_most_profitable_movie.sql
@@ -1,8 +1,8 @@
 SELECT m.title, SUM(tp.price) AS total_revenue
 FROM movies m
-         JOIN sessions s ON m.id = s.movie_id
-         JOIN tickets t ON s.id = t.session_id
-         JOIN ticket_prices tp ON t.ticket_price_id = tp.id
+         INNER JOIN sessions s ON m.id = s.movie_id
+         INNER JOIN ticket_prices tp ON s.id = tp.session_id
+         INNER JOIN tickets t ON tp.id = t.ticket_price_id
 GROUP BY m.id
 ORDER BY total_revenue DESC
     LIMIT 1;
