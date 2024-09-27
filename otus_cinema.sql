580123254ff012d0e9822d445fd17635d4e0c785
john9683 <john_9683@mail.ru>
2024-09-27 12:55:48 +0700

    removed json in seat_scheme

--- otus_cinema.sql
+++ otus_cinema.sql
@@ -1,4 +1,4 @@
--- CREATE DATABASE otus_cinema ENCODING = 'UTF8';
+CREATE DATABASE otus_cinema_layout_table ENCODING = 'UTF8';
 
 -- подключаемся к otus_cinema
 
@@ -39,20 +39,52 @@ VALUES
 
 CREATE TABLE seat_scheme (
 	id SERIAL PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    scheme JSON
+    title VARCHAR(50) NOT NULL
 );
 
-INSERT INTO seat_scheme(title, scheme)
+INSERT INTO seat_scheme(title)
 VALUES
-    ('малый зал', 
-	    '{
-            "row_1":{"0":{"noSeat":true},"1":{"noSeat":true},"number_1":{"number":1,"row":1,"locationId":1},"number_2":{"number":2,"row":1,"locationId":1},"number_3":{"number":3,"row":1,"locationId":1},"number_4":{"number":4,"row":1,"locationId":1}},
-            "row_2":{"0":{"noSeat":true},"1":{"noSeat":true},"number_1":{"number":1,"row":2,"locationId":3},"number_2":{"number":2,"row":2,"locationId":3},"number_3":{"number":3,"row":2,"locationId":3},"number_4":{"number":4,"row":2,"locationId":3}},
-            "row_3":{"0":{"noSeat":true},"1":{"noSeat":true},"number_1":{"number":1,"row":3,"locationId":3},"number_2":{"number":2,"row":3,"locationId":3},"number_3":{"number":3,"row":3,"locationId":3},"number_4":{"number":4,"row":3,"locationId":3}},
-            "row_4":{"number_1":{"number":1,"row":4,"locationId":2},"number_2":{"number":2,"row":4,"locationId":2},"number_3":{"number":3,"row":4,"locationId":2},"number_4":{"number":4,"row":4,"locationId":2},"number_5":{"number":4,"row":4,"locationId":2},"number_6":{"number":4,"row":4,"locationId":2}}
-        }' 
-    );  
+    ('малый зал'); 
+
+CREATE TABLE seat_layout (
+	id SERIAL PRIMARY KEY,
+    seat_scheme_id INTEGER REFERENCES seat_scheme,
+    row_number SMALLINT NOT NULL,
+    cell_number SMALLINT NOT NULL,
+    is_seat BOOLEAN NOT NULL,
+    seat_number SMALLINT,
+    location_id INTEGER REFERENCES seat_location
+);    
+
+INSERT INTO seat_layout(seat_scheme_id, row_number, cell_number, is_seat, seat_number, location_id)
+VALUES
+    (1, 1, 1, FALSE, NULL, NULL),
+    (1, 1, 2, FALSE, NULL, NULL),
+    (1, 1, 3, TRUE, 1, 1),
+    (1, 1, 4, TRUE, 2, 1),
+    (1, 1, 5, TRUE, 3, 1),
+    (1, 1, 6, TRUE, 4, 1),
+
+    (1, 2, 1, FALSE, NULL, NULL),
+    (1, 2, 2, FALSE, NULL, NULL),
+    (1, 2, 3, TRUE, 1, 3),
+    (1, 2, 4, TRUE, 2, 3),
+    (1, 2, 5, TRUE, 3, 3),
+    (1, 2, 6, TRUE, 4, 3),
+
+    (1, 3, 1, FALSE, NULL, NULL),
+    (1, 3, 2, FALSE, NULL, NULL),
+    (1, 3, 3, TRUE, 1, 3),
+    (1, 3, 4, TRUE, 2, 3),
+    (1, 3, 5, TRUE, 3, 3),
+    (1, 3, 6, TRUE, 4, 3),
+
+    (1, 4, 1, TRUE, 1, 2),
+    (1, 4, 2, TRUE, 2, 2),
+    (1, 4, 3, TRUE, 3, 2),
+    (1, 4, 4, TRUE, 4, 2),
+    (1, 4, 5, TRUE, 5, 2),
+    (1, 4, 6, TRUE, 6, 2);
 
 CREATE TABLE hall (
 	id SERIAL PRIMARY KEY,
@@ -139,39 +171,41 @@ CREATE TABLE ticket (
 
 CREATE INDEX ticket_schedule_idx ON ticket (id, schedule_id);
 
--- определяем цену для каждого кресла/зала/фильма/сеанса и заливаем таблицу билетов
+-- заливаем таблицу билетов
 
 DO
 $$
 
 DECLARE
 	_max_schedule integer := (SELECT COUNT(*) FROM schedule);
-	_max_row integer := (SELECT COUNT(*) FROM (SELECT jsonb_each(scheme::jsonb) FROM seat_scheme));
-	_max_seat integer := (SELECT COUNT(*) FROM (SELECT jsonb_each(scheme::jsonb -> 'row_1') FROM seat_scheme));
+	_max_row integer := (SELECT MAX(row_number) FROM seat_layout);
+	_max_cell integer := (SELECT MAX(cell_number) FROM seat_layout);
 	
 BEGIN
-    FOR _i IN 1.._max_schedule LOOP
-	FOR _r IN 1.._max_row LOOP
-	FOR _s IN 1.._max_seat LOOP
+    FOR _schedule IN 1.._max_schedule LOOP
+	FOR _row IN 1.._max_row LOOP
+	FOR _cell IN 1.._max_cell LOOP
 	
-	IF ((SELECT (scheme::jsonb -> ('row_' || _r)) -> ('number_' || _s) -> 'locationId' FROM seat_scheme) IS NOT NULL)
+	IF (SELECT is_seat FROM seat_layout WHERE row_number = _row AND cell_number = _cell)    
 	
 	THEN
 		INSERT INTO ticket (schedule_id, seat_row, seat_number, tariff_price)
-		SELECT schedule.id, _r, _s, (movie.min_price + tariff_movie.tariff_value + tariff_hall.tariff_value + tariff_seat.tariff_value + tariff_schedule.tariff_value) 
+		SELECT schedule.id, _row, seat_layout.seat_number, (movie.min_price + tariff_movie.tariff_value + tariff_hall.tariff_value + tariff_seat.tariff_value + tariff_schedule.tariff_value) 
 		FROM schedule schedule
 		JOIN hall hall ON hall.id = schedule.hall_id
 		JOIN movie movie ON movie.id = schedule.movie_id
 		JOIN seat_scheme seat_scheme ON seat_scheme.id = hall.seat_scheme_id
+		JOIN seat_layout seat_layout ON seat_scheme.id = seat_layout.seat_scheme_id
+			AND seat_layout.row_number = _row AND seat_layout.cell_number = _cell
 		JOIN tariff tariff_movie ON tariff_movie.price_key_id = 1 
 			AND tariff_movie.price_category_id = movie.price_category_id
 		JOIN tariff tariff_hall ON tariff_hall.price_key_id = 2 
 			AND tariff_hall.price_category_id = hall.price_category_id
 		JOIN tariff tariff_seat ON tariff_seat.price_key_id = 3 
-			AND tariff_seat.price_category_id = ((seat_scheme.scheme::jsonb -> ('row_' || _r)) -> ('number_' || _s) -> 'locationId')::INTEGER
+			AND tariff_seat.price_category_id = seat_layout.location_id			
 		JOIN tariff tariff_schedule ON tariff_schedule.price_key_id = 4 
 			AND tariff_schedule.price_category_id = schedule.price_category_id
-		WHERE schedule.id = _i;
+		WHERE schedule.id = _schedule;
 	END IF;
 	
 	END LOOP;
