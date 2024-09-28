4355739691a50296e29bc1e6d155389fc3b2c0ca
evgenyart <albatros98@yandex.ru>
2024-09-29 01:47:35 +0300

    fixs2

--- app/src/App.php
+++ app/src/App.php
@@ -10,6 +10,24 @@ class App
 {
     public function run()
     {
-        new ElasticRun();
+        $args = $_SERVER['argv'];
+
+        $ExceptionError = "Необходимо ввести параметр `init` либо `search`";
+
+        if (!isset($args[1])) {
+            throw new Exception($ExceptionError);
+        }
+
+        switch ($args[1]) {
+            case 'init':
+                Run::InitSrorage();
+                break;
+            case 'search':
+                Run::SearshInBase($args);
+                break;
+            default:
+                throw new Exception($ExceptionError);
+                break;
+        }
     }
 }
---
+++ app/src/CommandsInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evgenyart\ElasticHomework;
+
+interface CommandsInterface
+{
+    public static function initSrorage();
+    public static function searshInBase($arguments);
+}
