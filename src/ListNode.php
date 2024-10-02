8fa5c7af627abfdedabd0c90ea1256dab9942b66
amikha1lov <alexey.mikha1lov@yandex.ru>
2024-10-03 00:36:06 +0300

    feat[leetcode]: https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/ListNode.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
