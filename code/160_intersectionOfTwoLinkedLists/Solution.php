9e767f946a871a5b70aff4b9a3846dbef512627b
PIvanov <xeolit@gmail.com>
2024-10-08 21:02:34 +0300

    hw19. Добавлены файлы с решениями задачи.

---
+++ code/160_intersectionOfTwoLinkedLists/Solution.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Otus\MergeTwoLists;
+
+// Вычислительная сложность O(N + M), где N и M - длины списков headA и headB.
+// В худшем случае алгоритм обойдёт все узлы обоих списков.
+
+// Пространственная сложность O(1) т.к. это простой перебор списков
+class Solution
+{
+    /**
+     * @param ListNode $headA
+     * @param ListNode $headB
+     * @return ListNode|null
+     */
+    function getIntersectionNode(ListNode $headA, ListNode $headB): ?ListNode
+    {
+        $pointerA = $headA;
+        $pointerB = $headB;
+
+        while ($pointerA !== $pointerB) {
+            $pointerA = $pointerA ? $pointerA->next : $headB;
+            $pointerB = $pointerB ? $pointerB->next : $headA;
+        }
+
+        return $pointerA;
+    }
+}
