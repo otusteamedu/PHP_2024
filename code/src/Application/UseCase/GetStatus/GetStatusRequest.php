443bb0ac44def9acc3f48136a78713d9dd22d222
Viktor Rikhert <viking311@gmail.com>
2024-10-16 22:37:27 +0700

    rework openapi to attributes

--- code/src/Application/UseCase/GetStatus/GetStatusRequest.php
+++ code/src/Application/UseCase/GetStatus/GetStatusRequest.php
@@ -13,5 +13,4 @@ class GetStatusRequest
         public string $id
     ) {
     }
-
 }
--- code/src/Infrastructure/Application.php
+++ code/src/Infrastructure/Application.php
@@ -9,56 +9,62 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Viking311\Api\Infrastructure\Factory\Command\AddEventCommandFactory;
 use Viking311\Api\Infrastructure\Factory\Command\GetStatusCommandFactory;
+use OpenApi\Attributes as OA;
 
-/**
- * @OA\Info(
- *     title="API заявок на проведение мероприятий",
- *     description="Приложение принимает заявки на проведение мероприятий.",
- *     version="1.0"
- * )
- */
+#[OA\Info(
+    version: '1.0',
+    description: 'Приложение принимает заявки на проведение мероприятий.',
+    title: 'API заявок на проведение мероприятий',
+)]
 class Application
 {
-    /**
-     * @OA\Post(
-     *     path="/api/v1/events",
-     *     @OA\RequestBody(
-     *         @OA\JsonContent(
-     *              ref="#/components/schemas/AddEventRequest",
-     *              example={"name": "Adam", "email": "example@example.com", "eventDate": "2024-12-25 12:00", "address": "address", "guest": 1}
-     *          )
-     *     ),
-     *     @OA\Response(
-     *         response="200",
-     *         description="Возвращает Id созданной заявки",
-     *         @OA\JsonContent(
-     *             ref="#/components/schemas/AddEventResponse",
-     *             example={"eventId": "670f69ee1f624"}
-     *         )
-     *    )
-     * )
-     *
-     * @OA\Get (
-     *     path="/api/v1/events/{eventId}",
-     *     description="Получение статуса заявки по её Id",
-     *     @OA\Parameter (
-     *         name="eventId",
-     *         in="path",
-     *         required=true,
-     *         @OA\Schema (
-     *             type="string"
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response="200",
-     *         description="Возвращает статус заявки, если она существует",
-     *         @OA\JsonContent(
-     *              ref="#/components/schemas/GetStatusResponse"
-     *         )
-     *     )
-     * )
-     */
-    public function run()
+    #[OA\Post(
+        path: '/api/v1/events',
+        description: 'Создание новой заявки',
+        requestBody: new OA\RequestBody(
+            content: new OA\JsonContent(
+                ref:'#/components/schemas/AddEventRequest'
+            ),
+        ),
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Возвращает Id созданной заявки',
+                content: new OA\JsonContent(
+                    ref:'#/components/schemas/AddEventResponse'
+                ),
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Не валидные данные
+                ',
+            )
+        ]
+    )]
+    #[OA\Get(
+        path: '/api/v1/events/{eventId}',
+        description: 'Получение статуса заявки по её Id',
+        parameters: [
+            new OA\Parameter(
+                name: 'eventId',
+                in: 'path',
+                required: true,
+                schema: new OA\Schema(
+                    type: 'string'
+                ),
+            ),
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Возвращает статус заявки, если она существует',
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/GetStatusResponse'
+                )
+            )
+        ]
+    )]
+    public function run(): void
     {
         $app = AppFactory::create();
 
