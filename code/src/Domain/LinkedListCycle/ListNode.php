aa40d1b10a282e6f72422437f65a16d9f33ef825
“Irina <irina.s.yurtaeva@gmail.com>
2024-10-19 21:17:13 +0200

    hw14: Algo: Linked list cycle

---
+++ code/src/Domain/LinkedListCycle/ListNode.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Irayu\Hw14\Domain\LinkedListCycle;
+
+class ListNode
+{
+     public $val = 0;
+     public $next = null;
+
+    public function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
