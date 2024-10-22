ae53b19bac1b43ec75598c8603b4962f569cbe3b
john9683 <john_9683@mail.ru>
2024-10-22 21:58:44 +0700

    hw13

---
+++ LetterCombinations.php
@@ -0,0 +1,47 @@
+<?php
+
+class LetterCombinations
+{
+    /**
+     * @param String $digits
+     * @return String[]
+     */
+    function getLetterCombinations(string $digits): array 
+    {
+        if ($digits === '') {
+            return [];
+        }
+
+        $btnArray = [
+            '2' => ['a', 'b', 'c'],
+            '3' => ['d', 'e', 'f'],
+            '4' => ['g', 'h', 'i'],
+            '5' => ['j', 'k', 'l'],
+            '6' => ['m', 'n', 'o'],
+            '7' => ['p', 'q', 'r', 's'],
+            '8' => ['t', 'u', 'v'],
+            '9' => ['w', 'x', 'y', 'z']
+        ];
+
+        $n = strlen($digits);
+        if ($n === 1) {
+            return $btnArray[$digits[0]];
+        }
+
+        $letterCombinations = [''];
+        for ($i = 0; $i < $n; $i++) {
+            $iterationArray = [];
+            $letters = $btnArray[$digits[$i]];
+
+            foreach ($letterCombinations as $combination) {
+                foreach ($letters as $letter) {
+                    $iterationArray[] = $combination . $letter;
+                }
+            }
+
+            $letterCombinations = $iterationArray;
+        }
+
+        return $letterCombinations;
+    }
+}
---
+++ LinkedList.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+class LinkedList
+{
+    /**
+     * @param ListNode $head
+     * @return Boolean
+     */
+    public function hasCycle(ListNode $head): bool
+    {
+        $lNode = $head;
+        $rNode = $head;
+
+        while ($rNode !== null && $rNode->next !== null) {
+            $lNode = $lNode->next;
+            $rNode = $rNode->next->next;
+
+            if ($rNode === null) {
+                return false;
+            } elseif ($lNode === $rNode) {
+                return true;
+            }
+        }
+
+        return false;
+    } 
+}
