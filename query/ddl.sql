4af949080d4a36ccab45be22c630b7c5d125cb70
Vladislav Sukhov <VSukhov@prosv.ru>
2024-10-22 17:12:07 +0300

    Homework 9

--- query/ddl.sql
+++ query/ddl.sql
@@ -33,6 +33,7 @@ CREATE TABLE attribute_values
     value_boolean BOOLEAN,
     value_date    DATE,
     value_float   FLOAT,
+    value_integer INT,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (movie_id) REFERENCES movies (movie_id),
     FOREIGN KEY (attribute_id) REFERENCES attributes (attribute_id)
@@ -45,13 +46,15 @@ CREATE INDEX idx_value_text ON attribute_values (value_text);
 CREATE INDEX idx_value_boolean ON attribute_values (value_boolean);
 CREATE INDEX idx_value_date ON attribute_values (value_date);
 CREATE INDEX idx_value_float ON attribute_values (value_float);
+CREATE INDEX idx_value_integer ON attribute_values (value_integer);
 
 -- Тут наши типы
 INSERT INTO attribute_types (type_name, sql_type)
 VALUES ('Текст', 'VARCHAR'),
        ('Логический', 'BOOLEAN'),
        ('Дата', 'DATE'),
-       ('Числовой', 'FLOAT');
+       ('Числовой', 'FLOAT'),
+       ('Целочисленный', 'FLOAT');
 
 -- Добавляем фильмы
 INSERT INTO movies (title, release_year)
@@ -67,52 +70,57 @@ VALUES ('Рецензии критиков', 1),
        ('Премьера', 3),
        ('Сборы в прокате', 4),
        ('Начало продаж', 3),
-       ('Запуск события', 3);
+       ('Запуск события', 3),
+       ('Количество проданных билетов', 5);
 
 
 -- Для фильма Начало
-INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float)
+INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float, value_integer)
 VALUES (1, 1,
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum.',
-        NULL, NULL, NULL),
-       (1, 2, NULL, TRUE, NULL, NULL),
-       (1, 3, NULL, NULL, '2020-07-16', NULL),
-       (1, 4, NULL, NULL, NULL, 829.9),
-       (1, 5, NULL, NULL, '2020-06-01', NULL),
-       (1, 6, NULL, NULL, '2020-05-15', NULL);
+        NULL, NULL, NULL, NULL),
+       (1, 2, NULL, TRUE, NULL, NULL, NULL),
+       (1, 3, NULL, NULL, '2020-07-16', NULL, NULL),
+       (1, 4, NULL, NULL, NULL, 829.9, NULL),
+       (1, 5, NULL, NULL, '2020-06-01', NULL, NULL),
+       (1, 6, NULL, NULL, '2020-05-15', NULL, NULL),
+       (1, 7, NULL, NULL, NULL, NULL, 58666);
 
 -- Для фильма Титаник
-INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float)
+INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float, value_integer)
 VALUES (2, 1,
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.',
-        NULL, NULL, NULL),
-       (2, 2, NULL, TRUE, NULL, NULL),
-       (2, 3, NULL, NULL, '2021-12-19', NULL),
-       (2, 4, NULL, NULL, NULL, 2187.5),
-       (2, 5, NULL, NULL, '2021-11-01', NULL),
-       (2, 6, NULL, NULL, '2021-10-20', NULL);
+        NULL, NULL, NULL, NULL),
+       (2, 2, NULL, TRUE, NULL, NULL, NULL),
+       (2, 3, NULL, NULL, '2021-12-19', NULL, NULL),
+       (2, 4, NULL, NULL, NULL, 2187.5, NULL),
+       (2, 5, NULL, NULL, '2021-11-01', NULL, NULL),
+       (2, 6, NULL, NULL, '2021-10-20', NULL, NULL),
+       (2, 6, NULL, NULL, NULL, NULL, 546515);
 
 -- Для фильма Интерстеллар
-INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float)
+INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float, value_integer)
 VALUES (3, 1,
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.',
-        NULL, NULL, NULL),
-       (3, 2, NULL, FALSE, NULL, NULL),
-       (3, 3, NULL, NULL, '2023-11-07', NULL),
-       (3, 4, NULL, NULL, NULL, 677.5),
-       (3, 5, NULL, NULL, '2023-09-25', NULL),
-       (3, 6, NULL, NULL, '2023-09-10', NULL);
+        NULL, NULL, NULL, NULL),
+       (3, 2, NULL, FALSE, NULL, NULL, NULL),
+       (3, 3, NULL, NULL, '2023-11-07', NULL, NULL),
+       (3, 4, NULL, NULL, NULL, 677.5, NULL),
+       (3, 5, NULL, NULL, '2023-09-25', NULL, NULL),
+       (3, 6, NULL, NULL, '2023-09-10', NULL, NULL),
+       (3, 6, NULL, NULL, NULL, NULL, 658565);
 
 -- Для фильма Матрица
-INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float)
+INSERT INTO attribute_values (movie_id, attribute_id, value_text, value_boolean, value_date, value_float, value_integer)
 VALUES (4, 1,
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam.',
-        NULL, NULL, NULL),
-       (4, 2, NULL, TRUE, NULL, NULL),
-       (4, 3, NULL, NULL, '2024-03-31', NULL),
-       (4, 4, NULL, NULL, NULL, 466.3),
-       (4, 5, NULL, NULL, '2024-02-10', NULL),
-       (4, 6, NULL, NULL, '2024-01-25', NULL);
+        NULL, NULL, NULL, NULL),
+       (4, 2, NULL, TRUE, NULL, NULL, NULL),
+       (4, 3, NULL, NULL, '2024-03-31', NULL, NULL),
+       (4, 4, NULL, NULL, NULL, 466.3, NULL),
+       (4, 5, NULL, NULL, '2024-02-10', NULL, NULL),
+       (4, 6, NULL, NULL, '2024-01-25', NULL, NULL),
+       (4, 6, NULL, NULL, NULL, NULL, 213212);
 
 CREATE VIEW service_tasks AS
 SELECT m.title          AS movie_title,
