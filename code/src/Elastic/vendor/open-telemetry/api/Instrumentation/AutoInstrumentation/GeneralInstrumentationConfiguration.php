02edd59090fcc6b4a260f12921a1941fa68e538c
kyberlox <alexxx.timofeev.00@gmail.com>
2024-10-23 13:43:47 +0400

    My elastic homework!

--- code/src/Elastic/vendor/open-telemetry/api/Instrumentation/AutoInstrumentation/GeneralInstrumentationConfiguration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace OpenTelemetry\API\Instrumentation\AutoInstrumentation;
-
-interface GeneralInstrumentationConfiguration extends InstrumentationConfiguration
-{
-}
--- code/src/Elastic/vendor/open-telemetry/context/fiber/initialize_fiber_handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace OpenTelemetry\Context;
-
-if (ZendObserverFiber::isEnabled()) {
-    ZendObserverFiber::init();
-}
