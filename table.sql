6efeaedebeb74eeb16cd8017254135b34a85e63d
Komarov Sergey <sk-2006@mail.ru>
2024-10-23 10:00:47 +0300

    hw9

--- table.sql
+++ table.sql
@@ -4,7 +4,7 @@ CREATE TABLE films
     film_name VARCHAR(255) NOT NULL
 );
 
-CREATE TYPE value_type_enum AS ENUM ('TEXT', 'BOOLEAN', 'DATE', 'NUMBER');
+CREATE TYPE value_type_enum AS ENUM ('TEXT', 'BOOLEAN', 'DATE', 'NUMBER', 'FLOAT');
 
 CREATE TABLE attribute_types
 (
@@ -30,6 +30,7 @@ CREATE TABLE attribute_values
     value_boolean      BOOLEAN,
     value_date         DATE,
     value_number       BIGINT,
+    value_float        NUMERIC,
     FOREIGN KEY (attribute_id) REFERENCES attributes (attribute_id)
 );
 
@@ -37,7 +38,8 @@ INSERT INTO films (film_name)
 VALUES ('Начало'),
        ('Крестный отец'),
        ('Паразиты'),
-       ('Интерстеллар');
+       ('Интерстеллар')
+;
 
 INSERT INTO attribute_types (attribute_name, value_type)
 VALUES ('Рецензия критиков', 'TEXT'),
@@ -45,7 +47,9 @@ VALUES ('Рецензия критиков', 'TEXT'),
        ('Премьера', 'DATE'),
        ('Дата начала продажи билетов', 'DATE'),
        ('Дата рекламной компании', 'DATE'),
-       ('Год выпуска', 'NUMBER');
+       ('Год выпуска', 'NUMBER'),
+       ('Сборы', 'FLOAT')
+;
 
 INSERT INTO attributes (film_id, attribute_type_id)
 VALUES (1, 1),
@@ -54,57 +58,70 @@ VALUES (1, 1),
        (1, 4),
        (1, 5),
        (1, 6),
+       (1, 7),
        (2, 1),
        (2, 2),
        (2, 3),
        (2, 4),
        (2, 5),
        (2, 6),
+       (2, 7),
        (3, 1),
        (3, 2),
        (3, 3),
        (3, 4),
        (3, 5),
        (3, 6),
+       (3, 7),
        (4, 1),
        (4, 2),
        (4, 3),
        (4, 4),
        (4, 5),
-       (4, 6);
+       (4, 6),
+       (4, 7)
+;
 
-INSERT INTO attribute_values (attribute_id, value_text, value_boolean, value_date, value_number)
-VALUES (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', NULL, NULL, NULL),
-       (2, NULL, TRUE, NULL, NULL),
-       (3, NULL, NULL, '2010-07-16', NULL),
-       (4, NULL, NULL, '2010-06-01', NULL),
-       (5, NULL, NULL, '2010-06-10', NULL),
-       (6, NULL, NULL, NULL, 2010),
-       (7, 'Lorem ipsum dolor sit amet...', NULL, NULL, NULL),
-       (8, NULL, TRUE, NULL, NULL),
-       (9, NULL, NULL, '1972-03-24', NULL),
-       (10, NULL, NULL, '1972-03-01', NULL),
-       (11, NULL, NULL, '1972-03-05', NULL),
-       (12, NULL, NULL, NULL, 1972),
-       (13, 'Lorem ipsum dolor sit amet...', NULL, NULL, NULL),
-       (14, NULL, TRUE, NULL, NULL),
-       (15, NULL, NULL, '2019-05-30', NULL),
-       (16, NULL, NULL, '2019-04-20', NULL),
-       (17, NULL, NULL, '2019-04-25', NULL),
-       (18, NULL, NULL, NULL, 2019),
-       (19, 'Lorem ipsum dolor sit amet...', NULL, NULL, NULL),
-       (20, NULL, FALSE, NULL, NULL),
-       (21, NULL, NULL, '2014-11-07', NULL),
-       (22, NULL, NULL, '2014-10-01', NULL),
-       (23, NULL, NULL, '2014-10-05', NULL),
-       (24, NULL, NULL, NULL, 2014);
+INSERT INTO attribute_values (attribute_id, value_text, value_boolean, value_date, value_number, value_float)
+VALUES (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', NULL, NULL, NULL, NULL),
+       (2, NULL, TRUE, NULL, NULL, NULL),
+       (3, NULL, NULL, '2010-07-16', NULL, NULL),
+       (4, NULL, NULL, '2010-06-01', NULL, NULL),
+       (5, NULL, NULL, '2010-06-10', NULL, NULL),
+       (6, NULL, NULL, NULL, 2010, NULL),
+       (7, 'Lorem ipsum dolor sit amet...', NULL, NULL, NULL, NULL),
+       (8, NULL, TRUE, NULL, NULL, NULL),
+       (9, NULL, NULL, '1972-03-24', NULL, NULL),
+       (10, NULL, NULL, '1972-03-01', NULL, NULL),
+       (11, NULL, NULL, '1972-03-05', NULL, NULL),
+       (12, NULL, NULL, NULL, 1972, NULL),
+       (13, 'Lorem ipsum dolor sit amet...', NULL, NULL, NULL, NULL),
+       (14, NULL, TRUE, NULL, NULL, NULL),
+       (15, NULL, NULL, '2019-05-30', NULL, NULL),
+       (16, NULL, NULL, '2019-04-20', NULL, NULL),
+       (17, NULL, NULL, '2019-04-25', NULL, NULL),
+       (18, NULL, NULL, NULL, 2019, NULL),
+       (19, 'Lorem ipsum dolor sit amet...', NULL, NULL, NULL, NULL),
+       (20, NULL, FALSE, NULL, NULL, NULL),
+       (21, NULL, NULL, '2014-11-07', NULL, NULL),
+       (22, NULL, NULL, '2014-10-01', NULL, NULL),
+       (23, NULL, NULL, '2014-10-05', NULL, NULL),
+       (24, NULL, NULL, NULL, 2014, NULL),
+       (25, NULL, NULL, NULL, NULL, 1.1),
+       (26, NULL, NULL, NULL, NULL, 3.33),
+       (27, NULL, NULL, NULL, NULL, 10.3),
+       (28, NULL, NULL, NULL, NULL, 100.3)
+;
 
 CREATE INDEX idx_films_film_id ON films (film_id);
 CREATE INDEX idx_attribute_types_attribute_type_id ON attribute_types (attribute_type_id);
 CREATE INDEX idx_attributes_film_id ON attributes (film_id);
 CREATE INDEX idx_attributes_attribute_type_id ON attributes (attribute_type_id);
 CREATE INDEX idx_attribute_values_attribute_id ON attribute_values (attribute_id);
-CREATE INDEX idx_attribute_values_value_date ON attribute_values (value_date);
+CREATE INDEX idx_attribute_values_date ON attribute_values (value_date);
+CREATE INDEX idx_attribute_values_float ON attribute_values (value_float);
+CREATE INDEX idx_attribute_values_boolean ON attribute_values (value_boolean);
+CREATE INDEX idx_attribute_values_number ON attribute_values (value_number);
 
 CREATE VIEW service_tasks AS
 SELECT f.film_name       AS "Фильм",
@@ -125,6 +142,7 @@ SELECT f.film_name       AS "Фильм",
            WHEN at.value_type = 'BOOLEAN' THEN CASE WHEN av.value_boolean THEN 'Да' ELSE 'Нет' END
            WHEN at.value_type = 'DATE' THEN TO_CHAR(av.value_date, 'YYYY-MM-DD')
            WHEN at.value_type = 'NUMBER' THEN av.value_number
+           WHEN at.value_type = 'FLOAT' THEN av.value_float
            END           AS "Значение"
 FROM films f
          JOIN attributes a ON f.film_id = a.film_id
