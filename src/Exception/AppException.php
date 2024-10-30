81fc5a03dcce0a75df6c2a3bc43fa83e16aa53fc
Vladislav Sukhov <VSukhov@prosv.ru>
2024-10-30 12:38:38 +0300

    Homework 13

---
+++ src/Exception/AppException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VSukhov\Hw8\Exception;
+
+namespace VSukhov\Hw8\Exception;
+
+use Exception;
+
+class AppException extends Exception
+{
+}
---
+++ src/app.php
@@ -0,0 +1,11 @@
+<?php
+
+use VSukhov\Hw13\App\App;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+try {
+    (new App())->run();
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
