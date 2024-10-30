06ee40e7ea88036313f986b8750ef20fb3e911f6
Sergey Komarov <sk-2006@mail.ru>
2024-10-30 12:52:18 +0300

    hw13

--- LinkedList.php
+++ LinkedList.php
@@ -7,7 +7,8 @@ class ListNode
     public int $val = 0;
     public ?ListNode $next = null;
 
-    public function __construct(int $val = 0, ListNode $next = null) {
+    public function __construct(int $val = 0, ListNode $next = null)
+    {
         $this->val = $val;
         $this->next = $next;
     }
