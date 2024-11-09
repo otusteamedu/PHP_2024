d39fa4cf6cef7ac94249ce6a22869c26f79151a4
Andrey Kataev <andryjka@gmail.com>
2024-11-09 23:34:36 +0600

    sum bash script

---
+++ sum.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Ошибка: требуется два аргумента"
+  exit 1
+fi
+
+numReg="^-?[0-9]+([.][0-9]+)?$"
+
+if ! [[ $1 =~ $numReg && $2 =~ $numReg ]]; then
+  echo "Введите корректные числа"
+  exit 1
+fi
+
+result=$(echo "$1 + $2" | bc)
+
+echo "Результат сложения $1 + $2 = $result"
