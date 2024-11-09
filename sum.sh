ce53756d181ac37562e1ba4fa5cfc5eff6d4da38
Andrey Kataev <andryjka@gmail.com>
2024-11-09 23:44:55 +0600

    bc install

--- sum.sh
+++ sum.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if ! command -v bc >/dev/null 2>&1; then
+  echo "Команда 'bc' не установлена. Попытка установки..."
+
+  #ubuntu
+  if command -v apt >/dev/null 2>&1; then
+    sudo apt update && sudo apt install -y bc
+  fi
+fi
+
 if [ "$#" -ne 2 ]; then
   echo "Ошибка: требуется два аргумента"
   exit 1
@@ -11,6 +20,7 @@ if ! [[ $1 =~ $numReg && $2 =~ $numReg ]]; then
   echo "Введите корректные числа"
   exit 1
 fi
+
 
 result=$(echo "$1 + $2" | bc)
 
