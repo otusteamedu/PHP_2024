fbe5368aea2ae46480ca9e72245cba8af863ac20
Качко Игорь <igor@kr.digital>
2024-11-22 11:01:58 +0300

    hw4: ex1 & ex2

--- ex2/src/index.php
+++ ex2/src/index.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 //session_start();
 
-echo "<pre>";
-var_export($_SERVER["SERVER_ADDR"] . "," .$_SERVER["HOSTNAME"] );
-echo "</pre>";
+echo $_SERVER["SERVER_ADDR"] . ", " .$_SERVER["HOSTNAME"];
