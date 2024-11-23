9ea3bbbdeed6b575e50ae1bf43c4c7d6a998dbef
Rommshef <git\rommshef@gmail.com>
2024-11-23 16:33:06 +0300

    fix code add composer and php classes

--- balancer/volumes/code/index.php
+++ balancer/volumes/code/index.php
@@ -2,32 +2,8 @@
 
 declare(strict_types=1);
 
-if (isset($_POST['string']) && !empty(trim($_POST['string']))) {
-    $string = trim($_POST['string']);
-    $string = str_replace(' ', '', $string);
-    if (str_contains($string, ')') || str_contains($string, '(')) {
-        $str_arr = str_split($string);
-        $counter = 0;
-        foreach ($str_arr as $char) {
-            if ($char === '(') {
-                $counter++;
-            } elseif ($char === ')') {
-                $counter--;
-            }
-        }
-        if ($counter === 0) {
-            header("HTTP/1.1 200 OK");
-            echo 'Правильная скобочная последовательность' . PHP_EOL;
-        } else {
-            header("HTTP/1.1 400 Bad Request");
-            echo 'Все плохо: неправильная скобочная последовательность' . PHP_EOL;
-        }
-    } else {
-        header("HTTP/1.1 400 Bad Request");
-        echo 'Все плохо: скобок не обнаружено' . PHP_EOL;
-    }
-} else {
-    header("HTTP/1.1 400 Bad Request");
-    echo 'Все плохо: ничего не передали в параметр "string"' . PHP_EOL;
-}
+require './vendor/autoload.php';
 
+use RShevtsov\Hw4\App;
+
+echo (new App())->run();
