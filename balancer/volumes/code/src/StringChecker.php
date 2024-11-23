a1482cef15026469525368d6f16f8a88f5b14906
Rommshef <git\rommshef@gmail.com>
2024-11-23 16:49:24 +0300

    fix blank line

--- balancer/volumes/code/src/StringChecker.php
+++ balancer/volumes/code/src/StringChecker.php
@@ -15,38 +15,26 @@ class StringChecker
 
         $string = trim($string);
         $string = str_replace(' ', '', $string);
-
         if (str_contains($string, ')') || str_contains($string, '(')) {
-
             $str_arr = str_split($string);
-
             $counter = 0;
             foreach ($str_arr as $char) {
-
                 if ($char === '(') {
                     $counter++;
                 } elseif ($char === ')') {
                     $counter--;
                 }
-
             }
             if ($counter === 0) {
-
                 header("HTTP/1.1 200 OK");
                 return 'Правильная скобочная последовательность' . PHP_EOL;
-
             } else {
-
                 header("HTTP/1.1 400 Bad Request");
                 return 'Все плохо: неправильная скобочная последовательность' . PHP_EOL;
-
             }
-
         } else {
-
             header("HTTP/1.1 400 Bad Request");
             return 'Все плохо: скобок не обнаружено' . PHP_EOL;
-
         }
     }
 }
