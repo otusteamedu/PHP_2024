1919492baf206a36f07130e058e6a687b729fd9f
Alexander Syrovatkin <a.syrovatkin@4slovo.ru>
2024-11-26 22:43:58 +0300

    hw4

---
+++ code/index.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+$PARAM_NAME = 'string';
+$OPEN = '(';
+$CLOSE = ')';
+$cnt = 0;
+
+$string =  $_POST[$PARAM_NAME];
+
+if (strlen($string) == 0) error();
+
+for ($i = 0; $i < strlen($string); $i++) {
+    switch ($string[$i]) {
+        case $OPEN: $cnt++; break;
+        case $CLOSE: $cnt--; break;
+        default: error();
+    }
+    if ($cnt < 0) error();
+}
+if ($cnt !== 0) {
+    error();
+}
+
+function error(): void
+{
+    http_response_code(400); exit;
+}
