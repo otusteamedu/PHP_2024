d35cef921fb6e201a683c10c74d90467bee40a08
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-11-26 19:54:52 +0300

    Homework4-2

--- app/public/index.php
+++ app/public/index.php
@@ -6,4 +6,12 @@ require '../vendor/autoload.php';
 
 use AnatolyShilyaev\App\App;
 
+use Redis;
+
 echo (new App())->run();
+
+$redis = new Redis();
+$redis->connect('localhost', 6379);
+echo $redis->ping();
+
+phpinfo();
