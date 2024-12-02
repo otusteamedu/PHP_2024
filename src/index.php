79c401a4d9c50f7c8e91274c8a019284f026e65d
a.pecherskih <a.pecherskih@noone.ru>
2024-12-02 15:14:11 +0400

    [HW3] Composer package

--- src/index.php
+++ src/index.php
@@ -1,46 +1,17 @@
 <?php
 
-$env = parse_ini_file('.env');
+use PetrovaA\VerifyEmail\EmailVerifier;
 
-redis($env);
-mysql($env);
-memcached($env);
+require './vendor/autoload.php';
 
+$checkedEmails = [
+    'test@mail.ru' => false,
+    'test@email.ru' => false
+];
 
-function redis($env)
-{
-    $redis = new Redis();
-    $redis->connect($env['REDIS_HOST'], $env['REDIS_PORT']);
-
-    try {
-        $redis->ping();
-        echo "Redis connected successfully" . PHP_EOL;
-    } catch (Exception $e) {
-        die("Connection failed: " . $e->getMessage());
-    }
+foreach ($checkedEmails as $email => $isChecked) {
+    $isChecked = EmailVerifier::verifyEmail(email: $email, checkDNS: true);
+    $checkedEmails[$email] = $isChecked;
 }
 
-
-function mysql($env)
-{
-    $conn = new mysqli($env['DB_HOST'], $env['DB_USERNAME'], $env['DB_PASSWORD'], $env['DB_DATABASE']);
-
-    if ($conn->connect_error) {
-        die("Mysql connection failed: " . $conn->connect_error);
-    } else {
-        echo "Mysql connected successfully" . PHP_EOL;
-    }
-}
-
-function memcached($env)
-{
-    $mc = new Memcached();
-    $mc->addServer($env['MEMCACHED_HOST'], $env['MEMCACHED_PORT']);
-    $statuses = $mc->getStats();
-
-    if ($statuses[$env['MEMCACHED_HOST'] . ':' . $env['MEMCACHED_PORT']]['uptime'] < 1) {
-        die("Memcached connection failed");
-    } else {
-        echo "Memcached connected successfully" . PHP_EOL;
-    }
-}
+print_r($checkedEmails);
