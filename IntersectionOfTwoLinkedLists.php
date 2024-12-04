4b2901a9d45ac2d723a0586b761054c545b3a76e
john9683 <john_9683@mail.ru>
2024-12-04 11:15:18 +0700

    hw-19

---
+++ IntersectionOfTwoLinkedLists.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+
+class Solution 
+{
+    /**
+    * @param ListNode $headA
+    * @param ListNode $headB
+    * @return ListNode
+    */
+    public function getIntersectionNode($headA, $headB) 
+    {
+        if ($headA === $headB) {
+            return $headB;
+        }
+
+        $node1 = $headA;
+        $node2 = $headB;
+
+        while ($node1 !== $node2) {
+            $node1 = $node1 == null ? $headB : $node1->next;
+            $node2 = $node2 == null ? $headA : $node2->next;
+        }
+        
+        return $node2;
+    }
+}
