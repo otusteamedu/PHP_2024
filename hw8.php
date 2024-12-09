d2796cbfaa92be581f47257dfbdd264936042a89
tshakirov <t.shakirov@truckradar.com>
2024-12-09 19:37:47 +0300

    done v1

---
+++ hw8.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution
+{
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    public function mergeTwoLists($list1, $list2)
+    {
+        $dummy = new ListNode();
+        $current = $dummy;
+
+        while ($list1 !== null && $list2 !== null) {
+            if ($list1->val < $list2->val) {
+                $current->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $current->next = $list2;
+                $list2 = $list2->next;
+            }
+            $current = $current->next;
+        }
+
+        $current->next = $list1 ?? $list2;
+
+        return $dummy->next;
+    }
+}
