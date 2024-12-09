3d7bc381e14bc6d665f49920e471eb791e8ee532
Alexander Syrovatkin <a.syrovatkin@4slovo.ru>
2024-12-09 22:50:23 +0300

    hw6 - convert to PostgreSQL

--- ddl.sql
+++ ddl.sql
@@ -1,74 +1,61 @@
-DROP DATABASE IF EXISTS `cinema_db`;
+DROP DATABASE IF EXISTS cinema_db;
 
-CREATE SCHEMA IF NOT EXISTS `cinema_db` DEFAULT CHARACTER SET utf8;
-USE `cinema_db`;
+CREATE DATABASE cinema_db WITH ENCODING 'UTF8';
 
-CREATE TABLE IF NOT EXISTS `cinema_db`.`halls`
+\c  cinema_db
+
+CREATE TABLE IF NOT EXISTS halls
 (
-    `id`         INT         NOT NULL AUTO_INCREMENT,
-    `title`      VARCHAR(45) NULL,
-    `seat_count` INT         NULL,
-    PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB;
+    id         SERIAL PRIMARY KEY,
+    title      VARCHAR(45),
+    seat_count INT
+);
 
-CREATE TABLE IF NOT EXISTS `cinema_db`.`films`
+CREATE TABLE IF NOT EXISTS films
 (
-    `id`          INT         NOT NULL AUTO_INCREMENT,
-    `title`       VARCHAR(45) NULL,
-    `rental_cost` FLOAT       NULL,
-    PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB;
+    id          SERIAL PRIMARY KEY,
+    title       VARCHAR(45),
+    rental_cost FLOAT
+);
 
-CREATE TABLE IF NOT EXISTS `cinema_db`.`sessions`
+CREATE TABLE IF NOT EXISTS sessions
 (
-    `id`         INT  NOT NULL AUTO_INCREMENT,
-    `start_time` TIME NULL,
-    PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB;
+    id         SERIAL PRIMARY KEY,
+    start_time TIME
+);
 
-CREATE TABLE IF NOT EXISTS `cinema_db`.`pivot_with_prices`
+CREATE TABLE IF NOT EXISTS pivot_with_base_prices
 (
-    `id`         INT   NOT NULL AUTO_INCREMENT,
-    `hall_id`    INT   NULL,
-    `film_id`    INT   NULL,
-    `session_id` INT   NULL,
-    `price`      FLOAT NULL,
-    PRIMARY KEY (`id`),
+    id         SERIAL PRIMARY KEY,
+    hall_id    INT,
+    film_id    INT,
+    session_id INT,
+    price      FLOAT,
     UNIQUE (hall_id, film_id, session_id),
-    INDEX `fk_pivot_films_idx` (`film_id` ASC) VISIBLE,
-    INDEX `fk_pivot_halls_idx` (`hall_id` ASC) VISIBLE,
-    INDEX `fk_session_idx` (`session_id` ASC) VISIBLE,
-    CONSTRAINT `fk_pivot_films`
-        FOREIGN KEY (`film_id`)
-            REFERENCES `cinema_db`.`films` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `fk_pivot_halls`
-        FOREIGN KEY (`hall_id`)
-            REFERENCES `cinema_db`.`halls` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `fk_session`
-        FOREIGN KEY (`session_id`)
-            REFERENCES `cinema_db`.`sessions` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
+    CONSTRAINT fk_pivot_films FOREIGN KEY (film_id)
+        REFERENCES films (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_pivot_halls FOREIGN KEY (hall_id)
+        REFERENCES halls (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_session FOREIGN KEY (session_id)
+        REFERENCES sessions (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
 
-CREATE TABLE IF NOT EXISTS `cinema_db`.`tickets`
+
+CREATE TABLE IF NOT EXISTS tickets
 (
-    `id_pivot_with_prices` INT,
-    `seat`                 INT,
-    `session_date`         DATE,
-    PRIMARY KEY (`id_pivot_with_prices`, `seat`, `session_date`),
-    CONSTRAINT `fk_pivot_with_prices`
-        FOREIGN KEY (`id_pivot_with_prices`)
-            REFERENCES `cinema_db`.`pivot_with_prices` (`id`)
+    id_pivot_with_base_prices INT,
+    seat                 INT,
+    session_date         DATE,
+    PRIMARY KEY (id_pivot_with_base_prices, seat, session_date),
+    CONSTRAINT fk_pivot_with_base_prices
+        FOREIGN KEY (id_pivot_with_base_prices)
+            REFERENCES pivot_with_base_prices (id)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
+);
--- qyery.sql
+++ qyery.sql
@@ -1,15 +1,24 @@
-USE `cinema_db`;
-WITH revenue
-         AS (SELECT f.id,
-                    f.rental_cost,
-                    f.title,
-                    SUM(pwp.price) AS revenue_sum
-             FROM tickets t
-                      JOIN cinema_db.pivot_with_prices pwp on pwp.id = t.id_pivot_with_prices
-                      JOIN cinema_db.films f on f.id = pwp.film_id
-             GROUP BY f.id, f.rental_cost, f.title)
-SELECT r.title,
-       (r.revenue_sum - r.rental_cost) AS income
-FROM revenue r
-ORDER BY income desc
+\c  cinema_db
+WITH revenue AS (
+    SELECT
+        f.id,
+        f.rental_cost,
+        f.title,
+        SUM(pwp.price) AS revenue_sum
+    FROM
+        tickets t
+            JOIN
+        pivot_with_base_prices pwp ON pwp.id = t.id_pivot_with_base_prices
+            JOIN
+        films f ON f.id = pwp.film_id
+    GROUP BY
+        f.id, f.rental_cost, f.title
+)
+SELECT
+    r.title,
+    (r.revenue_sum - r.rental_cost) AS income
+FROM
+    revenue r
+ORDER BY
+    income DESC
 LIMIT 1;
