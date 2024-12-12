2c032c2171bcf44d420861e63d2376012ebb48d3
Качко Игорь <igor@kr.digital>
2024-12-12 12:34:54 +0300

    hw16: fix

--- app/src/Services/News/Strategy/NewsItemHtmlContent.php
+++ app/src/Services/News/Strategy/NewsItemHtmlContent.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Services\News\Strategy;
 
-use App\Services\News\Decorator\NewsItemAddTimeToReadContent;
 use App\Services\News\Decorator\NewsItemContentAddSubscribeBlock;
+use App\Services\News\Decorator\NewsItemContentAddTimeToRead;
 use App\Services\News\Decorator\NewsItemContentBase;
 
 class NewsItemHtmlContent implements NewsItemContent
@@ -13,7 +13,7 @@ class NewsItemHtmlContent implements NewsItemContent
     public function getContent(string $content): string
     {
         $newsItemContent = new NewsItemContentBase($content);
-        $newsItemContent = new NewsItemAddTimeToReadContent($newsItemContent);
+        $newsItemContent = new NewsItemContentAddTimeToRead($newsItemContent);
         $newsItemContent = new NewsItemContentAddSubscribeBlock($newsItemContent);
 
         return $newsItemContent->getContent();
