b919da07998d08800a3427f68aaf7c9895f3da85
Aliaksei Verameichyk <aliaksei.verameichyk@gmail.com>
2024-12-13 16:01:33 +0100

    minor fixes

--- public/index.php
+++ public/index.php
@@ -1,25 +1,8 @@
 <?php
 
-use App\ResponseHandler;
-use App\Validator;
+use App\App;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$responseHandler = new ResponseHandler();
-$validator = new Validator();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $input = $_POST['string'] ?? '';
-
-    if (empty($input)) {
-        $responseHandler->sendResponse(400, "Bad Request: The input string is empty.");
-    }
-
-    if ($validator->isValidParentheses($input)) {
-        $responseHandler->sendResponse(200, "OK: The parentheses string is valid.");
-    } else {
-        $responseHandler->sendResponse(400, "Bad Request: The parentheses string is invalid.");
-    }
-} else {
-    $responseHandler->sendResponse(405, "Method Not Allowed: Use POST.");
-}
+$app = new App();
+$app->run();
