e87499401f5c87f676aabb5c8f3abbdddd547af5
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-12-22 17:05:05 +0300

    homework-8

--- DDL.sql
+++ DDL.sql
@@ -1,34 +1,31 @@
-CREATE TABLE `Sessions` (
-	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	`movie_id` INTEGER NOT NULL,
-	`hall_id` INTEGER NOT NULL,
-	`date` DATE NOT NULL,
-	`begin_time` TIME NOT NULL,
-	`duration` INTEGER NOT NULL,
-	PRIMARY KEY(`id`)
-);
-
-
 CREATE TABLE `Movies` (
-	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	`name` VARCHAR(255) NOT NULL,
+	`id`	INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`name`	VARCHAR(255) NOT NULL,
 	PRIMARY KEY(`id`)
 );
 
-
-CREATE TABLE `Halls` (
-	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	`number` INTEGER NOT NULL,
-	`seats_count` INTEGER NOT NULL,
+CREATE TABLE Attribute_types
+(
+    `id`	INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    `name`	VARCHAR(120) NOT NULL UNIQUE,
 	PRIMARY KEY(`id`)
 );
 
-
-CREATE TABLE `Tickets` (
-	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	`session_id` INTEGER NOT NULL,
-	`price` DECIMAL NOT NULL,
-	`seat_number` INTEGER NOT NULL,
-	`is_sold` BOOLEAN NOT NULL,
+CREATE TABLE Attributes
+(
+    `id`				INTEGER 	NOT NULL 	AUTO_INCREMENT UNIQUE,
+    `attribute_type_id`	INTEGER 	NOT NULL 	REFERENCES Attribute_types (id) ON DELETE CASCADE,
+	`name` 				VARCHAR(255) NOT NULL,
 	PRIMARY KEY(`id`)
 );
+
+CREATE TABLE Values
+(
+    `id`				INTEGER 	NOT NULL 	AUTO_INCREMENT UNIQUE,
+	`movie_id`			INTEGER 	NOT NULL 	REFERENCES Movies (id) ON DELETE CASCADE,
+    `attribute_id`   	INTEGER 	NOT NULL 	REFERENCES Attribute (id) ON DELETE CASCADE,
+	`value`				TEXT		NOT NULL
+	PRIMARY KEY(`id`)
+);
+
+CREATE INDEX idx_attribute_type_movie ON Attributes (attribute_type_id);
