27380117cc83ce6d19c632a1f5fcfc8b2684837c
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-12-23 13:08:39 +0300

    homework-8-2

--- DATA.sql
+++ DATA.sql
@@ -6,35 +6,36 @@ INSERT INTO Attribute_types (name)
 VALUES ('text'),
        ('boolean'),
        ('date'),
+       ('timestamp');
 
 INSERT INTO Attributes (attribute_type_id, name)
 VALUES (1, 'рецензии'),
        (2, 'премия'),
        (3, 'важные даты'),
-       (3, 'служебные даты');
+       (4, 'служебные даты');
 
---Иван Васильевич меняет профессию
-INSERT INTO Values (movie_id, attribute_id, value_text)
+-- Иван Васильевич меняет профессию
+INSERT INTO `Values` (movie_id, attribute_id, `value_text`)
 VALUES (1, 1, 'Хороший новогодний фильм');
 
-INSERT INTO Values (movie_id, attribute_id, value_boolean)
+INSERT INTO `Values` (movie_id, attribute_id, `value_boolean`)
 VALUES (1, 2, TRUE);
 
-INSERT INTO Values (movie_id, attribute_id, value_date)
+INSERT INTO `Values` (movie_id, attribute_id, `value_date`)
 VALUES (1, 3, '1973-09-17');
 
-INSERT INTO Values (movie_id, attribute_id, value_timestamp)
+INSERT INTO `Values` (movie_id, attribute_id, `value_timestamp`)
 VALUES (1, 4, CURRENT_TIMESTAMP);
 
---Superman
-INSERT INTO Values (movie_id, attribute_id, value_text)
+-- Superman
+INSERT INTO `Values` (movie_id, attribute_id, `value_text`)
 VALUES (2, 1, 'Супергеройский фильм');
 
-INSERT INTO Values (movie_id, attribute_id, value_boolean)
+INSERT INTO `Values` (movie_id, attribute_id, `value_boolean`)
 VALUES (2, 2, FALSE);
 
-INSERT INTO Values (movie_id, attribute_id, value_date)
+INSERT INTO `Values` (movie_id, attribute_id, `value_date`)
 VALUES (2, 3, '2025-07-25');
 
-INSERT INTO Values (movie_id, attribute_id, value_timestamp)
+INSERT INTO `Values` (movie_id, attribute_id, `value_timestamp`)
 VALUES (2, 4, CURRENT_TIMESTAMP);
--- DDL.sql
+++ DDL.sql
@@ -4,14 +4,14 @@ CREATE TABLE `Movies` (
 	PRIMARY KEY(`id`)
 );
 
-CREATE TABLE Attribute_types
+CREATE TABLE `Attribute_types`
 (
     `id`	INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
     `name`	VARCHAR(120) NOT NULL UNIQUE,
 	PRIMARY KEY(`id`)
 );
 
-CREATE TABLE Attributes
+CREATE TABLE `Attributes`
 (
     `id`				INTEGER 	NOT NULL 	AUTO_INCREMENT UNIQUE,
     `attribute_type_id`	INTEGER 	NOT NULL 	REFERENCES Attribute_types (id) ON DELETE CASCADE,
@@ -19,13 +19,17 @@ CREATE TABLE Attributes
 	PRIMARY KEY(`id`)
 );
 
-CREATE TABLE Values
+CREATE TABLE `Values`
 (
     `id`				INTEGER 	NOT NULL 	AUTO_INCREMENT UNIQUE,
 	`movie_id`			INTEGER 	NOT NULL 	REFERENCES Movies (id) ON DELETE CASCADE,
-    `attribute_id`   	INTEGER 	NOT NULL 	REFERENCES Attribute (id) ON DELETE CASCADE,
-	`value`				TEXT		NOT NULL
+    `attribute_id`   	INTEGER 	NOT NULL 	REFERENCES Attributes (id) ON DELETE CASCADE,
+	`value_text`      	TEXT  					DEFAULT NULL,
+    `value_boolean`   	BOOLEAN  				DEFAULT NULL,
+    `value_date`      	DATE  					DEFAULT NULL,
+    `value_timestamp` 	TIMESTAMP  				DEFAULT NULL,
 	PRIMARY KEY(`id`)
 );
 
 CREATE INDEX idx_attribute_type_movie ON Attributes (attribute_type_id);
+CREATE INDEX idx_value_by_date ON `Values` (value_timestamp);
