306c8f4bc93544433f940764af4c78abb4a674cc
sibgatullin rinat <bulgar989@gmail.com>
2024-12-24 11:50:24 +0300

    fxi

--- leetcode.php
+++ leetcode.php
@@ -1,46 +1,69 @@
 <?php
 
-/**
- * Definition for a singly-linked list.
- * class ListNode {
- *     public $val = 0;
- *     public $next = null;
- *     function __construct($val = 0, $next = null) {
- *         $this->val = $val;
- *         $this->next = $next;
- *     }
- * }
- */
-class Solution {
-
-    /**
-     * @param ListNode $list1
-     * @param ListNode $list2
-     * @return ListNode
-     */
-    function mergeTwoLists($list1, $list2) {
-        $rawResul = null;
-        while (1) {
-            if ($list1 && !$list2 || ((!is_null($list1) && !is_null($list1)) && $list1->val <= $list2->val)) {
-                $rawResul = new ListNode($list1->val, $rawResul);
-                $list1 = $list1->next;
-            } elseif (!$list1 && $list2 || ((!is_null($list1) && !is_null($list1)) && $list1->val > $list2->val)) {
-                $rawResul = new ListNode($list2->val, $rawResul);
-                $list2 = $list2->next;
-            } else {
-                break;
-            }
-        }
-
-        while (1) {
-            $finalResul = new ListNode($rawResul->val, $finalResul);
-            if ($rawResul->next) {
-                $rawResul = $rawResul->next;
-            } else {
-                break;
-            }
-        }
-
-        return $finalResul;
+class ListNode {
+    public $val = 0;
+    public $next = null;
+    function __construct($val = 0, $next = null) {
+        $this->val = $val;
+        $this->next = $next;
     }
 }
+
+class Solution {
+    function mergeTwoLists($list1, $list2) {
+        $dummy = new ListNode();
+        $current = $dummy;
+
+        while ($list1 !== null && $list2 !== null) {
+            if ($list1->val <= $list2->val) {
+                $current->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $current->next = $list2;
+                $list2 = $list2->next;
+            }
+            $current = $current->next;
+        }
+
+        // Присоединяем оставшиеся узлы, если есть
+        if ($list1 !== null) {
+            $current->next = $list1;
+        } else {
+            $current->next = $list2;
+        }
+
+        return $dummy->next;
+    }
+}
+
+function createLinkedList($array) {
+    $head = null;
+    $current = null;
+    foreach ($array as $value) {
+        if ($head === null) {
+            $head = new ListNode($value);
+            $current = $head;
+        } else {
+            $current->next = new ListNode($value);
+            $current = $current->next;
+        }
+    }
+    return $head;
+}
+
+function printLinkedList($list) {
+    while ($list !== null) {
+        echo $list->val . " -> ";
+        $list = $list->next;
+    }
+    echo "null\n";
+}
+
+
+$list1 = createLinkedList([1, 2, 4]);
+$list2 = createLinkedList([1, 3, 4]);
+
+$solution = new Solution();
+$mergedList = $solution->mergeTwoLists($list1, $list2);
+
+printLinkedList($mergedList); // 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> null
