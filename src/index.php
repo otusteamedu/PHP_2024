f3d8d2f7d7bd51eceac858ef2a59d73389cd8c84
Nvlyrs <peskovatzkov.vs@gmail.com>
2025-01-01 15:50:05 +0300

    [hw3] - добавил пакет

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Apeskovatzkov\ChristmasTreeCreator\ChristmasTreeCreator;
+
+ChristmasTreeCreator::drawTree(20);
