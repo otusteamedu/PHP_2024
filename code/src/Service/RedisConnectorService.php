5b466309b7739292ed60ac9ece161387ff25d1d1
Качко Игорь <igor@kr.digital>
2025-01-03 18:33:22 +0300

    hw11

--- code/src/Service/RedisConnectorService.php
+++ code/src/Service/RedisConnectorService.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use Redis;
+
 class RedisConnectorService
 {
-    public function connect(): \Redis {
-        $redis = new \Redis();
-        $redis->connect('redis');
+    public function __construct(private readonly string $redisHost)
+    {
+    }
+
+    public function connect(): Redis {
+        $redis = new Redis();
+        $redis->connect($this->redisHost);
 
         return $redis;
     }
