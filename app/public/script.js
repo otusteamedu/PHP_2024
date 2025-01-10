1cbf5a64bf6d6ccb677538d504bce53860308fb5
Anatoly <Anatoly.Shilyaev@gmail.com>
2025-01-10 12:45:54 +0300

    homework-11

---
+++ app/public/script.js
@@ -0,0 +1,22 @@
+// Функция для отправки AJAX-запроса
+function buttonClick(method, action) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "index.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  // Обрабатываем ответ и обновляем страницу
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      document.getElementById("response").innerHTML = xhr.responseText;
+    }
+  };
+
+  // Отправляем данные
+  xhr.send("method=" + method + "&action=" + action);
+}
+
+const check = () => {
+  console.log("sdfhjkl");
+};
+
+window.onload(document.getElementById("check").onclick(check));
