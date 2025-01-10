eecbfdf935668e655320b7dffd5e1d972f33a218
Anatoly <Anatoly.Shilyaev@gmail.com>
2025-01-10 12:39:49 +0300

    homework-11

---
+++ app/src/App.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AnatolyShilyaev\Hw11;
+
+class App
+{
+    public function __construct() {}
+
+    public function run(): void
+    {
+        echo "Hello!";
+    }
+}
