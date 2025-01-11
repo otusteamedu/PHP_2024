133cb808b9d698a040481cdd1437c9b35e51b6c2
tshakirov <t.shakirov@truckradar.com>
2025-01-11 13:55:22 +0300

    add types, change query, change structure

--- create.sql
+++ create.sql
@@ -28,33 +28,11 @@ CREATE TABLE attribute_values (
      attribute_id INT REFERENCES attributes(attribute_id),
      value_text TEXT,
      value_boolean BOOLEAN,
-     value_date DATE
+     value_date DATE,
+     value_integer INT,
+     value_float DOUBLE PRECISION
 );
 
 CREATE INDEX idx_movie_id ON attribute_values(movie_id);
 CREATE INDEX idx_attribute_id ON attribute_values(attribute_id);
 CREATE INDEX idx_value_date ON attribute_values(value_date);
-
---View для сборки служебных данных:
-
-CREATE VIEW service_tasks AS
-SELECT m.title AS movie,
-         a.attribute_name AS task,
-         av.value_date AS date
-FROM movies m
-JOIN attribute_values av ON m.movie_id = av.movie_id
-JOIN attributes a ON av.attribute_id = a.attribute_id
-WHERE a.type_id = 4 AND av.value_date BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '20 days';
-
-
---View для сборки данных для маркетинга:
-
-CREATE VIEW marketing_data AS
-SELECT m.title AS movie,
-         at.type_name AS attribute_type,
-         a.attribute_name AS attribute,
-         COALESCE(av.value_text::TEXT, av.value_boolean::TEXT, av.value_date::TEXT) AS value
-FROM movies m
-JOIN attribute_values av ON m.movie_id = av.movie_id
-JOIN attributes a ON av.attribute_id = a.attribute_id
-JOIN attribute_types at ON a.type_id = at.type_id;
--- fill.sql
+++ fill.sql
@@ -17,9 +17,13 @@ INSERT INTO attribute_types (type_name) VALUES
 
 INSERT INTO attributes (attribute_name, type_id) VALUES
 ('Рецензии критиков', 1),
+('Отзыв неизвестной киноакадемии', 1),
 ('Оскар', 2),
+('Ника', 2),
 ('Мировая премьера', 3),
-('Дата начала продажи билетов', 4);
+('Премьера в РФ', 3),
+('Дата начала продажи билетов', 4),
+('Когда запускать рекламу на ТВ', 4);
 
 --attribute_values
 
@@ -40,3 +44,11 @@ INSERT INTO attribute_values (movie_id, attribute_id, value_date) VALUES
 (1, 4, '2010-06-01'),
 (2, 4, '1999-02-01'),
 (3, 4, '2014-10-01');
+
+INSERT INTO attribute_values (movie_id, attribute_id, value_integer) VALUES
+(1, 7, 100),
+(2, 7, 120);
+
+INSERT INTO attribute_values (movie_id, attribute_id, value_float) VALUES
+(1, 8, 9.5),
+(2, 8, 8.7);
