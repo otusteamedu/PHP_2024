cb04585859562d050c92cf2966e1b6361cd28b87
SlavaMakhov <v.mahov@cism-ms.ru>
2025-01-12 06:49:19 +0300

    Homework 9

---
+++ code/data.sql
@@ -0,0 +1,58 @@
+-- Наполнение таблицы movies (Фильмы)
+INSERT INTO movies (id, name)
+VALUES (1, 'Форсаж 7'),
+       (2, 'Железный человек');
+
+-- Наполнение таблицы attribute_types (Типы аттрибутов)
+INSERT INTO attribute_types (id, name, type)
+VALUES (1, 'Рецензия', 'text'),
+       (2, 'Премия', 'boolean'),
+       (3, 'Важные даты', 'date'),
+       (4, 'Служебные даты', 'date'),
+       (5, 'Часть', 'integer'),
+       (6, 'Деньги', 'decimal');
+
+-- Наполнение таблицы attributes (Аттрибуты)
+INSERT INTO attributes (id, name, attribute_type_id)
+VALUES (1, 'Рецензии на IMDb', 1),
+       (2, 'Рецензии на Кинопоиск', 1),
+       (3, 'Премия Оскар', 2),
+       (4, 'Премия Золотой глобус', 2),
+       (5, 'Мировая премьера', 3),
+       (6, 'Премьера в РФ', 3),
+       (7, 'Дата начала продажи билетов', 4),
+       (8, 'Дата рекламы на ТВ', 4),
+       (9, 'Часть фильма', 5),
+       (10, 'Кассовые сборы', 6),
+       (11, 'Бюджет фильма', 6);
+
+-- Наполнение таблицы values (Значения)
+INSERT INTO values (id, attribute_id, movie_id, text_value, boolean_value, date_value, integer_value, decimal_value)
+VALUES (1, 1, 1, 'Один из лучших боевиков, которые существуют, и определенно лучший в серии «Форсаж»...',
+        NULL, NULL, NULL, NULL),
+       (2, 2, 1, 'Фильм дарит положительные эмоции от всего того драйва, который открывается перед зрителем ...',
+        NULL, NULL, NULL, NULL),
+       (3, 3, 1, NULL, 'false', NULL, NULL, NULL),
+       (4, 4, 1, NULL, 'true', NULL, NULL, NULL),
+       (5, 5, 1, NULL, NULL, '2014-07-11', NULL, NULL),
+       (6, 6, 1, NULL, NULL, '2015-04-09', NULL, NULL),
+       (7, 7, 1, NULL, NULL, '2014-07-10', NULL, NULL),
+       (8, 8, 1, NULL, NULL, '2014-02-01', NULL, NULL),
+       (9, 9, 1, NULL, NULL, NULL, 7, NULL),
+       (10, 10, 1, NULL, NULL, NULL, NULL, 1516045911),
+       (11, 11, 1, NULL, NULL, NULL, NULL, 250000000),
+       (12, 1, 2, 'Фильм для людей, которые никогда не знали Железного человека в комиксах. Персонажи получили...',
+        NULL, NULL, NULL, NULL),
+       (13, 2, 2, 'Без сомнений данная фантастическая кинокартина снята очень качественно с визуальными эффектами...',
+        NULL, NULL, NULL, NULL),
+       (14, 3, 2, NULL, 'true', NULL, NULL, NULL),
+       (15, 4, 2, NULL, 'false', NULL, NULL, NULL),
+       (16, 5, 2, NULL, NULL, '2008-04-30', NULL, NULL),
+       (17, 6, 2, NULL, NULL, '2008-05-01', NULL, NULL),
+       (18, 7, 2, NULL, NULL, '2008-04-29', NULL, NULL),
+       (19, 8, 2, NULL, NULL, '2008-01-01', NULL, NULL),
+       (20, 9, 2, NULL, NULL, NULL, 1, NULL),
+       (21, 10, 2, NULL, NULL, NULL, NULL, 585796247),
+       (22, 11, 2, NULL, NULL, NULL, NULL, 140000000);
+
+
---
+++ code/ddl.sql
@@ -0,0 +1,58 @@
+-- Проверка существует ли таблица movies и её удаление
+DROP TABLE IF EXISTS public.movies CASCADE;
+-- Создание таблицы movies (Фильмы)
+CREATE TABLE public.movies
+(
+    "id"   BIGSERIAL    NOT NULL primary key,
+    "name" VARCHAR(100) NOT NULL
+);
+
+-- Проверка существует ли таблица attribute_types и её удаление
+DROP TABLE IF EXISTS public.attribute_types CASCADE;
+-- Создание таблицы attribute_types (Типы атрибутов)
+CREATE TABLE public.attribute_types
+(
+    "id"   BIGSERIAL    NOT NULL primary key,
+    "name" VARCHAR(100) NOT NULL,
+    "type" VARCHAR(50)  NOT NULL
+);
+
+-- Проверка существует ли таблица attributes и её удаление
+DROP TABLE IF EXISTS public.attributes CASCADE;
+-- Создание таблицы attributes (Аттрибуты) + связь с таблицей (attribute_types)
+CREATE TABLE public.attributes
+(
+    "id"                BIGSERIAL    NOT NULL primary key,
+    "name"              VARCHAR(100) NOT NULL,
+    "attribute_type_id" BIGINT       NOT NULL,
+
+    CONSTRAINT public_attributes_attribute_type_id_foreign FOREIGN KEY (attribute_type_id) REFERENCES attribute_types (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Создание индекса для поля attribute_type_id в таблице attributes
+CREATE INDEX public_attributes_attribute_type_id_index on attributes ("attribute_type_id");
+
+-- Проверка существует ли таблица values и её удаление
+DROP TABLE IF EXISTS public.values CASCADE;
+-- Создание таблицы values (Значения) + связь с таблицами (attributes, movies)
+CREATE TABLE public.values
+(
+    "id"              BIGSERIAL      NOT NULL primary key,
+    "attribute_id"    BIGINT         NOT NULL,
+    "movie_id"        BIGINT         NOT NULL,
+    "text_value"      TEXT           NULL DEFAULT NULL,
+    "boolean_value"   BOOLEAN        NULL DEFAULT NULL,
+    "date_value"      DATE           NULL DEFAULT NULL,
+    "integer_value"   INTEGER        NULL DEFAULT NULL,
+    "decimal_value"   DECIMAL(12, 2) NULL DEFAULT NULL,
+
+    CONSTRAINT public_values_attribute_id_foreign FOREIGN KEY (attribute_id) REFERENCES attributes (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT public_values_movie_id_foreign FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Создание индекса для поля attribute_id в таблице values
+CREATE INDEX public_values_attribute_id_index on values ("attribute_id");
+-- Создание индекса для поля movie_id в таблице values
+CREATE INDEX public_values_movie_id_index on values ("movie_id");
+
+
