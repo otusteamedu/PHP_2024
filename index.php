62e43b34ff09b70c2caf88c658a6fd7619a24487
Aliaksei Verameichyk <aliaksei.verameichyk@gmail.com>
2025-01-14 15:36:54 +0100

    fix code style

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ class ListNode
     public $val = 0;
     public $next = null;
 
-    function __construct($val = 0, $next = null)
+    public function __construct($val = 0, $next = null)
     {
         $this->val = $val;
         $this->next = $next;
@@ -14,13 +14,12 @@ class ListNode
 
 class Solution
 {
-
     /**
      * @param ListNode $list1
      * @param ListNode $list2
      * @return ListNode
      */
-    function mergeTwoLists($list1, $list2)
+    public function mergeTwoLists($list1, $list2)
     {
         $head = new ListNode();
         $current = $head;
