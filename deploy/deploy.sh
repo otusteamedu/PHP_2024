2013f27377c485613df5f6ce97342fb1737223d4
Качко Игорь <igor@kr.digital>
2025-01-23 19:14:05 +0300

    hw22 gitlab ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 # Пути для релизов и символических ссылок
-DEPLOY_PATH="/var/www/ikachko.ru/releases"
-CURRENT_PATH="/var/www/ikachko.ru/current"
-PREVIOUS_PATH="/var/www/ikachko.ru/previous"
-NGINX_CONFIG_PATH="/var/www/ikachko.ru/current/infra/nginx/ikachko.ru.conf"  # Путь к конфигу Nginx в проекте
+BASE_PATH="/var/www/ikachko.ru"
+DEPLOY_PATH="$BASE_PATH/releases"
+CURRENT_PATH="$BASE_PATH/current"
+PREVIOUS_PATH="$BASE_PATH/previous"
+NGINX_CONFIG_PATH="$BASE_PATH/current/infra/nginx/ikachko.ru.conf"  # Путь к конфигу Nginx в проекте
 NGINX_ENABLED_PATH="/etc/nginx/sites-enabled/ikachko.ru.conf"          # Путь к sites-enabled конфигу Nginx
 NGINX_AVAILABLE_PATH="/etc/nginx/sites-available/ikachko.ru.conf"          # Путь к sites-available конфигу Nginx
+BASE_ENV_FILE = "$BASE_PATH/.env"
+
 
 # Получаем временную метку
 TIMESTAMP=$(date +'%d-%m-%Y_%H:%M:%S')
 
 # Создаем директорию для нового релиза
 NEW_RELEASE_PATH="$DEPLOY_PATH/$TIMESTAMP"
+NEW_RELEASE_APP_PATH = "$NEW_RELEASE_PATH/app"
+NEW_RELEASE_ENV_ENV_FILE = "$NEW_RELEASE_APP_PATH/app/.env.local"
 mkdir -p $NEW_RELEASE_PATH
 
 # Копируем или клонируем проект в новую директорию
 git clone git@gitlab.com:myown9174003/bills_helper.git $NEW_RELEASE_PATH
+
+if [ -L "$NEW_RELEASE_APP_PATH" ]; then
+  cp $BASE_ENV_FILE $NEW_RELEASE_ENV_ENV_FILE
+  cd $NEW_RELEASE_APP_PATH
+  composer install
+  cd $BASE_PATH
+fi
 
 # Если текущая версия существует, сохраняем её как предыдущую
 if [ -L "$CURRENT_PATH" ]; then
