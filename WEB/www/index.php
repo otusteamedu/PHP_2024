ee4286ee38c367a94eb7c39f830dd6cb1ef9c9f0
Mitzury <bogdanovsi@gmail.com>
2025-01-24 16:31:48 +0300

    modify

--- WEB/www/index.php
+++ WEB/www/index.php
@@ -1,3 +1,18 @@
 <?php
-phpinfo();
+$result = $_SERVER;
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+//$json = file_get_contents('php://input');
+//$data = json_decode($json, true);
+//$string = $data['string'] ?? '';
+//
+//echo "Получили запрос: $string";
+//
+//if (!isset($data['string'])) {
+//    http_response_code(400);
+//    exit;
+//}
+
+
 ?>
