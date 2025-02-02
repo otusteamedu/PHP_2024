418fdd52a00fd61c1a21409ab3abde00318fe19e
Mitzury <bogdanovsi@gmail.com>
2025-02-02 23:24:46 +0300

    r

--- WEB/htdocs/index.php
+++ WEB/htdocs/index.php
@@ -4,6 +4,25 @@ require __DIR__ . '/autoload.php';
 
 use app\Controllers\MainController;
 use app\Services\ValidationService; // Corrected namespace
+use app\Services\EmailValidationService;
+
+// Список email-адресов для проверки
+$emails = [
+    'test@example.com',
+    'ivanivanov666',
+    'user@domain.com',
+    'vasya.petya@sub.domain.com',
+    'brus@egine.xyz'
+];
+
+// Создаем экземпляр сервиса
+$validationService = new EmailValidationService();
+
+// Проверяем каждый email
+foreach ($emails as $email) {
+    $isValid = $validationService->validateEmail($email);
+    echo $email . ' - ' . ($isValid ? 'Valid' : 'Invalid') . "<br>";
+}
 
 $validationService = new ValidationService();
 $controller = new MainController($validationService);
