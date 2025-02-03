b7b59d7cc13daf855ad20838a66b27df0d8a02e3
SlavaMakhov <v.mahov@cism-ms.ru>
2025-02-03 04:52:20 +0300

    Homework 13 code review psr

--- app/src/ListNode.php
+++ app/src/ListNode.php
@@ -10,7 +10,7 @@ class ListNode
     /** @var ListNode|null */
     public ?ListNode $next = null;
 
-    function __construct(?int $val = 0)
+    public function __construct(?int $val = 0)
     {
         $this->val = $val;
     }
@@ -22,7 +22,7 @@ class ListNode
      *
      * @return void
      */
-    function setNext(ListNode $next): void
+    public function setNext(ListNode $next): void
     {
         $this->next = $next;
     }
