5b43889a8bd4c27d1fcd3a0600bc9fb09b025f8b
Mitzury <bogdanovsi@gmail.com>
2025-02-09 18:44:39 +0300

    add composer

---
+++ WEB/context/composer/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Если composer.json отсутствует, создаем его вручную
+if [ ! -f composer.json ]; then
+    cat > composer.json <<EOF
+{
+  "name": "project/app",
+  "description": "My PHP Project",
+  "version": "1.0.0",
+  "require": {
+    "php": "^8.0"
+  },
+  "minimum-stability": "stable",
+  "prefer-stable": true
+}
+EOF
+fi
+
+# Устанавливаем зависимости
+composer install --no-progress --no-suggest
+#!/bin/sh
+# Запуск в режиме ожидания
+while true; do sleep 1000; done
+
+exec composer "$@"
