97f978cae271420158c9cf81b595b753e933ceed
PavelMiasnov <miasnov.p@gmail.com>
2025-02-24 11:52:36 +0300

    Homework: fix formatting

--- src/IntersectionOfTwoLinkedLists.php
+++ src/IntersectionOfTwoLinkedLists.php
@@ -4,16 +4,14 @@ declare(strict_types=1);
 
 namespace Otus\Hw19;
 
-class ListNode
-{
-    public $val = 0;
-    public $next = null;
-
-    public function __construct($val)
-    {
-        $this->val = $val;
-    }
-}
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
 
 class IntersectionOfTwoLinkedLists
 {
