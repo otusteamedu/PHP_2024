e489c1b964823457cf20b4b3220f1a76e681f4f9
Anatoly <Anatoly.Shilyaev@gmail.com>
2025-03-03 20:57:55 +0300

    homework-19

--- app/src/Application/HandleAsyncEvents/AsyncEventHandler.php
+++ app/src/Application/HandleAsyncEvents/AsyncEventHandler.php
@@ -6,7 +6,10 @@ readonly class AsyncEventHandler
 {
     const LISTEN_ASYNC_EVENTS_MSG = "Listen async events";
 
-    public function __construct(private AsyncEventRepository $repository) {}
+    public function __construct(private AsyncEventRepository $repository)
+    {
+        //empty construct
+    }
 
     public function __invoke(callable $callback): string
     {
--- app/src/Application/HandleUserData/UserDataHandler.php
+++ app/src/Application/HandleUserData/UserDataHandler.php
@@ -6,7 +6,10 @@ use AnatolyShilyaev\App\Domain\Request\Request;
 
 readonly class UserDataHandler
 {
-    public function __construct(private UserDataRepository $repository) {}
+    public function __construct(private UserDataRepository $repository)
+    {
+        //empty construct
+    }
 
     public function __invoke(Request $request): string
     {
