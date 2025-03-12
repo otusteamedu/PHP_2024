38872e48e1f8d4c94eac55e71813ed66928cd811
PavelMiasnov <miasnov.p@gmail.com>
2025-03-12 09:32:14 +0300

    edited entry point

--- app/public/index.php
+++ app/public/index.php
@@ -2,20 +2,10 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once dirname(__DIR__) . '/../vendor/autoload.php';
 
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Request;
 use PavelMiasnov\MediaMonitoring\Kernel;
 
-// Load environment variables
-if (file_exists(dirname(__DIR__) . '/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-}
-
-// Запуск ядра приложения
-$kernel = new Kernel($_ENV['APP_ENV'], (bool) $_ENV['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
