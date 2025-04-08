457b76bc2e8e59875ae5f6f8a628b05bed15cbf8
Sergey Shirykalov <shirykalov@yandex.ru>
2025-04-08 19:12:24 +0500

    Fix swagger conf

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -81,7 +81,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => storage_path('api-docs'),
+            'docs' => base_path('public/docs/'),
 
             /*
              * Absolute path to directory where to export views
